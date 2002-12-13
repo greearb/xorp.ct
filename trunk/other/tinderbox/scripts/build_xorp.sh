e03508d10325aa4d2cb32f33bb853b9ca3753b33
hodson <hodson@26435e26-4b25-483a-942d-477236992e84>
2002-12-13 20:09:27 +0000

    Add timeout to catch non-terminating tests.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@21 26435e26-4b25-483a-942d-477236992e84

--- trunk/other/tinderbox/scripts/build_xorp.sh
+++ trunk/other/tinderbox/scripts/build_xorp.sh
@@ -3,8 +3,39 @@
 CONFIG="$(dirname $0)/config"
 . $CONFIG
 
-cd ${ROOTDIR}/xorp
+TIMEOUT=3600
 
-( ./configure 1>&2 ) && ( gmake -k $* 1>&2 )
+timeout()
+{
+    cat >&2 <<-EOF
+
+****************************************************************************** 
+Suspect process is wedged. Timed out after a wait of ${TIMEOUT} seconds.
+****************************************************************************** 
+EOF
+    kill 0
+    exit -1
+}
+
+#
+# set_timeout <seconds>
+#
+set_timeout()
+{
+    (sleep $1 && timeout ) 2>&1 > /dev/null &
+    TIMEOUT_PID=$!
+}
+
+unset_timeout()
+{
+    kill -9 ${TIMEOUT_PID} 2>&1 > /dev/null
+}
+
+###############################################################################
+
+cd ${ROOTDIR}/xorp
+set_timeout ${TIMEOUT}
+( ./configure 1>&2 ) && ( gmake -k $@ 1>&2 )
+unset_timeout
 
 
