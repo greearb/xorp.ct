312212fa168ace69ba3e727b1e483044e277de0e
mjh <mjh@26435e26-4b25-483a-942d-477236992e84>
2003-01-21 03:52:32 +0000

    Fix initialization error that caused crash on Linux and FreeBSD with gcc 3.2.1
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@162 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxorp/ref_trie.hh
+++ trunk/xorp/libxorp/ref_trie.hh
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/libxorp/trie.hh,v 1.2 2003/01/16 19:09:28 hodson Exp $
+// $XORP: xorp/libxorp/ref_trie.hh,v 1.1 2003/01/21 01:11:07 mjh Exp $
 
 #ifndef __LIBXORP_REF_TRIE_HH__
 #define __LIBXORP_REF_TRIE_HH__
@@ -21,7 +21,7 @@
 
 // Macros 
 //#define VALIDATE_XORP_TRIE
-#define DEBUG_LOGGING
+//#define DEBUG_LOGGING
 
 #include "debug.h"
 
@@ -849,9 +849,9 @@ RefTrieNode<A, Payload> *
 RefTrieNode<A, Payload>::erase()
 {
     RefTrieNode *me, *parent, *child;
-
     if ((_references & NODE_REFS_MASK) > 0) {
 	_references |= NODE_DELETED;
+	me = this;
     } else {
 	_references |= NODE_DELETED;
 	if (_p) {
@@ -869,9 +869,9 @@ RefTrieNode<A, Payload>::erase()
 	for (me = this; me && me->_p == NULL &&
 		 (me->_left == NULL || me->_right == NULL); ) {
 
-	    // me->dump("erase");			// debugging
+	    //me->dump("erase");			// debugging
 
-	// Find parent and the one possible child (both can be NULL).
+	    // Find parent and the one possible child (both can be NULL).
 	    parent = me->_up;
 	    child = me->_left ? me->_left : me->_right;
 
