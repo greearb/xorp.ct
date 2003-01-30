493c271ec51d99b3f559191f629bd024eeac24b6
mjh <mjh@26435e26-4b25-483a-942d-477236992e84>
2003-01-30 19:21:11 +0000

    Unfinished policy filter code.  Not yet linked in for compilation.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@290 26435e26-4b25-483a-942d-477236992e84

---
+++ trunk/xorp/policy/policy_route.cc
@@ -0,0 +1,38 @@
+// -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*-
+
+// Copyright (c) 2001-2003 International Computer Science Institute
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software")
+// to deal in the Software without restriction, subject to the conditions
+// listed in the XORP LICENSE file. These conditions include: you must
+// preserve this copyright notice, and you cannot mention the copyright
+// holders in advertising related to the Software without their permission.
+// The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+// notice is a summary of the XORP LICENSE file; the license in that file is
+// legally binding.
+
+#ident "$XORP: xorp/devnotes/template.cc,v 1.2 2003/01/16 19:08:48 mjh Exp $"
+
+#include "policy_route.hh"
+
+template <typename A>
+PolicyRoute<A>::PolicyRoute(const IPNet<A> &net, 
+			    const A& nexthop, 
+			    const string& origin_protocol)
+    : _net(net), _nexthop(nexthop), _origin_protocol(origin_protocol)
+{
+}
+
+
+template <typename A>
+BGPPolicyRoute<A>::BGPPolicyRoute(const IPNet<A> &net, 
+				  const A& nexthop, 
+				  const AsPath& as_path, 
+				  OriginType origin,
+				  bool _ibgp)
+    : PolicyRoute(net, nexthop, "bgp"),
+      _as_path(as_path), _origin(origin), _ibgp(ibgp),
+      _has_med = false, _has_localpref = false;
+{
+}
