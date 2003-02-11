3e2a86925e35ae50c95dce56c29ede24bb2b6a5e
mjh <mjh@26435e26-4b25-483a-942d-477236992e84>
2003-02-11 00:06:07 +0000

    Return correct exit status.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@402 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/test_main.cc
+++ trunk/xorp/bgp/test_main.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/test_ribin.cc,v 1.11 2003/02/08 07:30:24 rizzo Exp $"
+#ident "$XORP: xorp/bgp/test_main.cc,v 1.1 2003/02/10 23:40:09 mjh Exp $"
 
 #include <stdio.h>
 #include "bgp_module.h"
@@ -41,57 +41,68 @@ int main(int argc, char** argv)
     bool test_ribout_succeeded = test_ribout(argc, argv);
 
 
-
+    bool status = 0;
     if (test_ribin_succeeded) {
 	printf("Test RibIn: PASS\n");
     } else {
+	status = -1;
 	printf("Test RibIn: FAIL\n");
     }
     if (test_deletion_succeeded) {
 	printf("Test Deletion: PASS\n");
     } else {
+	status = -1;
 	printf("Test Deletion: FAIL\n");
     }
     if (test_filter_succeeded) {
 	printf("Test Filter: PASS\n");
     } else {
+	status = -1;
 	printf("Test Filter: FAIL\n");
     }
     if (test_cache_succeeded) {
 	printf("Test Cache: PASS\n");
     } else {
+	status = -1;
 	printf("Test Cache: FAIL\n");
     }
     if (test_nhlookup_succeeded) {
 	printf("Test NhLookup: PASS\n");
     } else {
+	status = -1;
 	printf("Test NhLookup: FAIL\n");
     }
     if (test_cache_succeeded) {
 	printf("Test Cache: PASS\n");
     } else {
+	status = -1;
 	printf("Test Cache: FAIL\n");
     }
     if (test_decision_succeeded) {
 	printf("Test Decision: PASS\n");
     } else {
+	status = -1;
 	printf("Test Decision: FAIL\n");
     }
     if (test_fanout_succeeded) {
 	printf("Test Fanout: PASS\n");
     } else {
+	status = -1;
 	printf("Test Fanout: FAIL\n");
     }
     if (test_dump_succeeded) {
 	printf("Test Dump: PASS\n");
     } else {
+	status = -1;
 	printf("Test Dump: FAIL\n");
     }
     if (test_ribout_succeeded) {
 	printf("Test Ribout: PASS\n");
     } else {
+	status = -1;
 	printf("Test Ribout: FAIL\n");
     }
+    return status;
 }
 
 
