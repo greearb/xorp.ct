69a66f5097bf2ba6ab56fc6795f2b380cf71b85f
hodson <hodson@26435e26-4b25-483a-942d-477236992e84>
2003-02-26 00:11:08 +0000

    Move clean of finder info to point when new connection with finder is established, rather than first detecting when finder has gone away, ie allow processes to continue in absence of finder.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@463 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxipc/finder_ng_client.cc
+++ trunk/xorp/libxipc/finder_ng_client.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/devnotes/template.cc,v 1.2 2003/01/16 19:08:48 mjh Exp $"
+#ident "$XORP: xorp/libxipc/finder_ng_client.cc,v 1.1 2003/02/24 19:39:18 hodson Exp $"
 
 #include "finder_module.h"
 
@@ -415,6 +415,7 @@ FinderNGClient::messenger_birth_event(FinderMessengerBase* m)
 {
     debug_msg("messenger %p birth\n", m);    
     XLOG_ASSERT(0 == _messenger);
+    prepare_for_restart();
     _messenger = m;
     crank();
 }
@@ -425,7 +426,6 @@ FinderNGClient::messenger_death_event(FinderMessengerBase* m)
     debug_msg("messenger %p death\n", m);
     XLOG_ASSERT(m == _messenger);
     _messenger = 0;
-    prepare_for_restart();
 }
 
 void
