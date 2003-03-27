e443afd69b0b31b2f84e483a5eecaaeafd7cd802
mjh <mjh@26435e26-4b25-483a-942d-477236992e84>
2003-03-27 19:30:35 +0000

    Workaround for yacc incompatibilities on newer RedHat systems.  If you change any of the .yy files, you need to run "gmake yacc" explicitly to create the generated files.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@773 26435e26-4b25-483a-942d-477236992e84

---
+++ trunk/xorp/rtrmgr/y.opcmd_tab.h
@@ -0,0 +1,12 @@
+#ifndef YYERRCODE
+#define YYERRCODE 256
+#endif
+
+#define STRING 257
+#define COMMAND 258
+#define UPLEVEL 259
+#define DOWNLEVEL 260
+#define COLON 261
+#define END 262
+#define LITERAL 263
+#define VARIABLE 264
