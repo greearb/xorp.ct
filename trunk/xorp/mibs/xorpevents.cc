eed8df2df3309a6e9d80f94ea458fec5c8bb2e55
jcardona <jcardona@26435e26-4b25-483a-942d-477236992e84>
2003-04-09 22:41:00 +0000

    Applied changes suggested in yesterday's codewalk.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@853 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/mibs/xorpevents.cc
+++ trunk/xorp/mibs/xorpevents.cc
@@ -30,46 +30,56 @@ SnmpEventLoop * SnmpEventLoop::_sel = NULL;
 // only needed in this module, but not declared static so they can be invoked
 // from test programs
 //
-void 
-run_fd_callbacks (int, void *) 
+void
+run_fd_callbacks (int, void *)
 {
     SnmpEventLoop& e = SnmpEventLoop::the_instance();
-    /* 
+    /*
      * we know the next call will not block since one of XORP's monitored file
      * descriptors showed some activity, thus the 0 ms timeout
      */
     DEBUGMSGTL((e._log_name, "run all xorp file descriptor callbacks\n"));
     if (!e.selector_list().select(0))
 	snmp_log(LOG_WARNING, "call to run_fd_callbacks did nothing\n");
-    e._export_fds();
+    e.export_fds();
 }
 
 void
-run_timer_callbacks(u_int alarm_id, void *) 
+run_timer_callbacks(u_int alarm_id, void *)
 {
     SnmpEventLoop& e = SnmpEventLoop::the_instance();
     DEBUGMSGTL((e._log_name, "run all xorp timers\n"));
     DEBUGMSGTL((e._log_name, "# of timers: %d\n", e.timer_list().size()));
-    e.timer_list().run(); 
+    e.timer_list().run();
     SnmpEventLoop::AlarmMap::iterator p;
-    for (p=e._pending_alarms.begin(); p != e._pending_alarms.end(); p++) {
+    for (p = e._pending_alarms.begin(); p != e._pending_alarms.end(); ++p) {
 	if (alarm_id == (*p).second) {
 	    e._pending_alarms.erase(p);
 	    break;
 	}
     }
-    e._export_timers();
+    e.export_timers();
+}
+
+SnmpEventLoop&
+SnmpEventLoop::the_instance()
+{
+    if (!_sel) {
+	_sel = new SnmpEventLoop;
+	DEBUGMSGTL((_log_name, "new shared event loop...\n"));
+    }
+    return *_sel;
 }
 
 void
 SnmpEventLoop::export_events()
-{    
-    _export_timers();
-    _export_fds();
+{
+    export_timers();
+    export_fds();
 }
 
-void 
-SnmpEventLoop::_export_fds() 
+void
+SnmpEventLoop::export_fds()
 {
     fd_set read_fds, write_fds, except_fds;
     FD_ZERO(&read_fds); FD_ZERO(&write_fds); FD_ZERO(&except_fds);
@@ -77,99 +87,106 @@ SnmpEventLoop::_export_fds()
     s.get_fd_set(SEL_RD, read_fds);
     s.get_fd_set(SEL_WR, write_fds);
     s.get_fd_set(SEL_EX, except_fds);
-    for (int fd=3; fd<s.get_max_fd()+1; fd++) {
+    for (int fd = 0; fd<s.get_max_fd()+1; fd++) {
     // the check to see if the fd is already in _exported_*fds is needed
-    // because the snmpd call register_*fd does not check for duplicate fd's.  
-	if (FD_ISSET(fd, &read_fds) && 
+    // because the snmpd call register_*fd does not check for duplicate fd's.
+	if (FD_ISSET(fd, &read_fds) &&
 	    (_exported_readfds.end() == _exported_readfds.find(fd))) {
 	    if (FD_REGISTERED_OK != register_readfd(fd, run_fd_callbacks, NULL))
 		snmp_log(LOG_WARNING, "unable to import xorp fd %d\n", fd);
 	    else {
-		DEBUGMSGTL((_log_name, "imported xorp rdfd:%d\n",fd));
+		DEBUGMSGTL((_log_name, "imported xorp rdfd:%d\n", fd));
 		_exported_readfds.insert(fd);
 	    }
 	}
-	if (FD_ISSET(fd, &write_fds) && 
+	if (FD_ISSET(fd, &write_fds) &&
 	    (_exported_writefds.end() == _exported_writefds.find(fd))) {
-	    if (FD_REGISTERED_OK != register_writefd(fd, run_fd_callbacks,NULL))
+	    if (FD_REGISTERED_OK != register_writefd(fd, run_fd_callbacks, NULL))
                 snmp_log(LOG_WARNING, "unable to import xorp fd %d\n", fd);
 	    else {
-		DEBUGMSGTL((_log_name, "imported xorp wrfd:%d \n",fd));
+		DEBUGMSGTL((_log_name, "imported xorp wrfd:%d \n", fd));
 		_exported_writefds.insert(fd);
 	    }
 	}
 	if (FD_ISSET(fd, &except_fds) &&
 	    (_exported_exceptfds.end() == _exported_exceptfds.find(fd))) {
-	    if (FD_REGISTERED_OK != register_exceptfd(fd,run_fd_callbacks,NULL))
+	    if (FD_REGISTERED_OK != register_exceptfd(fd, run_fd_callbacks, NULL))
                 snmp_log(LOG_WARNING, "unable to import xorp fd %d\n", fd);
 	    else {
-		DEBUGMSGTL((_log_name, "imported xorp exfd:%d \n",fd));
+		DEBUGMSGTL((_log_name, "imported xorp exfd:%d \n", fd));
 		_exported_exceptfds.insert(fd);
 	    }
 	}
     }
     // and finally, remove all the fd's that are no longer in use
-    SnmpEventLoop::FdSet::iterator p;
-    for (p=_exported_readfds.begin(); p != _exported_readfds.end(); p++)
+
+    SnmpEventLoop::FdSet::iterator c, p;
+    for (c = _exported_readfds.begin(); c != _exported_readfds.end();) {
+	p = c++;		     // p can now be safely erased if needed
 	if (!FD_ISSET(*p, &read_fds)) {
 	    DEBUGMSGTL((_log_name, "erased xorp rdfd:%d\n",*p));
 	    unregister_readfd(*p);
 	    _exported_readfds.erase(p);
 	}
-    for (p=_exported_writefds.begin(); p != _exported_writefds.end(); p++)
+    }
+    for (c = _exported_writefds.begin(); c != _exported_writefds.end();) {
+	p = c++;		     // p can now be safely erased if needed
 	if (!FD_ISSET(*p, &write_fds)) {
 	    DEBUGMSGTL((_log_name, "erased xorp wrfd:%d\n",*p));
 	    unregister_writefd(*p);
 	    _exported_writefds.erase(p);
 	}
-    for (p=_exported_exceptfds.begin(); p != _exported_exceptfds.end(); p++)
+    }
+    for (c = _exported_exceptfds.begin(); c != _exported_exceptfds.end();) {
+	p = c++;		     // p can now be safely erased if needed
 	if (!FD_ISSET(*p, &except_fds)) {
 	    DEBUGMSGTL((_log_name, "erased xorp exfd:%d\n",*p));
 	    unregister_exceptfd(*p);
 	    _exported_exceptfds.erase(p);
 	}
+    }
 }
 
-void 
-SnmpEventLoop::_export_timers() 
+void
+SnmpEventLoop::export_timers()
 {
     TimerList & t = the_instance().timer_list();
     TimeVal del_tv, abs_tv;
     timeval snmp_tv;
-    unsigned int alarm_id;	
+    unsigned int alarm_id;
     if (!t.get_next_delay(del_tv)) return;
     t.get_next_expire(abs_tv);
-    if (_pending_alarms.end() != _pending_alarms.find(abs_tv)) 
+    if (_pending_alarms.end() != _pending_alarms.find(abs_tv))
 	return;  // timer already exported
-    DEBUGMSGTL((_log_name, "imported xorp timeout:%d s %d us \n", 
+    DEBUGMSGTL((_log_name, "imported xorp timeout:%d s %d us \n",
 			    del_tv.sec(), del_tv.usec()));
     del_tv.copy_out(snmp_tv);
     alarm_id = snmp_alarm_register_hr(snmp_tv, 0, run_timer_callbacks, NULL);
     if (!alarm_id) snmp_log(LOG_WARNING, "unable to import xorp timout");
-    else { 
+    else {
 	SnmpEventLoop::AlarmMap::value_type al(abs_tv, alarm_id);
 	_pending_alarms.insert(al);
     }
 }
 
-void 
-SnmpEventLoop::_clear_pending_alarms ()
-{ 
+void
+SnmpEventLoop::clear_pending_alarms ()
+{
     SnmpEventLoop::AlarmMap::iterator p;
-    for (p=_pending_alarms.begin(); p != _pending_alarms.end(); p++)
+    for (p = _pending_alarms.begin(); p != _pending_alarms.end(); p++)
 	snmp_alarm_unregister((*p).second);
     _pending_alarms.erase(_pending_alarms.begin(), _pending_alarms.end());
 }
 
-void 
-SnmpEventLoop::_clear_monitored_fds ()
-{ 
+void
+SnmpEventLoop::clear_monitored_fds ()
+{
     SnmpEventLoop::FdSet::iterator p;
-    for (p=_exported_readfds.begin(); p != _exported_readfds.end(); p++)
+    for (p = _exported_readfds.begin(); p != _exported_readfds.end(); p++)
 	unregister_readfd(*p);
-    for (p=_exported_writefds.begin(); p != _exported_writefds.end(); p++)
+    for (p = _exported_writefds.begin(); p != _exported_writefds.end(); p++)
 	unregister_writefd(*p);
-    for (p=_exported_exceptfds.begin(); p != _exported_exceptfds.end(); p++)
+    for (p = _exported_exceptfds.begin(); p != _exported_exceptfds.end(); p++)
 	unregister_exceptfd(*p);
     _exported_readfds.erase(_exported_readfds.begin(), _exported_readfds.end());
     _exported_writefds.erase(_exported_writefds.begin(), _exported_writefds.end());
--- trunk/xorp/mibs/xorpevents.hh
+++ trunk/xorp/mibs/xorpevents.hh
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/devnotes/template.hh,v 1.2 2003/01/16 19:08:48 mjh Exp $
+// $XORP: xorp/mibs/xorpevents.hh,v 1.1 2003/04/08 19:11:33 jcardona Exp $
 
 #ifndef __MIBS_XORPEVENTLOOP_HH__
 #define __MIBS_XORPEVENTLOOP_HH__
@@ -26,68 +26,67 @@
 class SnmpEventLoop;
 
 /**
- * @short A singleton @ref EventLoop capable of exporting events to the snmp agent 
+ * @short A singleton @ref EventLoop capable of exporting events to the snmp agent
  *
  * The @ref SnmpEventLoop class provides an EventLoop to all the MIB modules
  * that need to communicate with XORP.  The modules will use the EventLoop to
- * instanciate listeners required for asyncronous communication with XORP.  
+ * instanciate listeners required for asyncronous inter-process communication
+ * with XORP.
  */
 
 
 class SnmpEventLoop : public EventLoop {
 public:
     typedef std::set<int> FdSet;
-    typedef std::map<TimeVal,unsigned int> AlarmMap; 
+    typedef std::map<TimeVal, unsigned int> AlarmMap;
 public:
-    ~SnmpEventLoop() 
-    { 
+    ~SnmpEventLoop()
+    {
 	DEBUGMSGTL((_log_name, "shared event loop freed...!\n"));
-	_clear_pending_alarms(); 
-	_clear_monitored_fds();	
+	clear_pending_alarms();
+	clear_monitored_fds();
 	delete _sel;
     }
-    
-    
+
+
     /**
      * @return reference to the @ref SnmpEventLoop used by all XORP MIB
-     * modules.  This is the only way to instantiate this class, since
+     * modules.
+     *
+     * This function provides the only way to instantiate this class, since
      * all other constructors are private.  Use it like this:
+    <pre>
+     * SnmpEventLoop& e = SnmpEventLoop::the_instance();
+    </pre>
      *
-     * SnmpEventLoop& e = SnmpEventLoop::the_instance(); 
-     *
-     * Now use 'e' anywhere you would use an event loop in Xorp modules, 
+     * Now use 'e' anywhere you would use an event loop in Xorp modules,
      * for instance:
-     *
+    <pre>
      * XorpTimer xt = e.new_periodic(100, callback);
-     *
+    </pre>
      * Before returning from your MIB module, you should export the event
      * loop events to the snmp agent.
      *
+    <pre>
      * e.export_events();
+    </pre>
      *
-     * NOTE:  typically you would never need to call e.run(), since the 
+     * NOTE:  typically you would never need to call e.run(), since the
      * MIB modules should not block.
      */
-    static SnmpEventLoop& the_instance() 
-    {
-	if (!_sel) {
-	    _sel = new SnmpEventLoop;
-	    DEBUGMSGTL((_log_name, "new shared event loop...\n"));
-	}
-        return *_sel;
-    }
+    static SnmpEventLoop& the_instance();
 
     /**
-     * this function will transfer the timers and file descriptors
+     * This function transfers the timers and file descriptors
      * from the XORP event list onto the snmp agent registry. This
      * way the snmp agent will be able to monitor their activity
      * and invoke the appropriate callback function in one of XORP's
      * MIB modules.
      */
-    void	export_events();     
+    void	export_events();
 
      /**
-     * this is the name this class will use to identify itself in the 
+     * This is the name this class will use to identify itself in the
      * snmpd log.  Only used for debugging
      */
     static void set_log_name(const char * n) { _log_name = n; }
@@ -100,19 +99,19 @@ protected:
     FdSet _exported_exceptfds;
 
 private:
-    SnmpEventLoop(SnmpEventLoop& o);		    // Not implemented 
+    SnmpEventLoop(SnmpEventLoop& o);		    // Not implemented
     SnmpEventLoop& operator= (SnmpEventLoop& o);    // Not implemented
 
-    SnmpEventLoop() : EventLoop() {}	// Private to prevent instantiation 
-    void _export_timers();
-    void _export_fds();
-    void _clear_pending_alarms ();
-    void _clear_monitored_fds ();
+    SnmpEventLoop() : EventLoop() {}	// Private to prevent instantiation
+    void export_timers();
+    void export_fds();
+    void clear_pending_alarms ();
+    void clear_monitored_fds ();
     static SnmpEventLoop * _sel;
     static const char * _log_name;
 
-    friend void run_fd_callbacks (int, void *); 
-    friend void run_timer_callbacks(u_int, void *); 
+    friend void run_fd_callbacks (int, void *);
+    friend void run_timer_callbacks(u_int, void *);
 };
 
 
