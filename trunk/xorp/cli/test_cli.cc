32c409b97fdbac9fb689e147d9932b2a7b509594
hodson <hodson@26435e26-4b25-483a-942d-477236992e84>
2003-04-23 21:02:50 +0000

    Purge NG designation from Finder class, method, and file names now there is only a single version of the Finder.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@973 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/cli/test_cli.cc
+++ trunk/xorp/cli/test_cli.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/cli/test_cli.cc,v 1.10 2003/04/03 00:24:02 hodson Exp $"
+#ident "$XORP: xorp/cli/test_cli.cc,v 1.11 2003/04/22 23:27:16 hodson Exp $"
 
 
 //
@@ -171,9 +171,9 @@ main(int argc, char *argv[])
 	//
 	// Finder
 	//
-	FinderNGServer *finder = 0;
+	FinderServer *finder = 0;
 	try {
-	    finder = new FinderNGServer(eventloop);
+	    finder = new FinderServer(eventloop);
 	} catch (const InvalidPort&) {
 	    XLOG_FATAL("Could not start in-process Finder");
 	}
