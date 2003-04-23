4d62a0dd84ff4a6f54aa8994577469d5eab37b70
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2003-04-23 09:37:57 +0000

    Update the locally kept version of some header files.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@966 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/mrt/include/netinet/pim_var.h
+++ trunk/xorp/mrt/include/netinet/pim_var.h
@@ -1,36 +1,34 @@
+/*	$FreeBSD$	*/
+
 /*
- *  Copyright (c) 1998-2000 by the University of Southern California.
- *  All rights reserved.
+ * Copyright (c) 1998-2000
+ * University of Southern California/Information Sciences Institute.
+ * All rights reserved.
  *
- *  Permission to use, copy, modify, and distribute this software and
- *  its documentation in source and binary forms for lawful
- *  purposes and without fee is hereby granted, provided
- *  that the above copyright notice appear in all copies and that both
- *  the copyright notice and this permission notice appear in supporting
- *  documentation, and that any documentation, advertising materials,
- *  and other materials related to such distribution and use acknowledge
- *  that the software was developed by the University of Southern
- *  California and/or Information Sciences Institute.
- *  The name of the University of Southern California may not
- *  be used to endorse or promote products derived from this software
- *  without specific prior written permission.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- *  THE UNIVERSITY OF SOUTHERN CALIFORNIA DOES NOT MAKE ANY REPRESENTATIONS
- *  ABOUT THE SUITABILITY OF THIS SOFTWARE FOR ANY PURPOSE.  THIS SOFTWARE IS
- *  PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES,
- *  INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, TITLE, AND 
- *  NON-INFRINGEMENT.
- *
- *  IN NO EVENT SHALL USC, OR ANY OTHER CONTRIBUTOR BE LIABLE FOR ANY
- *  SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES, WHETHER IN CONTRACT,
- *  TORT, OR OTHER FORM OF ACTION, ARISING OUT OF OR IN CONNECTION WITH,
- *  THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
- *  Other copyrights might apply to parts of this software and are so
- *  noted when applicable.
+ * THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
-/* $Id$ */
 
 #ifndef _NETINET_PIM_VAR_H_
 #define _NETINET_PIM_VAR_H_
@@ -61,30 +59,9 @@ struct pimstat {
 	u_quad_t pims_snd_registers_bytes; /* sent regs. bytes (data only)   */
 };
 
-#ifndef __P
-#ifdef __STDC__
-#define __P(x)  x
-#else
-#define __P(x)  ()
-#endif
-#endif
-
-#if (defined(KERNEL)) || (defined(_KERNEL))
-extern struct pimstat pimstat;
-
-#if defined(NetBSD) || defined(__OpenBSD__)
-void pim_input __P((struct mbuf *, ...));
-#elif (defined(__FreeBSD__) && (__FreeBSD_version >= 400000))
-void pim_input __P((struct mbuf *, int, int));
-#else
-void pim_input __P((struct mbuf *, int));
-#endif /* ! (NetBSD || OpenBSD) */
-#endif /* KERNEL */
-
 /*
  * Names for PIM sysctl objects
  */
-#if (defined(__FreeBSD__)) || (defined(NetBSD)) || defined(__OpenBSD__) || (defined(__bsdi__))
 #define PIMCTL_STATS		1	/* statistics (read-only) */
 #define PIMCTL_MAXID		2
 
@@ -92,23 +69,12 @@ void pim_input __P((struct mbuf *, int));
 	{ 0, 0 },			\
 	{ "stats", CTLTYPE_STRUCT },	\
 }
-#endif /* FreeBSD || NetBSD || OpenBSD || bsdi */
 
-#if (defined(__FreeBSD__) && (__FreeBSD_version >= 400000))
-#if ((defined(KERNEL)) || (defined(_KERNEL)))
-#if defined(SYSCTL_DECL)
+#if (defined(KERNEL)) || (defined(_KERNEL))
+extern struct pimstat pimstat;
+
+void pim_input(struct mbuf *, int);
 SYSCTL_DECL(_net_inet_pim);
-#endif /* SYSCTL_DECL	 */
-#endif /* KERNEL	 */
-#endif /* FreeBSD >= 4.0 */
-
-#if (defined(NetBSD) || defined(__OpenBSD__) || defined(__bsdi__))
-#define PIMCTL_VARS {			\
-	0,				\
-	0,				\
-}
-int	pim_sysctl __P((int *, u_int, void *, size_t *, void *, size_t));
-#endif /* NetBSD || OpenBSD || bsdi */
+#endif /* KERNEL */
 
 #endif /* _NETINET_PIM_VAR_H_ */
-
