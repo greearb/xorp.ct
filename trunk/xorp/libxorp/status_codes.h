a1aa58bd15fe9e42b6757b858e41d66a57aaf268
mjh <mjh@26435e26-4b25-483a-942d-477236992e84>
2003-05-07 23:40:15 +0000

    Status codes definintion
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@1017 26435e26-4b25-483a-942d-477236992e84

---
+++ trunk/xorp/libxorp/status_codes.h
@@ -0,0 +1,33 @@
+/* -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*- */
+
+/*
+ * Copyright (c) 2001-2003 International Computer Science Institute
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software")
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the XORP LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the XORP LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+/*
+ * $XORP: xorp/devnotes/template.h,v 1.2 2003/01/16 19:08:48 mjh Exp $
+ */
+
+#ifndef __STATUS_CODES_H__
+#define __STATUS_CODES_H__
+
+typedef enum {
+  PROC_NULL = 0,
+  PROC_STARTUP = 1,
+  PROC_NOT_READY = 2,
+  PROC_READY = 3,
+  PROC_SHUTDOWN = 4,
+  PROC_FAILED = 5
+} ProcessStatus;
+
+#endif /* __STATUS_CODES_H__ */
