39ad668bba4ddbb7599f0f78042f2e0e803eca61
jcardona <jcardona@26435e26-4b25-483a-942d-477236992e84>
2003-05-09 00:42:38 +0000

    Changed config to use SNMPv2c, since root access is required to create SNMPv3 users
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@1028 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/mibs/snmpconfig/snmpenv.sh
+++ trunk/xorp/mibs/snmpconfig/snmpenv.sh
@@ -9,3 +9,14 @@
 export XORP=`realpath ../..`
 export MIBDIRS=+${XORP}/mibs/textual
 export MIBS=+BGP4-MIB
+
+# create snmp.conf in home directory.  This provides the default options to be
+# passed to the Net-SNMP command line tools (version, community string, etc.)
+
+if ! test -e ~/.snmp/snmp.conf; then
+    if ! test -d ~/.snmp; then
+	mkdir ~/.snmp
+    fi
+    echo 'copied default snmp.conf in directory ~/.snmp'
+    cp ${XORP}/mibs/snmpconfig/snmp.conf ~/.snmp/
+fi 
--- trunk/xorp/mibs/snmpdscripts/xorpenv.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#
-# Set the environment variables needed to run the scripts in this directory
-#
-# usage:
-# 
-#     $ source set_env.sh
-#
-export XORP=`realpath ../..`
