95ed486154709a4a28318c9372e6be1911bbb477
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2003-05-14 20:33:10 +0000

    Dummy FEA related bugfix.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@1063 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/ifconfig_set_dummy.cc
+++ trunk/xorp/fea/ifconfig_set_dummy.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/ifconfig_set_dummy.cc,v 1.1 2003/05/10 00:06:41 pavlin Exp $"
+#ident "$XORP: xorp/fea/ifconfig_set_dummy.cc,v 1.2 2003/05/14 01:13:43 pavlin Exp $"
 
 
 #include "fea_module.h"
@@ -59,6 +59,7 @@ IfConfigSetDummy::stop()
 bool
 IfConfigSetDummy::push_config(const IfTree& it)
 {
+    ifc().report_updates(it);
     ifc().set_live_config(it);
     
     return true;
