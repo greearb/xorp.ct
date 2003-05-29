26cf936941862f427b846528be74003fbfd5de4c
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2003-05-29 17:58:33 +0000

    Update the copyright header style (using the style from devnotes/template.cc)
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@1252 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/contrib/ospfd/xorp/ospf_config.C
+++ trunk/xorp/contrib/ospfd/xorp/ospf_config.C
@@ -1,17 +1,17 @@
-// -*- c-basic-offset: 4 -*-
+// -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*-
 
-// Copyright (c) 2001,2002 International Computer Science Institute
+// Copyright (c) 2001-2003 International Computer Science Institute
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software")
 // to deal in the Software without restriction, subject to the conditions
-// listed in the Xorp LICENSE file. These conditions include: you must
+// listed in the XORP LICENSE file. These conditions include: you must
 // preserve this copyright notice, and you cannot mention the copyright
 // holders in advertising related to the Software without their permission.
 // The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
-// notice is a summary of the Xorp LICENSE file; the license in that file is
+// notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
-//
+
 // You may also (at your option) redistribute this software and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
@@ -21,7 +21,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-#ident "$XORP: xorp/ospfd/xorp/ospf_config.C,v 1.12 2002/12/09 18:29:24 hodson Exp $"
+#ident "$XORP: xorp/ospfd/xorp/ospf_config.C,v 1.1.1.1 2002/12/11 23:56:10 hodson Exp $"
 
 #if defined(OLD_CONFIG_CODE)
 
