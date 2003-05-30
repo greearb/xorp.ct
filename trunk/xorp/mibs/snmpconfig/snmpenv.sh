26ad3c319c25e03e6f6428f4933844e7d7e2bd7f
hodson <hodson@26435e26-4b25-483a-942d-477236992e84>
2003-05-30 01:18:53 +0000

    Coerce SNMP make check target to work with cross-directory build.
    snmpdscripts require fixing up by configure to work with
    cross-directory build and/or installation.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@1275 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/mibs/snmpconfig/snmpenv.sh
+++ trunk/xorp/mibs/snmpconfig/snmpenv.sh
@@ -6,30 +6,34 @@
 # 
 #     $ . snmpenv.sh
 #
-XORP=`(cd ../..; pwd)`
-MIBDIRS=+${XORP}/mibs/textual
+
+if [ "X${srcdir}" = "X" ] ; then srcdir=`dirname $0` ; fi
+
+XORP_TOP_SRCDIR=`(cd ${srcdir}/../..; pwd)`
+XORP_TOP_BUILDDIR=`(cd ${PWD}/../..; pwd)`
+
+MIBDIRS=+${XORP_TOP_SRCDIR}/mibs/textual
 MIBS=+BGP4-MIB
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${XORP}/mibs
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${XORP_TOP_BUILDDIR}/mibs
  
-export XORP MIBDIRS MIBS LD_LIBRARY_PATH
+export XORP_TOP_SRCDIR XORP_TOP_BUILDDIR MIBDIRS MIBS LD_LIBRARY_PATH
 
 # create snmp.conf in home directory.  This provides the default options to be
 # passed to the Net-SNMP command line tools (version, community string, etc.)
 
 if ! test -e ~/.snmp/snmp.conf; then
-    if ! test -d ~/.snmp; then
-	mkdir ~/.snmp
-    fi
+	mkdir -p ~/.snmp
+    cp ${XORP_TOP_SRCDIR}/mibs/snmpconfig/snmp.conf ~/.snmp/
     echo 'copied default snmp.conf in directory ~/.snmp'
-    cp ${XORP}/mibs/snmpconfig/snmp.conf ~/.snmp/
-
 fi 
 
 # create an entry in snmpd.conf to automatically load the XORP interface mib
 # module.  After that module is loaded, we can use the xorp_if_mib interface
 # to load other modules using XRL's
 #
-NEW_SNMPD_CONF=${XORP}/mibs/snmpconfig/snmpd.conf
-cp ${XORP}/mibs/snmpconfig/snmpd.preconf ${NEW_SNMPD_CONF}
-echo "dlmod xorp_if_mib_module ${XORP}/mibs/xorp_if_mib_module.so"  \
-    >> ${NEW_SNMPD_CONF} 
+NEW_SNMPD_CONF=${XORP_TOP_BUILDDIR}/mibs/snmpconfig/snmpd.conf
+mkdir -p ${XORP_TOP_BUILDDIR}/mibs/snmpconfig/
+cp ${XORP_TOP_SRCDIR}/mibs/snmpconfig/snmpd.preconf ${NEW_SNMPD_CONF}
+echo "dlmod xorp_if_mib_module "					\
+     "${XORP_TOP_BUILDDIR}/mibs/xorp_if_mib_module.so"			\
+	>> ${NEW_SNMPD_CONF} 
