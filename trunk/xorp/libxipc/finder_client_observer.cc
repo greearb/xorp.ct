f03130a577210954252cd51869566563e404f740
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2003-06-20 01:27:17 +0000

    gcc-3.2 compilation fix: add #include "libxorp/xorp.h"
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@1566 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxipc/finder_client_observer.cc
+++ trunk/xorp/libxipc/finder_client_observer.cc
@@ -12,9 +12,12 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/devnotes/template.cc,v 1.2 2003/01/16 19:08:48 mjh Exp $"
+#ident "$XORP: xorp/libxipc/finder_client_observer.cc,v 1.1 2003/06/19 19:20:07 hodson Exp $"
 
 #include <string>
+
+#include "libxorp/xorp.h"
+
 #include "finder_client_observer.hh"
 
 FinderClientObserver::~FinderClientObserver()
