7b315818afcff3f5241be671b7716a868a7cede7
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2003-07-25 02:10:38 +0000

    sumary -> summary.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@1770 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/tools/xorpsh_print_routes.cc
+++ trunk/xorp/bgp/tools/xorpsh_print_routes.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/tools/xorpsh_print_routes.cc,v 1.1 2003/02/24 12:53:47 mjh Exp $"
+#ident "$XORP: xorp/bgp/tools/xorpsh_print_routes.cc,v 1.2 2003/03/10 23:20:10 hodson Exp $"
 
 #include "print_routes.hh"
 #include "bgp/aspath.hh"
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
 	return -1;
     }
     if (argc == 5) {
-	if (strcmp(argv[4], "sumary")==0) {
+	if (strcmp(argv[4], "summary")==0) {
 	    verbose = PrintRoutes::SUMMARY;
 	} else if (strcmp(argv[4], "detail")==0) {
 	    verbose = PrintRoutes::DETAIL;
