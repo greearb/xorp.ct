ad54e292e9f898ba9d4194d7b4af8719bde2b124
hodson <hodson@26435e26-4b25-483a-942d-477236992e84>
2003-08-04 23:46:44 +0000

    Add output process tester.  This test needs cleaning up and wants a test for the table request output processing, but it's a start and has revealed quite a few gotchas already.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@1823 26435e26-4b25-483a-942d-477236992e84

---
+++ trunk/xorp/rip/test_outputs.cc
@@ -0,0 +1,564 @@
+// -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*-
+
+// Copyright (c) 2001-2003 International Computer Science Institute
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software")
+// to deal in the Software without restriction, subject to the conditions
+// listed in the XORP LICENSE file. These conditions include: you must
+// preserve this copyright notice, and you cannot mention the copyright
+// holders in advertising related to the Software without their permission.
+// The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+// notice is a summary of the XORP LICENSE file; the license in that file is
+// legally binding.
+
+#ident "$XORP: xorp/rip/test_update_queue.cc,v 1.3 2003/08/01 04:08:13 hodson Exp $"
+
+#include <set>
+
+#include "rip_module.h"
+
+#include "libxorp/xlog.h"
+
+#include "libxorp/c_format.hh"
+#include "libxorp/eventloop.hh"
+#include "libxorp/ipv4.hh"
+#include "libxorp/ipv4net.hh"
+
+#include "auth.hh"
+#include "output_updates.hh"
+#include "port.hh"
+#include "peer.hh"
+#include "packet_queue.hh"
+#include "route_db.hh"
+#include "system.hh"
+#include "update_queue.hh"
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This test comprises of a RIP system with 2 ports injecting routes.  One of
+// the ports has an associated OutputUpdates instance that generates packets
+// containing the updates.
+//
+//			   +---	System ----+
+//	       		TestPort	OtherPort
+// 	 Routes ------> TestPeer	OtherPeer <------ Routes
+//			   |
+//			   V
+//		    update packets (inspected)
+//
+// We look at the updates in the and compare them against what we'd expect
+// to see.
+//
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Generic Constants
+//
+
+static const char *program_name         = "test_output_update";
+static const char *program_description  = "Test RIP Port processing";
+static const char *program_version_id   = "0.1";
+static const char *program_date         = "August, 2003";
+static const char *program_copyright    = "See file LICENSE.XORP";
+static const char *program_return_value = "0 on success, 1 if test error, "
+					  "2 if internal error";
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Verbosity level control
+//
+
+static bool s_verbose = false;
+bool verbose()                  { return s_verbose; }
+void set_verbose(bool v)        { s_verbose = v; }
+
+#define verbose_log(x...) _verbose_log(__FILE__,__LINE__, x)
+
+#define _verbose_log(file, line, x...)					\
+do {									\
+    if (verbose()) {							\
+	printf("From %s:%d: ", file, line);				\
+	printf(x);							\
+	fflush(stdout);							\
+    }									\
+} while(0)
+
+
+// ----------------------------------------------------------------------------
+// Spoof Port that supports just a single Peer
+//
+
+template <typename A>
+class SpoofPort : public Port<A> {
+public:
+    SpoofPort(PortManagerBase<A>& pm, A addr) : Port<A>(pm)
+    {
+	_peers.push_back(new Peer<A>(*this, addr));
+	verbose_log("Constructing SpoofPort instance\n");
+    }
+    ~SpoofPort()
+    {
+	verbose_log("Destructing SpoofPort instance\n");
+	while (_peers.empty() == false) {
+	    delete _peers.front();
+	    _peers.pop_front();
+	}
+    }
+};
+
+// ----------------------------------------------------------------------------
+// BlockedPortIO
+//
+// This is a port IO class that reports it's always pending so packets
+// build up in the packet queue and never leave...
+
+template <typename A>
+class BlockedPortIO : public PortIOBase<A>
+{
+public:
+    typedef A			Addr;
+    typedef PortIOUserBase<A>	PortIOUser;
+
+public:
+    BlockedPortIO(PortIOUserBase<A>& user)
+	: PortIOBase<A>(user, "if0", "vif0", IPv4("10.0.0.1"))
+    {
+    }
+
+    /**
+     * Called by RIP Port instance.
+     */
+    bool send(const Addr&,
+	      uint16_t,
+	      const uint8_t*,
+	      size_t)
+    {
+	XLOG_FATAL("Called send inappropriately");
+	return true;
+    }
+
+    bool pending() const
+    {
+	return true;
+    }
+
+private:
+};
+
+// ----------------------------------------------------------------------------
+// Type specific helpers
+
+template <typename A>
+struct DefaultPeer {
+    static A get();
+};
+
+template <typename A>
+struct OtherPeer {
+    static A get();
+};
+
+template <>
+IPv4 DefaultPeer<IPv4>::get() { return IPv4("10.0.0.1"); }
+
+template <>
+IPv4 OtherPeer<IPv4>::get() { return IPv4("192.168.0.1"); }
+
+template <>
+IPv6 DefaultPeer<IPv6>::get() { return IPv6("10::1"); }
+
+// ----------------------------------------------------------------------------
+// Spoof Port Manager instance support a single Spoof Port which in turn
+// contains a single Peer.
+//
+
+template <typename A>
+class SpoofPortManager : public PortManagerBase<A> {
+public:
+    SpoofPortManager(System<A>& s)
+	: PortManagerBase<A>(s)
+    {
+	_ports.push_back(new SpoofPort<A>(*this, DefaultPeer<A>::get()));
+	_ports.push_back(new SpoofPort<A>(*this, OtherPeer<A>::get()));
+    }
+
+    ~SpoofPortManager()
+    {
+	while (!_ports.empty()) {
+	    delete _ports.front();
+	    _ports.pop_front();
+	}
+    }
+
+    Port<A>* test_port()
+    {
+	return _ports.front();
+    }
+
+    const Port<A>* test_port() const
+    {
+	return _ports.front();
+    }
+
+    Port<A>* other_port()
+    {
+	return _ports.back();
+    }
+
+    const Port<A>* other_port() const
+    {
+	return _ports.back();
+    }
+
+    Peer<A>* test_peer()
+    {
+	return test_port()->peers().front();
+    }
+
+    const Peer<A>* test_peer() const
+    {
+	return test_port()->peers().front();
+    }
+
+    Peer<A>* other_peer()
+    {
+	return other_port()->peers().front();
+    }
+
+    const Peer<A>* other_peer() const
+    {
+	return other_port()->peers().front();
+    }
+};
+
+
+
+
+//----------------------------------------------------------------------------
+// Horizon checkers
+
+template <typename A>
+class HorizonValidatorBase
+{
+public:
+    HorizonValidatorBase()
+	: _total_routes_seen(0), _test_peer_routes_seen(0)
+    {}
+
+    /* Check an individual response packet is valid */
+    virtual bool valid_response(const RipPacket<A>*	p,
+				const set<IPNet<A> >&	test_peer_nets,
+				const set<IPNet<A> >&	other_peer_nets) = 0;
+
+    /* Final check responses valid */
+    virtual bool valid_in_sum() const = 0;
+
+    inline uint32_t total_routes_seen() const
+    {
+	return _total_routes_seen;
+    }
+
+    inline uint32_t test_peer_routes_seen() const
+    {
+	return _test_peer_routes_seen;
+    }
+
+protected:
+    uint32_t _total_routes_seen;
+    uint32_t _test_peer_routes_seen;
+};
+
+template <typename A>
+class NoHorizonValidator : public HorizonValidatorBase<A> {
+public:
+    bool valid_response(const RipPacket<A>*	p,
+			const set<IPNet<A> >&	test_peer_nets,
+			const set<IPNet<A> >&	other_peer_nets)
+    {
+	for (uint32_t i = 0; i < p->max_entries(); i++) {
+	    _total_routes_seen++;
+
+	    if (test_peer_nets.find(p->route_entry(i)->net()) !=
+		test_peer_nets.end()) {
+		_test_peer_routes_seen++;
+	    } else if (other_peer_nets.find(p->route_entry(i)->net()) !=
+		       other_peer_nets.end()) {
+		// No-op
+	    } else {
+		// Not a test peer net and not an other peer net
+		// ==> it's bogus
+		verbose_log("Failed Processing entry %d / %d %s cost %d\n",
+			    i, p->max_entries(),
+			    p->route_entry(i)->net().str().c_str(),
+			    p->route_entry(i)->metric());
+		return false;
+	    }
+	}
+	return true;
+    }
+
+    bool valid_in_sum() const
+    {
+	verbose_log("total routes seen %d, test peer routes seen = %d\n",
+		    total_routes_seen(), test_peer_routes_seen());
+	return test_peer_routes_seen() * 2 == total_routes_seen();
+    }
+};
+
+template <typename A>
+class SplitHorizonValidator : public HorizonValidatorBase<A> {
+public:
+    bool valid_response(const RipPacket<A>*	p,
+			const set<IPNet<A> >&	/* test_peer_nets */,
+			const set<IPNet<A> >&	other_peer_nets)
+    {
+	for (uint32_t i = 0; i < p->max_entries(); i++) {
+	    _total_routes_seen++;
+	    if (other_peer_nets.find(p->route_entry(i)->net()) ==
+		       other_peer_nets.end()) {
+		verbose_log("Saw own or alien route with split horizon\n");
+		// ==> it's bogus
+		verbose_log("Failed Processing entry %d / %d %s cost %d\n",
+			    i, p->max_entries(),
+			    p->route_entry(i)->net().str().c_str(),
+			    p->route_entry(i)->metric());
+		return false;
+	    }
+	}
+	return true;
+    }
+
+    bool valid_in_sum() const
+    {
+	verbose_log("total routes seen %d, test peer routes seen = %d\n",
+		    total_routes_seen(), test_peer_routes_seen());
+	return test_peer_routes_seen() == 0;
+    }
+};
+
+
+// ----------------------------------------------------------------------------
+// Injected Network state
+
+static void
+make_nets(const IPv4Net& base, uint32_t n, set<IPv4Net>& nets)
+{
+    IPv4Net ipn = base;
+    while (n > 0) {
+	nets.insert(ipn);
+	++ipn;
+	--n;
+    }
+}
+
+template <typename A>
+class UpdateQueueTester
+{
+public:
+    UpdateQueueTester()
+	: _e(), _rip_system(_e), _pm(_rip_system)
+    {
+	_pm.test_port()->constants().set_expiry_secs(10);
+	_pm.test_port()->constants().set_deletion_secs(5);
+	_pm.test_port()->set_advertise_default_route(false);
+
+	_pm.other_port()->constants().set_expiry_secs(10);
+	_pm.other_port()->constants().set_deletion_secs(5);
+
+	// IPv4 specific despite being in a template
+	_pm.test_port()->af_state().set_auth_handler(new NullAuthHandler());
+	_pm.other_port()->af_state().set_auth_handler(new NullAuthHandler());
+
+	_pm.test_port()->set_io_handler(new BlockedPortIO<A>(*_pm.test_port()),
+					true);
+	_pm.other_port()->set_io_handler(
+			new BlockedPortIO<A>(*_pm.other_port()), true);
+    }
+
+    ~UpdateQueueTester()
+    {
+	RouteDB<A>& rdb = _rip_system.route_db();
+	rdb.flush_routes();
+	delete _pm.test_port()->af_state().auth_handler();
+	delete _pm.other_port()->af_state().auth_handler();
+    }
+
+    int run_test(RipHorizon horizon, HorizonValidatorBase<A>& validator)
+    {
+	_pm.test_port()->set_horizon(horizon);
+
+	const uint32_t n_routes = 577;
+	RouteDB<A>&	 rdb = _rip_system.route_db();
+
+	set<IPv4Net> tpeer_nets;
+	set<IPv4Net> opeer_nets;
+
+	make_nets(IPNet<A>(DefaultPeer<A>::get(), 16), n_routes, tpeer_nets);
+	make_nets(IPNet<A>(OtherPeer<A>::get(), 16), n_routes, opeer_nets);
+
+	PacketQueue<A>	 op_out;				// Output out
+	OutputUpdates<A> ou(_e, *_pm.test_port(), op_out, rdb); // Update gen
+
+	verbose_log("Injecting routes from test peer.\n");
+	for (typename set<IPNet<A> >::const_iterator n = tpeer_nets.begin();
+	     n != tpeer_nets.end(); n++) {
+	    RouteEntryOrigin<A>* reo = _pm.test_peer();
+	    if (rdb.update_route(*n, A::ZERO(), 5u, 0u, reo) == false) {
+		verbose_log("Failed to add route for %s\n",
+			    n->str().c_str());
+		return 1;
+	    }
+	}
+
+	verbose_log("Injecting routes from other peer.\n");
+	for (typename set<IPNet<A> >::const_iterator n = opeer_nets.begin();
+	     n != opeer_nets.end(); n++) {
+	    RouteEntryOrigin<A>* reo = _pm.other_peer();
+	    if (rdb.update_route(*n, A::ZERO(), 5u, 0u, reo) == false) {
+		verbose_log("Failed to add route for %s\n",
+			    n->str().c_str());
+		return 1;
+	    }
+	}
+
+	bool timeout = false;
+	XorpTimer tot = _e.set_flag_after_ms(3000, &timeout);
+	ou.run();
+
+	while (ou.running() && timeout == false) {
+	    _e.run();
+	}
+	verbose_log("%d bytes buffered in packet queue.\n",
+		    op_out.buffered_bytes());
+	if (timeout) {
+	    verbose_log("Timed out!\n");
+	    return 1;
+	}
+
+	uint32_t cnt = 0;
+	while (op_out.empty() == false) {
+	    verbose_log("==> Processing packet %d %d bytes remain\n",
+			cnt, op_out.buffered_bytes());
+	    if (validator.valid_response(op_out.head(), tpeer_nets, opeer_nets)
+		== false) {
+		verbose_log("Failed on packet validation.\n");
+		return 1;
+	    }
+	    op_out.pop_head();
+	    cnt++;
+	}
+
+	if (validator.valid_in_sum() == false) {
+	    verbose_log("Not valid in sum.\n");
+	    return 1;
+	}
+
+	return 0;
+    }
+
+protected:
+    EventLoop		_e;
+    System<A>		_rip_system;
+    SpoofPortManager<A> _pm;
+};
+
+
+/**
+ * Print program info to output stream.
+ *
+ * @param stream the output stream the print the program info to.
+ */
+static void
+print_program_info(FILE *stream)
+{
+    fprintf(stream, "Name:          %s\n", program_name);
+    fprintf(stream, "Description:   %s\n", program_description);
+    fprintf(stream, "Version:       %s\n", program_version_id);
+    fprintf(stream, "Date:          %s\n", program_date);
+    fprintf(stream, "Copyright:     %s\n", program_copyright);
+    fprintf(stream, "Return:        %s\n", program_return_value);
+}
+
+/*
+ * Print program usage information to the stderr.
+ *
+ * @param progname the name of the program.
+ */
+static void
+usage(const char* progname)
+{
+    print_program_info(stderr);
+    fprintf(stderr, "usage: %s [-v] [-h]\n", progname);
+    fprintf(stderr, "       -h          : usage (this message)\n");
+    fprintf(stderr, "       -v          : verbose output\n");
+}
+
+int
+main(int argc, char* const argv[])
+{
+    //
+    // Initialize and start xlog
+    //
+    xlog_init(argv[0], NULL);
+    xlog_set_verbose(XLOG_VERBOSE_LOW);         // Least verbose messages
+    // XXX: verbosity of the error messages temporary increased
+    xlog_level_set_verbose(XLOG_LEVEL_ERROR, XLOG_VERBOSE_HIGH);
+    xlog_add_default_output();
+    xlog_start();
+
+    int ch;
+    while ((ch = getopt(argc, argv, "hv")) != -1) {
+        switch (ch) {
+        case 'v':
+            set_verbose(true);
+            break;
+        case 'h':
+        case '?':
+        default:
+            usage(argv[0]);
+            xlog_stop();
+            xlog_exit();
+            if (ch == 'h')
+                return (0);
+            else
+                return (1);
+        }
+    }
+    argc -= optind;
+    argv += optind;
+
+    int rval = 0;
+    XorpUnexpectedHandler x(xorp_unexpected_handler);
+    try {
+
+	{
+	    UpdateQueueTester<IPv4> uqt;
+	    SplitHorizonValidator<IPv4> shv;
+	    rval += uqt.run_test(SPLIT, shv);
+	}
+#if 0
+	{
+	    UpdateQueueTester<IPv4> uqt;
+	    rval += uqt.run_test(SPLIT_POISON_REVERSE);
+	}
+#endif
+	{
+	    UpdateQueueTester<IPv4> uqt;
+	    NoHorizonValidator<IPv4> nohv;
+	    rval += uqt.run_test(NONE, nohv);
+	}
+    } catch (...) {
+        // Internal error
+        xorp_print_standard_exceptions();
+        rval = 2;
+    }
+
+    //
+    // Gracefully stop and exit xlog
+    //
+    xlog_stop();
+    xlog_exit();
+
+    return rval;
+}
