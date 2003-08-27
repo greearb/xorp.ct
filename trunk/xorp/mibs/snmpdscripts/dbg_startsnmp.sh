0828a5f8ae72588326be71498ece4c7f0d46c51a
jcardona <jcardona@26435e26-4b25-483a-942d-477236992e84>
2003-08-27 00:42:31 +0000

    Setting LD_LIBRARY_PATH
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@1973 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/mibs/snmpdscripts/dbg_startsnmp.sh
+++ trunk/xorp/mibs/snmpdscripts/dbg_startsnmp.sh
@@ -9,6 +9,8 @@ echo "dlmod xorp_if_mib_module  ${MIBS_DIR}/xorp_if_mib_module.so" > snmpd.conf
 echo "trap2sink localhost xorp ${SNMPTRAPD_PORT}"                 >> snmpd.conf
 echo "rwcommunity ${SNMP_DBG_COMMUNITY}"                          >> snmpd.conf
 
+export LD_LIBRARY_PATH=${MIBS_DIR}
+
 snmptrapd -o ${SNMPTRAPD_LOG} localhost:${SNMPTRAPD_PORT}
 snmpd -f -r -Dxorp_if_mib_module,dlmod,usmUser,bgp4_mib_1657 -C -c ${PWD}/snmpd.conf -l ${SNMPD_LOG} localhost:${SNMPD_PORT}
 
