242f403976344cee7e6a46679306189cda151ba1
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2003-09-12 22:39:09 +0000

    * Refactor the /proc information fetch mechanism (mostly the IPv4   case):   - First we call the ioctl()'s pull_config() to get most     of the interface information (including the IP aliases information).   - Then, in case of IPv4 we get the list of interfaces from     /proc/net/dev . This info would include the tunnel interface names     that is not returned by the ioctl(SIOCGIFCONF) method.     Then, for every interface name that is not on the interface tree     (i.e., that ioctl(SIOCGIFCONF) doesn't know about)     we call IfConfigIoctl::parse_buffer_ifreq() that will call     various ioctl()'s to fill-in the missing info for that interface     (MTU, IP address, etc).   This is the model used by Linux's ifconfig (from net-tools), and   seems to be the only workaround of collecting all the missing   interface information.
    * Few tweaks to the IPv6 case, where we need to use /proc/net/if_inet6
      to get the IP addresses, etc (similar to Linux's ifconfig).
      We still don't obtain the endpoint address in case of IPv6 point-to-point
      interfaces, because there is no information in ifconfig's source code
      or the Linux kernel code how to do that. Could be that
      RedHat-7.x (Linux kernel 2.4.x) doesn't support IPv6 tunnels?
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@2165 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/ifconfig_get_proc_linux.cc
+++ trunk/xorp/fea/ifconfig_get_proc_linux.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/ifconfig_get_proc_linux.cc,v 1.2 2003/08/28 14:42:19 pavlin Exp $"
+#ident "$XORP: xorp/fea/ifconfig_get_proc_linux.cc,v 1.3 2003/09/02 16:38:11 fred Exp $"
 
 #define PROC_LINUX_FILE_V4 "/proc/net/dev"
 #define PROC_LINUX_FILE_V6 "/proc/net/if_inet6"
@@ -40,7 +40,8 @@
 //
 // Get information about network interfaces from the underlying system.
 //
-// The mechanism to obtain the information is Linux's /proc/net/dev 
+// The mechanism to obtain the information is Linux's /proc/net/dev (for IPv4)
+// or /proc/net/if_inet6 (for IPv6).
 //
 
 IfConfigGetProcLinux::IfConfigGetProcLinux(IfConfig& ifc)
@@ -59,12 +60,19 @@ IfConfigGetProcLinux::~IfConfigGetProcLinux()
 int
 IfConfigGetProcLinux::start()
 {
+    // XXX: this method relies on the ioctl() method
+    if (ifc().ifc_get_ioctl().start() < 0)
+	return (XORP_ERROR);
+    
     return (XORP_OK);
 }
 
 int
 IfConfigGetProcLinux::stop()
 {
+    // XXX: this method relies on the ioctl() method
+    ifc().ifc_get_ioctl().stop();
+    
     return (XORP_OK);
 }
 
@@ -84,28 +92,33 @@ IfConfigGetProcLinux::read_config(IfTree& )
 #else // HAVE_PROC_LINUX
 
 static char *get_name(char *name, char *p);
-static bool proc_read_ifconf_linux(IfTree& iftree, int family);
-static bool if_fetch_linux_v4(IfTree& it);
+static bool proc_read_ifconf_linux(IfConfig& ifc, IfTree& iftree, int family);
+static bool if_fetch_linux_v4(IfConfig& ifc, IfTree& it);
 
 #ifdef HAVE_IPV6
-static bool if_fetch_linux_v6(IfTree& it);
+static bool if_fetch_linux_v6(IfConfig& ifc, IfTree& it);
 #endif // HAVE_IPV6
 
 bool
 IfConfigGetProcLinux::read_config(IfTree& iftree)
 {
-    if (proc_read_ifconf_linux(iftree, AF_INET) != true)
+    // XXX: this method relies on the ioctl() method
+    ifc().ifc_get_ioctl().pull_config(iftree);
+    
+    //
+    // The IPv4 information
+    //
+    if (proc_read_ifconf_linux(ifc(), iftree, AF_INET) != true)
 	return false;
-
+    
 #ifdef HAVE_IPV6
     //
     // The IPv6 information
     //
-    if (proc_read_ifconf_linux(iftree, AF_INET6) != true)
+    if (proc_read_ifconf_linux(ifc(), iftree, AF_INET6) != true)
 	return false;
-    //
 #endif // HAVE_IPV6
-
+    
     return true;
 }
 
@@ -114,19 +127,22 @@ IfConfigGetProcLinux::read_config(IfTree& iftree)
 //
 
 static bool
-proc_read_ifconf_linux(IfTree& iftree, int family)
+proc_read_ifconf_linux(IfConfig& ifc, IfTree& iftree, int family)
 {
     switch (family) {
     case AF_INET:
-	if_fetch_linux_v4(iftree);
+	//
+	// The IPv4 information
+	//
+	if_fetch_linux_v4(ifc, iftree);
 	break;
 
 #ifdef HAVE_IPV6
-    //
-    // IPv6 information
-    //
+	//
+	// The IPv6 information
+	//
     case AF_INET6:
-	if_fetch_linux_v6(iftree);
+	if_fetch_linux_v6(ifc, iftree);
 	break;
 #endif // HAVE_IPV6
 
@@ -166,319 +182,279 @@ static char *get_name(char *name, char *p)
 }
 
 // 
-// IPv4 Interface info fetch derived from ifconfig_parse_ifreq.cc
+// IPv4 interface info fetch
 //
-bool if_fetch_linux_v4(IfTree& it)
+// We get only the list of interfaces from /proc/net/dev.
+// Then, for each interface in that list we check if it is already
+// in the IfTree that is partially filled-in with
+// IfConfigGetIoctl::read_config().
+// If the interface is in that list, then we ignore it. If the interface is
+// NOT in that list, then we call IfConfigGet::parse_buffer_ifreq()
+// to get the rest of the information about it (such as the interface hardware
+// address, MTU, etc) by calling various ioctl()'s.
+//
+// Why do we need to do this in such complicated way?
+// Because on Linux ioctl(SIOCGIFCONF) doesn't return information about
+// tunnel interfaces, and because /proc/net/dev doesn't return information
+// about IP address aliases (e.g., interface names like eth0:0).
+// Hence, we have to follow the model used by Linux's ifconfig
+// (as part of the net-tools) collection to merge the information from
+// both mechanisms. Enjoy!
+//
+static bool
+if_fetch_linux_v4(IfConfig& ifc, IfTree& it)
 {
-    struct ifreq ifr;
-    int soc;
     FILE *fh;
     char buf[512];
-    int err;
     char *s, ifname[IFNAMSIZ];
 
-    soc = socket(AF_INET, SOCK_DGRAM, 0);
-    if (soc < 0) {
-	XLOG_FATAL("Could not initialize IPv4 ioctl() socket");
+    fh = fopen(PROC_LINUX_FILE_V4, "r");
+    if (fh == NULL) {
+	XLOG_FATAL("Cannot open file %s for reading: %s",
+		   PROC_LINUX_FILE_V4, strerror(errno)); 
 	return false;
     }
-
-    fh = fopen(PROC_LINUX_FILE_V4, "r");
-    if (!fh) {
-	XLOG_FATAL("Warning: cannot open %s (%s).",
-	PROC_LINUX_FILE_V4, strerror(errno)); 
-	}	
-    fgets(buf, sizeof buf, fh);	/* lose starting 2 lines of comments  */
-    s = get_name(ifname, buf);    
+    fgets(buf, sizeof buf, fh);		// lose starting 2 lines of comments
+    s = get_name(ifname, buf);
     if (strcmp(ifname, "Inter-|") != 0) {
 	XLOG_ERROR("%s: improper file contents", PROC_LINUX_FILE_V4);
+	fclose(fh);
 	return false;
     }
     fgets(buf, sizeof buf, fh);
     s = get_name(ifname, buf);    
     if (strcmp(ifname, "face") != 0) {
 	XLOG_ERROR("%s: improper file contents", PROC_LINUX_FILE_V4);
+	fclose(fh);
 	return false;
     }
-
-    err = 0;
-    while (fgets(buf, sizeof buf, fh)) {
-	s = get_name(ifname, buf);    
-
-	it.add_if(ifname); // retrieve node, create if necessary 
-	debug_msg("interface: %s\n",ifname);
-
+    
+    while (fgets(buf, sizeof buf, fh) != NULL) {
+	struct ifreq ifreq;
+	
 	//
-	// Fill in tree interface 
+	// Get the interface name
 	//
-	IfTreeInterface& fi = it.get_if(ifname)->second;
+	s = get_name(ifname, buf);
+	if (s == NULL) {
+	    XLOG_ERROR("%s: cannot get interface name for line %s",
+		       PROC_LINUX_FILE_V4, buf);
+	    continue;
+	}
+	
+	if (it.get_if(ifname) != it.ifs().end()) {
+	    // Already have this interface. Ignore it.
+	    continue;
+	}
+	
+	memset(&ifreq, 0, sizeof(ifreq));
+	strncpy(ifreq.ifr_name, ifname, sizeof(ifreq.ifr_name));
+	ifc.ifc_get_ioctl().parse_buffer_ifreq(
+	    it, AF_INET,
+	    reinterpret_cast<const uint8_t *>(&ifreq), sizeof(ifreq));
+    }
+    if (ferror(fh)) {
+	XLOG_ERROR("%s read failed: %s", PROC_LINUX_FILE_V4, strerror(errno));
+    }
+    
+    // Clean up
+    fclose(fh);
+    
+    return true;
+}
 
-	// MAC address
-	ether_addr ea;
-	strcpy(ifr.ifr_name, ifname);
-	if (ioctl(soc, SIOCGIFHWADDR, &ifr) < 0) 
-	    memset(&ea, 0, 32);
-	else 
-	    memcpy(&ea, ifr.ifr_hwaddr.sa_data, 8);
-	fi.set_mac(EtherMac(ea));
-	const Mac& mac = fi.mac();
-	debug_msg("MAC address: %s\n",mac.str().c_str());
-
-	// MTU 
+// 
+// IPv6 interface info fetch
+//
+// Unlike the IPv4 case (see the comments about if_fetch_linux_v4()),
+// we get the list of interfaces and all the IPv6 address information
+// from /proc/net/if_inet6.
+// Then, we use ioctl()'s to obtain information such as interface
+// hardware address, MTU, etc.
+//
+// The reason that we do NOT call IfConfigGet::parse_buffer_ifreq()
+// to fill-in the missing information is because in Linux the in6_ifreq
+// structure has completely different format from the IPv4 equivalent
+// of "struct ifreq" (i.e., there is no in6_ifreq.ifr_name field), and
+// that structure is completely different from the BSD structure with
+// the same name. No comment.
+//
+#ifdef HAVE_IPV6
+static bool
+if_fetch_linux_v6(IfConfig& ifc, IfTree& it)
+{
+    FILE *fh;
+    char devname[IFNAMSIZ+20+1];
+    char addr6p[8][5], addr6[40];
+    int plen, scope, dad_status, if_idx;
+    struct ifreq;
+    int sock;
+    
+    fh = fopen(PROC_LINUX_FILE_V6, "r");
+    if (fh == NULL) {
+	XLOG_FATAL("Cannot open file %s for reading: %s",
+		   PROC_LINUX_FILE_V6, strerror(errno)); 
+	return false;
+    }
+    
+    sock = socket(AF_INET6, SOCK_DGRAM, 0);
+    if (sock < 0) {
+	XLOG_FATAL("Could not initialize ioctl() socket");
+	fclose(fh);
+	return false;
+    }
+    
+    while (fscanf(fh, "%4s%4s%4s%4s%4s%4s%4s%4s %02x %02x %02x %02x %20s\n",
+		  addr6p[0], addr6p[1], addr6p[2], addr6p[3],
+		  addr6p[4], addr6p[5], addr6p[6], addr6p[7],
+		  &if_idx, &plen, &scope, &dad_status, devname) != EOF) {
+	u_short if_index = 0;
+	string if_name, alias_if_name;
+	
+	//
+	// Get the interface name
+	//
+	char tmp_if_name[IFNAMSIZ+1];
+	strncpy(tmp_if_name, devname, sizeof(tmp_if_name));
+	char *cptr;
+	if ( (cptr = strchr(tmp_if_name, ':')) != NULL) {
+	    // Replace colon with null. Needed because in Solaris and Linux
+	    // the interface name changes for aliases.
+	    *cptr = '\0';
+	}
+	if_name = string(devname);
+	alias_if_name = string(tmp_if_name);
+	debug_msg("interface: %s\n", if_name.c_str());
+	debug_msg("alias interface: %s\n", alias_if_name.c_str());
+	
+	//
+	// Get the physical interface index
+	//
+	if_index = if_idx;
+	if (if_index == 0) {
+	    XLOG_FATAL("Could not find physical interface index "
+		       "for interface %s",
+		       if_name.c_str());
+	}
+	debug_msg("interface index: %d\n", if_index);
+	
+	//
+	// Add the interface (if a new one)
+	//
+	ifc.map_ifindex(if_index, alias_if_name);
+	it.add_if(alias_if_name);
+	IfTreeInterface& fi = it.get_if(alias_if_name)->second;
+	
+	//
+	// Get the MAC address
+	//
+	do {
+#ifdef SIOCGIFHWADDR
+	    memset(&ifreq, 0, sizeof(ifreq));
+	    strncpy(ifreq.ifr_name, if_name.c_str(), sizeof(ifreq.ifr_name));
+	    if (ioctl(sock, SIOCGIFHWADDR, &ifreq) < 0) {
+		XLOG_ERROR("ioctl(SIOCGIFHWADDR) for interface %s failed: %s",
+			   if_name.c_str(), strerror(errno));
+	    } else {
+		ether_addr ea;
+		memcpy(&ea, ifreq.ifr_hwaddr.sa_data, sizeof(ea));
+		fi.set_mac(EtherMac(ea));
+		break;
+	    }
+#endif // SIOCGIFHWADDR
+	    
+	    break;
+	} while (false);
+	debug_msg("MAC address: %s\n", fi.mac().str().c_str());
+	
+	//
+	// Get the MTU
+	//
 	int mtu = 0;
-	strcpy(ifr.ifr_name, ifname);
-	if (ioctl(soc, SIOCGIFMTU, &ifr) < 0)
-	    mtu = 0;
-	else
-	    mtu = ifr.ifr_mtu;
+	memset(&ifreq, 0, sizeof(ifreq));
+	strncpy(ifreq.ifr_name, if_name.c_str(), sizeof(ifreq.ifr_name));
+	if (ioctl(sock, SIOCGIFMTU, &ifreq) < 0) {
+	    XLOG_ERROR("ioctl(SIOCGIFMTU) for interface %s failed: %s",
+		       if_name.c_str(), strerror(errno));
+	} else {
+	    mtu = ifreq.ifr_mtu;
+	}
 	fi.set_mtu(mtu);
-	debug_msg("MTU: %d\n",fi.mtu());
-
-	// fi.set_enabled(flags & IFF_UP);
+	debug_msg("MTU: %d\n", fi.mtu());
+	
+	//
+	// Get the flags
+	//
 	int flags = 0;
-	strcpy(ifr.ifr_name, ifname);
-	if (ioctl(soc, SIOCGIFFLAGS, &ifr) < 0)
-	    return false;
-	flags = ifr.ifr_flags; 
+	// int flags6 = dad_status;
+	memset(&ifreq, 0, sizeof(ifreq));
+	strncpy(ifreq.ifr_name, if_name.c_str(), sizeof(ifreq.ifr_name));
+	if (ioctl(sock, SIOCGIFFLAGS, &ifreq) < 0) {
+	    XLOG_ERROR("ioctl(SIOCGIFFLAGS) for interface %s failed: %s",
+		       if_name.c_str(), strerror(errno));
+	} else {
+	    flags = ifreq.ifr_flags;
+	}
 	fi.set_enabled(flags & IFF_UP);
 	debug_msg("enabled: %s\n", fi.enabled() ? "true" : "false");
-
+	
 	// XXX: vifname == ifname on this platform
-	fi.add_vif(ifname);
-	IfTreeVif& fv = fi.get_vif(ifname)->second;
-
-	// Interface index
-	int idx = 0;
-	strcpy(ifr.ifr_name, ifname);
-	if (ioctl(soc, SIOCGIFINDEX, &ifr) < 0)
-	    idx = 0;
-	else
-	    idx = ifr.ifr_ifindex;
-	fv.set_pif_index(idx);
-	debug_msg("interface index: %d\n",fv.pif_index());
-
-	// set vif flags
+	fi.add_vif(alias_if_name);
+	IfTreeVif& fv = fi.get_vif(alias_if_name)->second;
+	
+	//
+	// Set the physical interface index for the vif
+	//
+	fv.set_pif_index(if_index);
+	
+	//
+	// Set the vif flags
+	//
 	fv.set_enabled(fi.enabled() && (flags & IFF_UP));
 	fv.set_broadcast(flags & IFF_BROADCAST);
 	fv.set_loopback(flags & IFF_LOOPBACK);
 	fv.set_point_to_point(flags & IFF_POINTOPOINT);
 	fv.set_multicast(flags & IFF_MULTICAST);
-	debug_msg("vif enabled: %s\n",        
-	    fv.enabled() ? "true" : "false");
-	debug_msg("vif broadcast: %s\n",      
-	    fv.broadcast() ? "true" : "false");
-	debug_msg("vif loopback: %s\n",       
-	    fv.loopback() ? "true" : "false");
-	debug_msg("vif point_to_point: %s\n", 
-	    fv.point_to_point() ? "true" : "false");
-	debug_msg("vif multicast: %s\n",      
-	    fv.multicast() ? "true" : "false");
-
-	//
-	// Get the IP address, netmask, broadcast address, P2P destination
-	//
-	// The default values
-	IPvX lcl_addr = IPvX::ZERO(AF_INET);
-	IPvX subnet_mask = IPvX::ZERO(AF_INET);
-	IPvX broadcast_addr = IPvX::ZERO(AF_INET);
-	IPvX peer_addr = IPvX::ZERO(AF_INET);
-	bool has_broadcast_addr = false;
-	bool has_peer_addr = false;
-
-	// Get the IP address
-	strcpy(ifr.ifr_name, ifname);
-	ifr.ifr_addr.sa_family = AF_INET;
-	if (ioctl(soc, SIOCGIFADDR, &ifr) == 0) {
-	    lcl_addr.copy_in(ifr.ifr_addr);
-	    lcl_addr = kernel_ipvx_adjust(lcl_addr);
-	}
-	debug_msg("IP address: %s\n", lcl_addr.str().c_str());
-
-	// Get the netmask
-	strcpy(ifr.ifr_name, ifname);
-	if (ioctl(soc, SIOCGIFNETMASK, &ifr) >= 0)
-	    subnet_mask.copy_in(ifr.ifr_addr);
-	debug_msg("IP netmask: %s\n", subnet_mask.str().c_str());
-
-	// Get the broadcast address	
-	if (fv.broadcast()) {
-	    strcpy(ifr.ifr_name, ifname);
-	    if (ioctl(soc, SIOCGIFBRDADDR, &ifr) >= 0) {
-		broadcast_addr.copy_in(ifr.ifr_addr);
-		has_broadcast_addr = true;
-	    }
-	    debug_msg("Broadcast address: %s\n", broadcast_addr.str().c_str());
-	}
-
-	// Get the p2p address
-	if (fv.point_to_point()) {
-	    strcpy(ifr.ifr_name, ifname);
-	    if (ioctl(soc, SIOCGIFDSTADDR, &ifr) >= 0) {
-		peer_addr.copy_in(ifr.ifr_dstaddr);
-		has_peer_addr = true;
-	    }
-	}
-	debug_msg("Peer address: %s\n", peer_addr.str().c_str());
-
-	debug_msg("\n"); // put empty line between interfaces
-
-	// Add the address
-	fv.add_addr(lcl_addr.get_ipv4());
-	IfTreeAddr4& fa = fv.get_addr(lcl_addr.get_ipv4())->second;
-	fa.set_enabled(fv.enabled() && (flags & IFF_UP));
-	fa.set_broadcast(fv.broadcast()
-			 && (flags & IFF_BROADCAST)
-			 && has_broadcast_addr);
-	fa.set_loopback(fv.loopback() && (flags & IFF_LOOPBACK));
-	fa.set_point_to_point(fv.point_to_point()
-			      && (flags & IFF_POINTOPOINT)
-			      && has_peer_addr);
-	fa.set_multicast(fv.multicast() && (flags & IFF_MULTICAST));
+	debug_msg("vif enabled: %s\n", fv.enabled() ? "true" : "false");
+	debug_msg("vif broadcast: %s\n", fv.broadcast() ? "true" : "false");
+	debug_msg("vif loopback: %s\n", fv.loopback() ? "true" : "false");
+	debug_msg("vif point_to_point: %s\n", fv.point_to_point() ? "true"
+		  : "false");
+	debug_msg("vif multicast: %s\n", fv.multicast() ? "true" : "false");
 	
-	fa.set_prefix(subnet_mask.prefix_length());
-	if (fa.broadcast())
-	    fa.set_bcast(broadcast_addr.get_ipv4());
-	if (fa.point_to_point())
-	    fa.set_endpoint(peer_addr.get_ipv4());
-    }
-    if (ferror(fh)) {
-	XLOG_ERROR("%s read failed: %s", 
-	    PROC_LINUX_FILE_V4, strerror(errno));
-    }
-
-    // Clean up, shut down socket
-    fclose(fh);
-    close(soc);
-
-    return true;
-}
-
-// 
-// IPv6 Interface info fetch derived from ifconfig_parse_ifreq.cc
-//
-#ifdef HAVE_IPV6
-bool if_fetch_linux_v6(IfTree& it)
-{
-    FILE *fh;
-    int soc;
-    char * ifname;
-    struct ifreq ifr;
-    char addr6p[8][5], addr6[40];
-    int plen, scope, dad_status, if_idx;
-
-    fh = fopen(PROC_LINUX_FILE_V6, "r");
-    if (!fh) {
-	XLOG_FATAL("Warning: cannot open %s (%s).",
-	    PROC_LINUX_FILE_V6, strerror(errno)); 
-	return false;
-	}	
-
-    soc = socket(AF_INET6, SOCK_DGRAM, 0);
-    if (soc < 0) {
-	XLOG_FATAL("Could not initialize ioctl() socket");
-	return false;
-    }
-
-    //while (fscanf(fh, "%s\n", ifname) != EOF) {
-    while (fscanf(fh, "%4s%4s%4s%4s%4s%4s%4s%4s %02x %02x %02x %02x %20s\n",
-		  addr6p[0], addr6p[1], addr6p[2], addr6p[3],
-		  addr6p[4], addr6p[5], addr6p[6], addr6p[7],
-		  &if_idx, &plen, &scope, &dad_status, ifname) != EOF) {
-	it.add_if(ifname); // retrieve node, create if necessary 
-	debug_msg("interface: %s\n",ifname);
-
 	//
-	// Fill in tree interface 
-	//
-	IfTreeInterface& fi = it.get_if(ifname)->second;
-
-	// MAC address
-	ether_addr ea;
-	strcpy(ifr.ifr_name, ifname);
-	if (ioctl(soc, SIOCGIFHWADDR, &ifr) < 0) 
-	    memset(&ea, 0, 32);
-	else 
-	    memcpy(&ea, ifr.ifr_hwaddr.sa_data, 8);
-	fi.set_mac(EtherMac(ea));
-	const Mac& mac = fi.mac();
-	debug_msg("MAC address: %s\n",mac.str().c_str());
-
-	// MTU 
-	int mtu = 0;
-	strcpy(ifr.ifr_name, ifname);
-	if (ioctl(soc, SIOCGIFMTU, &ifr) < 0)
-	    mtu = 0;
-	else
-	    mtu = ifr.ifr_mtu;
-	fi.set_mtu(mtu);
-	debug_msg("MTU: %d\n",fi.mtu());
-
-	// fi.set_enabled(flags & IFF_UP);
-	int flags = 0;
-	strcpy(ifr.ifr_name, ifname);
-	if (ioctl(soc, SIOCGIFFLAGS, &ifr) < 0)
-	    return false;
-	flags = ifr.ifr_flags; 
-	fi.set_enabled(flags & IFF_UP);
-	debug_msg("enabled: %s\n", fi.enabled() ? "true" : "false");
-
-	// XXX: vifname == ifname on this platform
-	fi.add_vif(ifname);
-	IfTreeVif& fv = fi.get_vif(ifname)->second;
-
-	// Interface index
-	fv.set_pif_index(if_idx);
-	debug_msg("interface index: %d\n",fv.pif_index());
-
-	// set vif flags
-	fv.set_enabled(fi.enabled() && (flags & IFF_UP));
-	fv.set_broadcast(false);
-	fv.set_loopback(flags & IFF_LOOPBACK);
-	fv.set_point_to_point(flags & IFF_POINTOPOINT);
-	fv.set_multicast(flags & IFF_MULTICAST);
-	debug_msg("vif enabled: %s\n",        
-	    fv.enabled() ? "true" : "false");
-	debug_msg("vif broadcast: %s\n",      
-	    fv.broadcast() ? "true" : "false");
-	debug_msg("vif loopback: %s\n",       
-	    fv.loopback() ? "true" : "false");
-	debug_msg("vif point_to_point: %s\n", 
-	    fv.point_to_point() ? "true" : "false");
-	debug_msg("vif multicast: %s\n",      
-	    fv.multicast() ? "true" : "false");
-
-	//
-	// Get the IP address, netmask, broadcast address, P2P destination
+	// Get the IP address, netmask, P2P destination
 	//
 	// The default values
-	IPvX lcl_addr = IPvX::ZERO(AF_INET6);
-	IPvX subnet_mask = IPvX::ZERO(AF_INET6);
-	IPvX peer_addr = IPvX::ZERO(AF_INET6);
+	IPv6 lcl_addr;
+	IPv6 subnet_mask;
+	IPv6 peer_addr;
 	bool has_peer_addr = false;
-
+	
 	// Get the IP address
 	sprintf(addr6, "%s:%s:%s:%s:%s:%s:%s:%s",
 		addr6p[0], addr6p[1], addr6p[2], addr6p[3],
 		addr6p[4], addr6p[5], addr6p[6], addr6p[7]);
-	lcl_addr = IPvX(addr6);
+	lcl_addr = IPv6(addr6);
+	lcl_addr = kernel_ipv6_adjust(lcl_addr);
 	debug_msg("IP address: %s\n", lcl_addr.str().c_str());
-
+	
 	// Get the netmask
-	subnet_mask = IPvX::make_prefix(AF_INET6, plen);
+	subnet_mask = IPv6::make_prefix(plen);
 	debug_msg("IP netmask: %s\n", subnet_mask.str().c_str());
-
+	
 	// Get the p2p address
 	if (fv.point_to_point()) {
 	    XLOG_FATAL("IPv6 point-to-point address unimplemented");
 	    has_peer_addr = true;
-	    // debug_msg("Peer address: %s\n", peer_addr.str().c_str());
+	    debug_msg("Peer address: %s\n", peer_addr.str().c_str());
 	}
-
-	debug_msg("\n"); // put empty line between interfaces
-
+	
+	debug_msg("\n");	// put an empty line between interfaces
+	
 	// Add the address
-	fv.add_addr(lcl_addr.get_ipv6());
-	IfTreeAddr6& fa = fv.get_addr(lcl_addr.get_ipv6())->second;
+	fv.add_addr(lcl_addr);
+	IfTreeAddr6& fa = fv.get_addr(lcl_addr)->second;
 	fa.set_enabled(fv.enabled() && (flags & IFF_UP));
 	fa.set_loopback(fv.loopback() && (flags & IFF_LOOPBACK));
 	fa.set_point_to_point(fv.point_to_point() && (flags & IFF_POINTOPOINT));
@@ -486,16 +462,16 @@ bool if_fetch_linux_v6(IfTree& it)
 	
 	fa.set_prefix(subnet_mask.prefix_length());
 	if (fa.point_to_point())
-	    fa.set_endpoint(peer_addr.get_ipv6());
+	    fa.set_endpoint(peer_addr);
     }
+    
     if (ferror(fh)) {
-	XLOG_ERROR("%s read failed: %s", 
-	    PROC_LINUX_FILE_V6, strerror(errno));
+	XLOG_ERROR("%s read failed: %s", PROC_LINUX_FILE_V6, strerror(errno));
     }
-
+    
+    close(sock);
     fclose(fh);
-    close(soc);
-
+    
     return true;
 }
 #endif // HAVE_IPV6
