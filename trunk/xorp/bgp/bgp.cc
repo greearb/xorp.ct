5a4b30c31d81b65be2f72f7f973b9eef55c6ec4f
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2003-10-01 02:48:08 +0000

    Be more forgiving about the number of outstanding timers there can be.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@2375 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/bgp.cc
+++ trunk/xorp/bgp/bgp.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/bgp.cc,v 1.11 2003/09/16 21:00:25 hodson Exp $"
+#ident "$XORP: xorp/bgp/bgp.cc,v 1.12 2003/09/30 03:07:55 pavlin Exp $"
 
 // #define DEBUG_MAXIMUM_DELAY
 // #define DEBUG_LOGGING
@@ -78,12 +78,13 @@ BGPMain::~BGPMain()
     _peerlist->all_stop();
 
     /*
-    ** NOTE: We allow one timer to be pending. The timer is in the xrl_router.
+    ** NOTE: We allow four timers to be pending. The timers are in the
+    ** xrl_router.
     */
     debug_msg("-------------------------------------------\n");
     debug_msg("Waiting for all peers to go to idle\n");
     while (_peerlist->not_all_idle()
-	   || eventloop().timer_list_length() > 1) {
+	   || eventloop().timer_list_length() > 4) {
 	    eventloop().run();
 	    XLOG_INFO("EVENT: peerlist %d timers %u",
 		      _peerlist->not_all_idle(),
