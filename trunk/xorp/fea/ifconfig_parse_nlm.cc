3b4e89f836f9e2bfbff180738a0410b5e2ea7d8a
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2003-10-02 16:55:16 +0000

    Change the type of variable "flags" from int to unsigned int.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@2395 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/ifconfig_parse_nlm.cc
+++ trunk/xorp/fea/ifconfig_parse_nlm.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/ifconfig_parse_nlm.cc,v 1.3 2003/09/30 18:27:02 pavlin Exp $"
+#ident "$XORP: xorp/fea/ifconfig_parse_nlm.cc,v 1.4 2003/10/01 22:49:47 pavlin Exp $"
 
 
 #include "fea_module.h"
@@ -229,7 +229,7 @@ nlm_newlink_to_fea_cfg(IfConfig& ifc, IfTree& it,
     //
     // Get the flags
     //
-    int flags = ifinfomsg->ifi_flags;
+    unsigned int flags = ifinfomsg->ifi_flags;
     fi.set_enabled(flags & IFF_UP);
     debug_msg("enabled: %s\n", fi.enabled() ? "true" : "false");
     
