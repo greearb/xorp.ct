e723bfc030177640a5cda57402ccf4703e0db201
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2003-10-03 00:10:23 +0000

    Set the value of reserved field ifi_change (in struct ifinfomsg) to 0xffffffff as recommended in the manual page.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@2405 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/ifconfig_get_netlink.cc
+++ trunk/xorp/fea/ifconfig_get_netlink.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/ifconfig_get_netlink.cc,v 1.1 2003/09/22 05:45:57 pavlin Exp $"
+#ident "$XORP: xorp/fea/ifconfig_get_netlink.cc,v 1.2 2003/10/01 22:49:47 pavlin Exp $"
 
 
 #include "fea_module.h"
@@ -158,7 +158,7 @@ IfConfigGetNetlink::read_config(IfTree& it)
     ifinfomsg->ifi_type = IFLA_UNSPEC;
     ifinfomsg->ifi_index = 0;
     ifinfomsg->ifi_flags = 0;
-    ifinfomsg->ifi_change = 0;
+    ifinfomsg->ifi_change = 0xffffffff;
     
     if (ns4.sendto(ifibuf, nlh->nlmsg_len, 0,
 		   reinterpret_cast<struct sockaddr*>(&snl),
