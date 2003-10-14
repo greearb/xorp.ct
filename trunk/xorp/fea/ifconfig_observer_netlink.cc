e7222027def0ed6424296dbe10d7d85607a1a0ea
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2003-10-14 20:22:13 +0000

    Conditionally call stop() in IfConfigObserverRtsock and IfConfigObserverNetlink destructors (i.e., only if the particular mechanism is supported), otherwise we get a coredump if we try to gracefully stop the fea (a problem found by Orion).
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@2464 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/ifconfig_observer_netlink.cc
+++ trunk/xorp/fea/ifconfig_observer_netlink.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/ifconfig_observer_netlink.cc,v 1.1 2003/10/14 01:36:26 pavlin Exp $"
+#ident "$XORP: xorp/fea/ifconfig_observer_netlink.cc,v 1.2 2003/10/14 01:45:02 pavlin Exp $"
 
 #include "fea_module.h"
 #include "libxorp/xorp.h"
@@ -52,7 +52,9 @@ IfConfigObserverNetlink::IfConfigObserverNetlink(IfConfig& ifc)
 
 IfConfigObserverNetlink::~IfConfigObserverNetlink()
 {
+#ifdef HAVE_NETLINK_SOCKETS
     stop();
+#endif
 }
 
 int
--- trunk/xorp/fea/ifconfig_observer_rtsock.cc
+++ trunk/xorp/fea/ifconfig_observer_rtsock.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/ifconfig_observer_rtsock.cc,v 1.1 2003/05/02 23:21:38 pavlin Exp $"
+#ident "$XORP: xorp/fea/ifconfig_observer_rtsock.cc,v 1.2 2003/05/28 21:50:55 pavlin Exp $"
 
 #include "fea_module.h"
 #include "libxorp/xorp.h"
@@ -43,7 +43,9 @@ IfConfigObserverRtsock::IfConfigObserverRtsock(IfConfig& ifc)
 
 IfConfigObserverRtsock::~IfConfigObserverRtsock()
 {
+#ifdef HAVE_ROUTING_SOCKETS
     stop();
+#endif
 }
 
 int
