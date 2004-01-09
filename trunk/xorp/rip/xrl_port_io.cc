c63edf8c4c1b9ca948a95d062dce2a2bb4dda488
hodson <hodson@26435e26-4b25-483a-942d-477236992e84>
2004-01-09 00:28:13 +0000

    First pass at Xrl based packet send code.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@2918 26435e26-4b25-483a-942d-477236992e84

---
+++ trunk/xorp/rip/xrl_port_io.cc
@@ -0,0 +1,196 @@
+// -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*-
+
+// Copyright (c) 2001-2003 International Computer Science Institute
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software")
+// to deal in the Software without restriction, subject to the conditions
+// listed in the XORP LICENSE file. These conditions include: you must
+// preserve this copyright notice, and you cannot mention the copyright
+// holders in advertising related to the Software without their permission.
+// The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+// notice is a summary of the XORP LICENSE file; the license in that file is
+// legally binding.
+
+#ident "$XORP: xorp/devnotes/template.cc,v 1.2 2003/01/16 19:08:48 mjh Exp $"
+
+#include "constants.hh"
+#include "xrl_port_io.hh"
+
+#include "libxipc/xrl_router.hh"
+
+#include "xrl/interfaces/socket4_locator_xif.hh"
+#include "xrl/interfaces/socket4_xif.hh"
+
+// ----------------------------------------------------------------------------
+// IPv4 specialized XrlPortIO
+
+template <>
+bool
+XrlPortIO<IPv4>::request_socket_server()
+{
+    XrlSocket4LocatorV0p1Client cl(&_xr);
+    return cl.send_find_socket_server_for_addr("fea", _addr,
+		callback(this, &XrlPortIO<IPv4>::socket_server_cb));
+}
+
+template <>
+bool
+XrlPortIO<IPv4>::request_socket_open()
+{
+    XrlSocket4V0p1Client cl(&_xr);
+    return cl.send_udp_open_bind_join(
+		_ss.c_str(),
+		_xr.instance_name(),
+		_addr,
+		RIP_AF_CONSTANTS<IPv4>::IP_PORT,
+		RIP_AF_CONSTANTS<IPv4>::IP_GROUP(),
+		RIP_TTL,
+		false,
+		callback(this, &XrlPortIO<IPv4>::socket_open_cb)
+		);
+}
+
+template <>
+bool
+XrlPortIO<IPv4>::request_socket_close()
+{
+    XrlSocket4V0p1Client cl(&_xr);
+    return cl.send_close(_ss.c_str(), _sid,
+			 callback(this, &XrlPortIO<IPv4>::socket_close_cb));
+}
+
+template <>
+bool
+XrlPortIO<IPv4>::send(const IPv4& 		dst_addr,
+		      uint16_t 			dst_port,
+		      const vector<uint8_t>&  	rip_packet)
+{
+    if (_pending)
+	return false;
+
+    XrlSocket4V0p1Client cl(&_xr);
+    if (cl.send_send_to(_ss.c_str(), _sid, dst_addr, dst_port, rip_packet,
+			callback(this, &XrlPortIO<IPv4>::send_cb))) {
+	_pending = true;
+	return true;
+    }
+    return false;
+}
+
+
+// ----------------------------------------------------------------------------
+// IPv6 specialized XrlPortIO
+
+template <>
+bool
+XrlPortIO<IPv6>::request_socket_server()
+{
+    return false;
+}
+
+template <>
+bool
+XrlPortIO<IPv6>::request_socket_open()
+{
+    return false;
+}
+
+template <>
+bool
+XrlPortIO<IPv6>::request_socket_close()
+{
+    return false;
+}
+
+template <>
+bool
+XrlPortIO<IPv6>::send(const IPv6& 		/* dst_addr */,
+		      uint16_t			/* dst_port */,
+		      const vector<uint8_t>&	/* rip_packet*/)
+
+{
+    return false;
+}
+
+
+// ----------------------------------------------------------------------------
+// Non-specialized XrlPortIO methods.
+
+template <typename A>
+bool
+XrlPortIO<A>::pending() const
+{
+    return _pending;
+}
+
+template <typename A>
+void
+XrlPortIO<A>::startup()
+{
+    _pending = true;
+    set_status(STARTING);
+    if (request_socket_server() == false) {
+	set_status(FAILED, "Failed to find appropriate socket server.");
+    }
+}
+
+template <typename A>
+void
+XrlPortIO<A>::shutdown()
+{
+    _pending = true;
+    set_status(SHUTTING_DOWN);
+    if (request_socket_close() == false) {
+	set_status(SHUTDOWN);
+    }
+}
+
+template <typename A>
+void
+XrlPortIO<A>::socket_server_cb(const XrlError& e, const string* pss)
+{
+    if (e != XrlError::OKAY()) {
+	set_status(FAILED);
+	return;
+    }
+
+    _ss = *pss;
+    if (request_socket_open() == false) {
+	set_status(FAILED, "Failed sending socket open request.");
+    }
+}
+
+template <typename A>
+void
+XrlPortIO<A>::socket_open_cb(const XrlError& e, const string* psid)
+{
+    if (e != XrlError::OKAY()) {
+	set_status(FAILED, "Failed to instantiate socket");
+	return;
+    }
+
+    _sid = *psid;
+    _pending = false;
+    set_status(RUNNING);
+}
+
+template <typename A>
+void
+XrlPortIO<A>::send_cb(const XrlError& xe)
+{
+    _pending = false;
+    _user.port_io_send_completion(xe == XrlError::OKAY());
+}
+
+template <typename A>
+void
+XrlPortIO<A>::socket_close_cb(const XrlError&)
+{
+    set_status(SHUTDOWN);
+}
+
+template XrlPortIO<IPv4>;
+template XrlPortIO<IPv6>;
+
+
---
+++ trunk/xorp/rip/xrl_port_io.hh
@@ -0,0 +1,110 @@
+// -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*-
+
+// Copyright (c) 2001-2003 International Computer Science Institute
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software")
+// to deal in the Software without restriction, subject to the conditions
+// listed in the XORP LICENSE file. These conditions include: you must
+// preserve this copyright notice, and you cannot mention the copyright
+// holders in advertising related to the Software without their permission.
+// The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+// notice is a summary of the XORP LICENSE file; the license in that file is
+// legally binding.
+
+// $XORP: xorp/devnotes/template.hh,v 1.2 2003/01/16 19:08:48 mjh Exp $
+
+#ifndef __XRL_PORT_IO_HH__
+#define __XRL_PORT_IO_HH__
+
+#include <string>
+#include "libxorp/ipv4.hh"
+#include "libxorp/ipv6.hh"
+#include "libxorp/safe_callback_obj.hh"
+#include "libxorp/service.hh"
+#include "port_io.hh"
+
+class XrlError;
+class XrlRouter;
+class SocketXrlQueue;
+
+template <typename A>
+class XrlPortIO
+    : public PortIOBase<A>, public ServiceBase, public CallbackSafeObject
+{
+public:
+    typedef A			Addr;
+    typedef PortIOUserBase<A>	PortIOUser;
+public:
+    XrlPortIO(XrlRouter&	xr,
+	      PortIOUser&	port,
+	      const string&	ifname,
+	      const string&	vifname,
+	      const Addr&	addr)
+	: PortIOBase<A>(port, ifname, vifname, addr, false),
+	  _xr(xr), _pending(false)
+    {}
+
+    /**
+     * Startup.  Sends request to FEA for socket server for address
+     * and then attempts to instantiate socket with socket server.  If
+     * both operations are successful, instance status transitions to
+     * RUNNING.  Otherwise, it transitions to failed.
+     */
+    void startup();
+
+    /**
+     * Shutdown.  Sends request to close socket and transitions into
+     * SHUTTING_DOWN state.  When socket is closed transition to
+     * SHUTDOWN occurs.
+     */
+    void shutdown();
+
+    /**
+     * Send packet.  Status of instance must be running.  When packet is sent,
+     * the @ref pending() method will return true until the Xrl sending the
+     * packet has completed.
+     *
+     * @param dst_addr address to send packet.
+     * @param dst_port port to send packet to.
+     * @param rip_packet vector containing rip packet to be sent.
+     *
+     * @return false on immediately detectable failure, true otherwise.
+     */
+    bool send(const Addr&		dst_addr,
+	      uint16_t			dst_port,
+	      const vector<uint8_t>& 	rip_packet);
+
+    /**
+     * Return true if awaiting @ref send() completion, false otherwise.
+     */
+    bool pending() const;
+
+    /**
+     * Get name of socket server used to instantiate socket.
+     */
+    inline const string& socket_server() const { return _ss; }
+
+    /**
+     * Get socket id.
+     */
+    inline const string& socket_id() const { return _sid; }
+
+private:
+    bool request_socket_server();
+    bool request_socket_open();
+    bool request_socket_close();
+
+    void socket_server_cb(const XrlError& xe, const string* pss);
+    void socket_open_cb(const XrlError& xe, const string* psid);
+    void send_cb(const XrlError& xe);
+    void socket_close_cb(const XrlError& xe);
+
+protected:
+    XrlRouter&	_xr;
+    string	_ss;			// Socket Server Target Name
+    string	_sid;			// Socket id
+    bool	_pending;
+};
+
+#endif // __PATH_TO_TEMPLATE_HH__
