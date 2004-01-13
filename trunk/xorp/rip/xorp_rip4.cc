4fd7aa087055e228aedaced63817510944833119
hodson <hodson@26435e26-4b25-483a-942d-477236992e84>
2004-01-13 20:43:09 +0000

    Add code to watch status of assorted RIP entities and update the info for the Xrl get_status.
    Change main eventloop to run while no RIP entities have failed.
    
    Shutdown RIP entities in case of failure.  Run until they all
    transition into SHUTDOWN or FAILED or until an arbitrary time has
    elapsed.
    
    Fix port range check.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@2954 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/rip/xorp_rip4.cc
+++ trunk/xorp/rip/xorp_rip4.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/rip/xorp_rip4.cc,v 1.1 2003/11/04 23:39:58 hodson Exp $"
+#ident "$XORP: xorp/rip/xorp_rip4.cc,v 1.2 2004/01/09 00:29:03 hodson Exp $"
 
 #include "rip_module.h"
 #include "libxorp/xlog.h"
@@ -31,29 +31,126 @@
 #include "rip/xrl_process_spy.hh"
 #include "rip/xrl_rib_notifier.hh"
 
-#if 0
-class RipInstance : public ServiceChangeObserverBase {
+
+/**
+ * Unary function class to build string containing ServiceBase objects
+ * in a particular state.
+ */
+class service_names_in {
 public:
-    RipInstance(const string& finder_host, uint16_t finder_port);
+    service_names_in(ServiceStatus in_state, string& out)
+	: _st(in_state), _o(out)
+    {}
 
-    // Runs until stop is signalled through XrlTarget
-    void run();
+    inline void operator() (const ServiceBase* sb)
+    {
+	if (sb->status() != _st) return;
+	if (_o.empty() == false)
+	    _o += ", ";
+	_o += sb->service_name();
+    }
 
-    void status_change(ServiceBase*  service,
-		       ServiceStatus old_status,
-		       ServiceStatus new_status);
-
-    void create_process_spy();
-
-private:
-    RipInstance(const RipInstance&);
-    RipInstance& operator=(const RipInstance&);
-
-private:
-    bool _stop;
+protected:
+    ServiceStatus 	_st;
+    string& 		_o;
 };
-#endif /* 0 */
 
+
+/**
+ * Class to receive state changes of services used by RIP and update
+ * status in the xrl interface accordingly.
+ */
+class Service2XrlTarget4Status : public ServiceChangeObserverBase {
+protected:
+    void status_change(ServiceBase*, ServiceStatus, ServiceStatus)
+    {
+	update_status();
+    }
+
+public:
+    Service2XrlTarget4Status(XrlRip4Target& t) : _t(t), _st(READY) {}
+
+    ~Service2XrlTarget4Status()
+    {
+	while (_services.empty() == false) {
+	    _services.front()->unset_observer(this);
+	    _services.pop_front();
+	}
+    }
+
+    bool add_service(ServiceBase* sb)
+    {
+	if (sb->set_observer(this)) {
+	    _services.push_back(sb);
+	    update_status();
+	    return true;
+	}
+	return false;
+    }
+
+    void remove_service(ServiceBase* sb)
+    {
+	list<ServiceBase*>::iterator i;
+	i = find(_services.begin(), _services.end(), sb);
+	if (i != _services.end()) {
+	    _services.erase(i);
+	}
+	sb->unset_observer(this);
+    }
+
+    void update_status()
+    {
+	// XXX This is particularly inefficient and not very good.
+
+	// Clear existing state mask and then fill with current
+	// service states.   This is then propagated to the XrlTarget.
+
+	_st = ServiceStatus(0u);
+
+	list<ServiceBase*>::const_iterator i;
+	for (i = _services.begin(); i != _services.end(); ++i) {
+	    ServiceBase *sb = *i;
+	    _st = ServiceStatus(_st | sb->status());
+	}
+
+	if (_st & FAILED) {
+	    string svcs;
+	    for_each(_services.begin(), _services.end(),
+		     service_names_in(FAILED, svcs));
+	    _t.set_status(PROC_FAILED, "Failure(s): " + svcs);
+	    return;
+	}
+
+	if (_st & SHUTTING_DOWN) {
+	    string svcs;
+	    for_each(_services.begin(), _services.end(),
+		     service_names_in(SHUTTING_DOWN, svcs));
+	    _t.set_status(PROC_SHUTDOWN, "Awaiting shutdown of: " + svcs);
+	    return;
+	}
+
+	if (_st & (STARTING|READY)) {
+	    string svcs;
+	    for_each(_services.begin(), _services.end(),
+		     service_names_in(STARTING, svcs));
+	    _t.set_status(PROC_STARTUP, "Awaiting start up of: " + svcs);
+	    return;
+	}
+
+	XLOG_ASSERT(_st == RUNNING);
+	_t.set_status(PROC_READY, "");
+    }
+    inline bool have_status(ServiceStatus st) {
+	return _st & st;
+    }
+
+protected:
+    XrlRip4Target& 	_t;
+    list<ServiceBase*>	_services;
+    ServiceStatus	_st;
+};
+
+
 void
 rip_main(const string& finder_host, uint16_t finder_port)
 {
@@ -72,27 +169,48 @@ rip_main(const string& finder_host, uint16_t finder_port)
 	while (xsr.ready() == false) {
 	    e.run();
 	}
-	xr4t.set_status(PROC_STARTUP, "Checking for FEA and RIB processes.");
+
+	Service2XrlTarget4Status smon(xr4t);
 
 	// Start up process spy
 	xps.startup();
+	smon.add_service(&xps);
 
 	XrlRibNotifier<IPv4> xn(e, rip_system.route_db().update_queue(), xsr);
-
-	// Start up rib notifier
 	xn.startup();
+	smon.add_service(&xn);
 
 	// Start up interface mirror
 	ixm.startup();
+	smon.add_service(&ixm);
 
 	// Start up xrl port manager
 	xpm.startup();
+	smon.add_service(&xpm);
 
 	while (stop_requested == false &&
-	       xn.status() != FAILED) {
+	       smon.have_status(FAILED) == false) {
 	    e.run();
 	    printf(".");
 	}
+
+	xps.shutdown();
+	xn.shutdown();
+	ixm.shutdown();
+	xpm.shutdown();
+
+	bool flag(false);
+	XorpTimer t = e.set_flag_after_ms(5000, &flag);
+	while (flag == false &&
+	       smon.have_status(ServiceStatus(~(FAILED|SHUTDOWN)))) {
+	    printf("x");
+	    e.run();
+	}
+
+	smon.remove_service(&xps);
+	smon.remove_service(&xn);
+	smon.remove_service(&ixm);
+	smon.remove_service(&xpm);
     } catch (...) {
 	xorp_catch_standard_exceptions();
     }
@@ -127,7 +245,7 @@ parse_finder_args(const string& host_colon_port, string& host, uint16_t& port)
 	host = string(host_colon_port, 0, sp);
 	string s_port = string(host_colon_port, sp + 1, 14);
 	uint32_t t_port = atoi(s_port.c_str());
-	if (t_port == 0 || t_port > 0x65535) {
+	if (t_port == 0 || t_port > 65535) {
 	    XLOG_ERROR("Finder port %d is not in range 1--65535.\n", t_port);
 	    return false;
 	}
