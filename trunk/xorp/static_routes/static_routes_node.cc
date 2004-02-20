464d312e9b8c345cf2663f2c778f75e8e9f262f3
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2004-02-20 05:17:52 +0000

    Compilation fix for MacOS
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@3119 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/static_routes/static_routes_node.cc
+++ trunk/xorp/static_routes/static_routes_node.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/static_routes/static_routes_node.cc,v 1.1 2004/02/12 20:11:25 pavlin Exp $"
+#ident "$XORP: xorp/static_routes/static_routes_node.cc,v 1.2 2004/02/14 00:09:20 pavlin Exp $"
 
 
 //
@@ -200,8 +200,8 @@ StaticRoutesNode::node_status(string& reason_msg)
 	break;
     case PROC_STARTUP:
 	// Get the message about the startup progress
-	reason_msg = c_format("Waiting for %d startup events",
-			      _startup_requests_n);
+	reason_msg = c_format("Waiting for %u startup events",
+			      reinterpret_cast<uint32_t>(_startup_requests_n));
 	break;
     case PROC_NOT_READY:
 	// XXX: this state is unused
@@ -212,8 +212,8 @@ StaticRoutesNode::node_status(string& reason_msg)
 	break;
     case PROC_SHUTDOWN:
 	// Get the message about the shutdown progress
-	reason_msg = c_format("Waiting for %d shutdown events",
-			      _shutdown_requests_n);
+	reason_msg = c_format("Waiting for %u shutdown events",
+			      reinterpret_cast<uint32_t>(_shutdown_requests_n));
 	break;
     case PROC_FAILED:
 	// XXX: this state is unused
