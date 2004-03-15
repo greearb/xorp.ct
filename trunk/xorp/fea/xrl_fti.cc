a523a20aee6caa42af112c7f62e350a833b2a2eb
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2004-03-15 23:33:42 +0000

    Added support to the XrlFtiTransactionManager for managing FIB clients (i.e., clients to receive updates whenever the Forwarding Information Base changes).
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@3396 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/xrl_fti.cc
+++ trunk/xorp/fea/xrl_fti.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/xrl_fti.cc,v 1.2 2003/03/10 23:20:17 hodson Exp $"
+#ident "$XORP: xorp/fea/xrl_fti.cc,v 1.3 2003/03/18 01:01:30 pavlin Exp $"
 
 #include "xrl_fti.hh"
 
@@ -74,3 +74,324 @@ XrlFtiTransactionManager::add(uint32_t tid,
     //
     return XrlCmdError::COMMAND_FAILED("Unknown resource shortage");
 }
+
+XrlCmdError
+XrlFtiTransactionManager::add_fib_client4(const string& target_name)
+{
+    // Test if we have this client already
+    if (_fib_clients4.find(target_name) != _fib_clients4.end()) {
+	string error_msg = c_format("Target %s is already an IPv4 FIB client",
+				    target_name.c_str());
+	return XrlCmdError::COMMAND_FAILED(error_msg);
+    }
+
+    // Add the client
+    _fib_clients4.insert(make_pair(target_name,
+				   FibClient4(target_name, *this)));
+    FibClient4& fib_client = _fib_clients4.find(target_name)->second;
+
+    // Activate the client
+    list<Fte4> fte_list;
+    if (ftic().get_table4(fte_list) != true) {
+	string error_msg = "Cannot get the IPv4 FIB";
+	return XrlCmdError::COMMAND_FAILED(error_msg);
+    }
+    fib_client.activate(fte_list);
+
+    return XrlCmdError::OKAY();
+}
+
+XrlCmdError
+XrlFtiTransactionManager::add_fib_client6(const string& target_name)
+{
+    // Test if we have this client already
+    if (_fib_clients6.find(target_name) != _fib_clients6.end()) {
+	string error_msg = c_format("Target %s is already an IPv6 FIB client",
+				    target_name.c_str());
+	return XrlCmdError::COMMAND_FAILED(error_msg);
+    }
+
+    // Add the client
+    _fib_clients6.insert(make_pair(target_name,
+				   FibClient6(target_name, *this)));
+    FibClient6& fib_client = _fib_clients6.find(target_name)->second;
+
+    // Activate the client
+    list<Fte6> fte_list;
+    if (ftic().get_table6(fte_list) != true) {
+	string error_msg = "Cannot get the IPv6 FIB";
+	return XrlCmdError::COMMAND_FAILED(error_msg);
+    }
+    fib_client.activate(fte_list);
+
+    return XrlCmdError::OKAY();
+}
+
+XrlCmdError
+XrlFtiTransactionManager::delete_fib_client4(const string& target_name)
+{
+    map<string, FibClient4>::iterator iter;
+
+    iter = _fib_clients4.find(target_name);
+    if (iter == _fib_clients4.end()) {
+	string error_msg = c_format("Target %s is not an IPv4 FIB client",
+				    target_name.c_str());
+	return XrlCmdError::COMMAND_FAILED(error_msg);
+    }
+
+    _fib_clients4.erase(iter);
+
+    return XrlCmdError::OKAY();
+}
+
+XrlCmdError
+XrlFtiTransactionManager::delete_fib_client6(const string& target_name)
+{
+    map<string, FibClient6>::iterator iter;
+
+    iter = _fib_clients6.find(target_name);
+    if (iter == _fib_clients6.end()) {
+	string error_msg = c_format("Target %s is not an IPv6 FIB client",
+				    target_name.c_str());
+	return XrlCmdError::COMMAND_FAILED(error_msg);
+    }
+
+    _fib_clients6.erase(iter);
+
+    return XrlCmdError::OKAY();
+}
+
+int
+XrlFtiTransactionManager::send_fib_client_add_route(const string& target_name,
+						    const Fte4& fte)
+{
+    bool success;
+
+    success = _xrl_fea_fib_client.send_add_route4(
+	target_name.c_str(),
+	fte.net(),
+	fte.gateway(),
+	fte.ifname(),
+	fte.vifname(),
+	fte.metric(),
+	fte.admin_distance(),
+	string("NOT_SUPPORTED"),
+	fte.xorp_route(),
+	callback(this,
+		 &XrlFtiTransactionManager::send_fib_client_add_route4_cb,
+		 target_name));
+
+    if (success)
+	return XORP_OK;
+    else
+	return XORP_ERROR;
+}
+
+int
+XrlFtiTransactionManager::send_fib_client_add_route(const string& target_name,
+						    const Fte6& fte)
+{
+    bool success;
+
+    success = _xrl_fea_fib_client.send_add_route6(
+	target_name.c_str(),
+	fte.net(),
+	fte.gateway(),
+	fte.ifname(),
+	fte.vifname(),
+	fte.metric(),
+	fte.admin_distance(),
+	string("NOT_SUPPORTED"),
+	fte.xorp_route(),
+	callback(this,
+		 &XrlFtiTransactionManager::send_fib_client_add_route6_cb,
+		 target_name));
+
+    if (success)
+	return XORP_OK;
+    else
+	return XORP_ERROR;
+}
+
+int
+XrlFtiTransactionManager::send_fib_client_delete_route(const string& target_name,
+						       const Fte4& fte)
+{
+    bool success;
+
+    success = _xrl_fea_fib_client.send_delete_route4(
+	target_name.c_str(),
+	fte.net(),
+	callback(this,
+		 &XrlFtiTransactionManager::send_fib_client_delete_route6_cb,
+		 target_name));
+
+    if (success)
+	return XORP_OK;
+    else
+	return XORP_ERROR;
+}
+
+int
+XrlFtiTransactionManager::send_fib_client_delete_route(const string& target_name,
+						       const Fte6& fte)
+{
+    bool success;
+
+    success = _xrl_fea_fib_client.send_delete_route6(
+	target_name.c_str(),
+	fte.net(),
+	callback(this,
+		 &XrlFtiTransactionManager::send_fib_client_delete_route6_cb,
+		 target_name));
+
+    if (success)
+	return XORP_OK;
+    else
+	return XORP_ERROR;
+}
+
+void
+XrlFtiTransactionManager::send_fib_client_add_route4_cb(
+    const XrlError& xrl_error,
+    string target_name)
+{
+    map<string, FibClient4>::iterator iter;
+
+    iter = _fib_clients4.find(target_name);
+    if (iter == _fib_clients4.end()) {
+	// The client has probably gone. Silently ignore.
+	return;
+    }
+
+    FibClient4& fib_client = iter->second;
+    fib_client.send_fib_client_route_change_cb(xrl_error);
+}
+
+void
+XrlFtiTransactionManager::send_fib_client_add_route6_cb(
+    const XrlError& xrl_error,
+    string target_name)
+{
+    map<string, FibClient6>::iterator iter;
+
+    iter = _fib_clients6.find(target_name);
+    if (iter == _fib_clients6.end()) {
+	// The client has probably gone. Silently ignore.
+	return;
+    }
+
+    FibClient6& fib_client = iter->second;
+    fib_client.send_fib_client_route_change_cb(xrl_error);
+}
+
+void
+XrlFtiTransactionManager::send_fib_client_delete_route4_cb(
+    const XrlError& xrl_error,
+    string target_name)
+{
+    map<string, FibClient4>::iterator iter;
+
+    iter = _fib_clients4.find(target_name);
+    if (iter == _fib_clients4.end()) {
+	// The client has probably gone. Silently ignore.
+	return;
+    }
+
+    FibClient4& fib_client = iter->second;
+    fib_client.send_fib_client_route_change_cb(xrl_error);
+}
+
+void
+XrlFtiTransactionManager::send_fib_client_delete_route6_cb(
+    const XrlError& xrl_error,
+    string target_name)
+{
+    map<string, FibClient6>::iterator iter;
+
+    iter = _fib_clients6.find(target_name);
+    if (iter == _fib_clients6.end()) {
+	// The client has probably gone. Silently ignore.
+	return;
+    }
+
+    FibClient6& fib_client = iter->second;
+    fib_client.send_fib_client_route_change_cb(xrl_error);
+}
+
+template<class F>
+void
+XrlFtiTransactionManager::FibClient<F>::activate(const list<F>& fte_list)
+{
+    // Create the queue with the "add" commands
+    typename list<F>::const_iterator iter;
+    for (iter = fte_list.begin(); iter != fte_list.end(); ++iter) {
+	const F& fte = *iter;
+	_inform_fib_client_queue.push_back(make_pair(true, fte));
+    }
+
+    // Start the timer to send all FIB entries
+    _inform_fib_client_queue_timer = eventloop().new_oneoff_after(
+	TimeVal(0, 0),
+	callback(this, &XrlFtiTransactionManager::FibClient<F>::send_fib_client_route_change));
+}
+
+template<class F>
+void
+XrlFtiTransactionManager::FibClient<F>::send_fib_client_route_change()
+{
+    int success = XORP_ERROR;
+
+    if (_inform_fib_client_queue.empty())
+	return;		// No more route changes to send
+
+    AddDeleteFte& add_delete_fte = _inform_fib_client_queue.front();
+    bool is_add = add_delete_fte.first;
+    F& fte = add_delete_fte.second;
+
+    //
+    // Send the appropriate XRLs
+    //
+    if (is_add) {
+	// Add a route
+	success = _xftm.send_fib_client_add_route(_target_name, fte);
+    } else {
+	// Delete a route
+	success = _xftm.send_fib_client_delete_route(_target_name, fte);
+    }
+
+    if (success == XORP_OK)
+	return;		// OK
+
+    //
+    // If an error, then start a timer to try again
+    // TODO: XXX: the timer value is hardcoded here!!
+    //
+    _inform_fib_client_queue_timer = eventloop().new_oneoff_after(
+	TimeVal(1, 0),
+	callback(this, &XrlFtiTransactionManager::FibClient<F>::send_fib_client_route_change));
+}
+
+template<class F>
+void
+XrlFtiTransactionManager::FibClient<F>::send_fib_client_route_change_cb(
+    const XrlError& xrl_error)
+{
+    // If success, then send the next route change
+    if (xrl_error == XrlError::OKAY()) {
+	_inform_fib_client_queue.pop_front();
+	send_fib_client_route_change();
+	return;
+    }
+
+    //
+    // If an error, then start a timer to try again
+    // TODO: XXX: the timer value is hardcoded here!!
+    //
+    _inform_fib_client_queue_timer = eventloop().new_oneoff_after(
+	TimeVal(1, 0),
+	callback(this, &XrlFtiTransactionManager::FibClient<F>::send_fib_client_route_change));
+}
+
+template class XrlFtiTransactionManager::FibClient<Fte4>;
+template class XrlFtiTransactionManager::FibClient<Fte6>;
--- trunk/xorp/fea/xrl_fti.hh
+++ trunk/xorp/fea/xrl_fti.hh
@@ -12,13 +12,18 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/fea/xrl_fti.hh,v 1.3 2003/03/10 23:20:17 hodson Exp $
+// $XORP: xorp/fea/xrl_fti.hh,v 1.4 2003/05/02 07:50:49 pavlin Exp $
 
 #ifndef __FEA_XRL_FTI_HH__
 #define __FEA_XRL_FTI_HH__
 
 #include "libxipc/xrl_router.hh"
+
+#include "xrl/interfaces/fea_fib_client_xif.hh"
+
 #include "fti_transaction.hh"
+#include "fte.hh"
+
 
 /**
  * Helper class for helping with Fti transactions via an Xrl
@@ -39,23 +44,176 @@ public:
      */
     XrlFtiTransactionManager(EventLoop&	e,
 			     FtiConfig&	ftic,
+			     XrlRouter* xrl_router,
 			     uint32_t	max_ops = 200)
-	: _ftm(e, ftic), _max_ops(max_ops)
+	: _ftm(e, ftic), _max_ops(max_ops), _xrl_fea_fib_client(xrl_router)
     {}
 
+    /**
+     * Start a Fti transaction.
+     * 
+     * @param tid the return-by-reference transaction ID.
+     * @return the XRL command error.
+     */
     XrlCmdError start_transaction(uint32_t& tid);
 
+    /**
+     * Commit a Fti transaction.
+     * 
+     * @param tid the ID of the transaction to commit.
+     * @return the XRL command error.
+     */
     XrlCmdError commit_transaction(uint32_t tid);
 
+    /**
+     * Abort a Fti transaction.
+     * 
+     * @param tid the ID of the transaction to abort.
+     * @return the XRL command error.
+     */
     XrlCmdError abort_transaction(uint32_t tid);
 
+    /**
+     * Add a Fti operation.
+     * 
+     * @param tid the transaction ID.
+     * @param op the operation to add.
+     * @return the XRL command error.
+     */
     XrlCmdError add(uint32_t tid, const FtiTransactionManager::Operation& op);
 
+    /**
+     * Get the FtiConfig entry.
+     * 
+     * @return a reference to the corresponding FtiConfig entry.
+     * @see FtiConfig.
+     */
     FtiConfig& ftic() { return _ftm.ftic(); }
+
+    /**
+     * Add an IPv4 FIB client.
+     * 
+     * @param target_name the target name of the client to add.
+     * @return the XRL command error.
+     */
+    XrlCmdError add_fib_client4(const string& target_name);
+
+    /**
+     * Add an IPv6 FIB client.
+     * 
+     * @param target_name the target name of the client to add.
+     * @return the XRL command error.
+     */
+    XrlCmdError add_fib_client6(const string& target_name);
+
+    /**
+     * Delete an IPv4 FIB client.
+     * 
+     * @param target_name the target name of the client to delete.
+     * @return the XRL command error.
+     */
+    XrlCmdError delete_fib_client4(const string& target_name);
+
+    /**
+     * Delete an IPv6 FIB client.
+     * 
+     * @param target_name the target name of the client to delete.
+     * @return the XRL command error.
+     */
+    XrlCmdError delete_fib_client6(const string& target_name);
+
+    /**
+     * Send an XRL to a FIB client to add an IPv4 route.
+     *
+     * @param target_name the target name of the FIB client.
+     * @param fte the Fte with the route information to add.
+     * @return XORP_OK on success, otherwise XORP_ERROR.
+     * @see Fte4.
+     */
+    int send_fib_client_add_route(const string& target_name,
+				  const Fte4& fte);
+
+    /**
+     * Send an XRL to a FIB client to add an IPv6 route.
+     *
+     * @param target_name the target name of the FIB client.
+     * @param fte the Fte with the route information to add.
+     * @return XORP_OK on success, otherwise XORP_ERROR.
+     * @see Fte6.
+     */
+    int send_fib_client_add_route(const string& target_name,
+				  const Fte6& fte);
+
+    /**
+     * Send an XRL to a FIB client to delete an IPv4 route.
+     *
+     * @param target_name the target name of the FIB client.
+     * @param fte the Fte with the route information to delete.
+     * @return XORP_OK on success, otherwise XORP_ERROR.
+     * @see Fte4.
+     */
+    int send_fib_client_delete_route(const string& target_name,
+				     const Fte4& fte);
+
+    /**
+     * Send an XRL to a FIB client to delete an IPv6 route.
+     *
+     * @param target_name the target name of the FIB client.
+     * @param fte the Fte with the route information to delete.
+     * @return XORP_OK on success, otherwise XORP_ERROR.
+     * @see Fte6.
+     */
+    int send_fib_client_delete_route(const string& target_name,
+				     const Fte6& fte);
 
 protected:
     FtiTransactionManager  _ftm;
     uint32_t		   _max_ops;	// Maximum operations in a transaction
+
+private:
+
+    void send_fib_client_add_route4_cb(const XrlError& xrl_error,
+				       string target_name);
+    void send_fib_client_add_route6_cb(const XrlError& xrl_error,
+				       string target_name);
+    void send_fib_client_delete_route4_cb(const XrlError& xrl_error,
+					  string target_name);
+    void send_fib_client_delete_route6_cb(const XrlError& xrl_error,
+					  string target_name);
+
+    /**
+     * A template class for storing FIB client information.
+     */
+    template<class F>
+    class FibClient {
+    public:
+	FibClient(const string& target_name, XrlFtiTransactionManager& xftm)
+	    : _target_name(target_name), _xftm(xftm) {}
+
+	void activate(const list<F>& fte_list);
+	void	send_fib_client_route_change_cb(const XrlError& xrl_error);
+
+    private:
+	EventLoop& eventloop() { return _xftm.ftic().eventloop(); }
+	bool	is_ipv4() const;
+	bool	is_ipv6() const;
+	void	send_fib_client_route_change();
+
+	typedef pair<bool, F>	AddDeleteFte;
+	list<AddDeleteFte>	_inform_fib_client_queue;
+	XorpTimer		_inform_fib_client_queue_timer;
+
+	string			_target_name;	// Target name of the client
+	XrlFtiTransactionManager& _xftm;
+    };
+
+    typedef FibClient<Fte4>	FibClient4;
+    typedef FibClient<Fte6>	FibClient6;
+
+    map<string, FibClient4>	_fib_clients4;
+    map<string, FibClient6>	_fib_clients6;
+
+    XrlFeaFibClientV0p1Client	_xrl_fea_fib_client;
 };
 
 #endif // __FEA_XRL_FTI_HH__
