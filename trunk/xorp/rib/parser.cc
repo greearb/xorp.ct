4e57d2949e84d507ea908ecf67ad89975c14ee81
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2004-03-19 23:45:11 +0000

    Add a comment that the parser would accept empty lines.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@3454 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/rib/parser.cc
+++ trunk/xorp/rib/parser.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/rib/parser.cc,v 1.9 2004/02/11 08:48:45 pavlin Exp $"
+#ident "$XORP: xorp/rib/parser.cc,v 1.10 2004/03/19 02:36:00 pavlin Exp $"
 
 #include <stdexcept>
 
@@ -272,6 +272,8 @@ Parser::parse(const string& s) const
 
     vector<string> words(10);
     int wcount = split_into_words(str, words);
+
+    // Accept empty lines
     if (wcount == 0)
 	return XORP_OK;
 
