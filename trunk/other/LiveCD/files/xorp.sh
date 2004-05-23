2e153e7bf1949969c5fd6549ad752f7e48026ccf
mjh <mjh@26435e26-4b25-483a-942d-477236992e84>
2004-05-23 11:15:21 +0000

    First pass at FreeBSD-based LiveCD creation toolkit.  Still pretty clunky, but does actually work (at least on FreeBSD 4.9-RELEASE).
    Probably not worthwhile testing yet - this is a moving target right now.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@3890 26435e26-4b25-483a-942d-477236992e84

---
+++ trunk/other/LiveCD/files/xorp.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+ 
+
+DEV=/dev/fd0
+MNT_PNT=/mnt/floppy
+FD_BASE=
+RF_BASE=/
+RF_BACKUP=/var/tmp
+LOCK_FILE=/var/run/floppy_file.lck
+LOG_FILE=/var/log/floppy_mount.log
+FILE_LIST="/etc/passwd:644 /etc/xorp.cfg:644 /etc/group:644"
+CFG_FILE=/etc/xorp.cfg
+
+discover_interfaces() {
+
+	echo "interfaces {" > $CFG_FILE
+
+	for i in `ifconfig -a | awk -F : '$2~/flags=/ {print $1}'`
+	do
+      		echo "    interface $i {" >> $CFG_FILE
+		if [ "${i}" != "lo0" ]; then
+		    echo "        description: \"detected interface\"" >> $CFG_FILE
+		else
+		    echo "        description: \"loopback interface\"" >> $CFG_FILE
+		fi
+       		echo "    }"  >> $CFG_FILE
+       		echo " ">> $CFG_FILE
+	done
+
+	echo "}" >> $CFG_FILE
+
+	echo  "fea {" >> $CFG_FILE
+	echo  "    enable-unicast-forwarding4: true" >> $CFG_FILE
+	echo "}" >> $CFG_FILE
+
+}
+
+copy_files_in () {
+	/usr/local/xorp/bin/xorp_load.py -mode loadin $MNT_PNT/manifest.dat
+}
+
+copy_files_back() {
+	/usr/local/xorp/bin/xorp_load.py -mode saveout $MNT_PNT/manifest.dat
+}
+
+unmount_fd() {
+	
+	echo -n "Mounting floppy disk... "
+	#umount it first, just in case it got left mounted....
+	umount /mnt/floppy 1>/dev/null 2>/dev/null
+	mount -t msdos $DEV $MNT_PNT 1>$LOG_FILE 2>$LOG_FILE
+	if [ $? -eq 0 ]; then 
+	    	echo "OK"
+	    	copy_files_back
+	    	sync
+    	    	echo -n "Unmounting floppy disk... "
+	    	umount $MNT_PNT 1>$LOG_FILE 2>$LOG_FILE
+	    	
+		if [ $? -eq 0 ]; then 
+	    		echo "OK"
+		else
+	    		echo "FAIL"
+		fi
+      	fi
+}
+
+mount_fd() {
+	
+	echo -n "Mounting floppy disk... "
+	#umount it first, just in case it got left mounted....
+	umount /mnt/floppy 1>/dev/null 2>/dev/null
+	mount -t msdos $DEV $MNT_PNT 1>$LOG_FILE 2>$LOG_FILE
+	if [ $? -eq 0 ]; then 
+	    	echo "OK"
+	    	copy_files_in
+	    	sync
+    	    	echo -n "Unmounting floppy disk... "
+	    	umount $MNT_PNT 1>$LOG_FILE 2>$LOG_FILE
+	    	
+		if [ $? -eq 0 ]; then 
+	    		echo "OK"
+		else
+	    		echo "FAIL"
+		fi
+	else
+	    echo "FAILED to load any config from floppy, creating config."
+	    discover_interfaces
+      	fi
+}
+
+start() {
+	echo "Loading XORP config"
+       	mount_fd
+
+	echo "Starting XORP router manager process"
+	/usr/local/xorp/bin/xorp_rtrmgr -b $CFG_FILE 1>/dev/null 2>/dev/null &
+	exit 0
+}
+
+stop() {
+	echo "Syncing back XORP to floppy"
+	killall xorp_rtrmgr 
+	unmount_fd
+	exit 0
+}
+
+reload() {
+	echo "Reloading doesn't make sense, don't do it"
+	exit 0
+}
+
+case "$1" in
+start)
+        start
+        ;;
+stop)
+        stop
+        ;;
+*)
+        echo "Usage: `basename $0` {start|stop}" >&2
+        ;;
+esac
+
+exit 0
---
+++ trunk/other/LiveCD/files/xorp_load.py
@@ -0,0 +1,125 @@
+#!/usr/local/bin/python
+
+# copyright a.greenhalgh@cs.ucl.ac.uk, 2004
+# use entirely at your own risk.
+
+# Things to fix 
+# - hard coded permission of 755
+# - hard coded manifest location
+# - code in floppy location , can use manifest file for now
+
+import sys
+import shutil
+import os
+import pwd 
+import grp
+
+# File entry group
+class FileEntry:
+  def __init__(self,source,destination,permission,owner,group,md5):
+    self.source = source
+    self.destination = destination
+    self.permission = permission
+    self.owner = owner
+    self.group = group
+    self.md5 = md5
+  def print_fields(self):
+    return "source : " + self.source + " destination : " + self.destination + " permission : " + self.permission + " owner : " + self.owner + " group : " + self.group + " md5 : " + self.md5;
+  
+# Load manifest
+def loadManifest(filename):
+  manifest = [];
+  manifest_file = open(filename,'r')
+  lines = manifest_file.readlines()
+  manifest_file.close()
+
+  for j in lines:
+    i = j.split();
+    x = FileEntry(i[0],i[1],i[2],i[3],i[4],i[5])
+    manifest.append(x);
+  return manifest
+
+# Copy file in
+def copyFileIn(fe):
+  shutil.copyfile(fe.source,fe.destination);
+  os.chown(fe.destination,pwd.getpwnam(fe.owner).pw_uid,grp.getgrnam(fe.group).gr_gid);
+  os.chmod(fe.destination,0755);
+  print "copied in file " + fe.source + "."
+
+# Copy all files in
+def copyAllFilesIn(manifest):
+  print "Copying All files to floppy."
+  for i in manifest:
+    copyFileIn(i)
+
+# Copy file out
+
+def copyFileOut(fe):
+  shutil.copyfile(fe.destination,fe.source);
+  os.chown(fe.source,pwd.getpwnam(fe.owner).pw_uid,grp.getgrnam(fe.group).gr_gid);
+  os.chmod(fe.source,0755);
+  print "copied out file " + fe.destination + "."
+
+# Copy all files out
+
+def copyAllFilesOut(manifest):
+  print "Copying All files to floppy."
+  for i in manifest:
+    copyFileOut(i)
+
+# Save file from os to disk, only called by router manager
+
+def singleFileSaveToFloppy(filename):
+  manifest_file_location = "manifest.dat";
+  manifest_files = loadManifest(manifest_file_location)
+  for f in manifest_files:
+    if filename == f.destination:
+	copyFileOut(f)
+	return
+	
+
+def helpMsg(arguments):
+	print "Wrong number of arguments."
+        print " " + arguments[0] + " <filename>"
+        print "    called by rtrmgr when a file is saved"
+	print " " + arguments[0] + " -mode loadin <manifest file>"
+	print "    loads files on bootup"
+	print " " + arguments[0] + " -mode saveout <manifest file>"
+        print "    save files to floppy on system shutdown"
+	return 1
+
+# Main
+
+arguments = sys.argv
+
+#### 1 argument - help
+if len(arguments) == 1:
+   sys.exit(helpMsg(arguments))
+  
+#### 2 arguments - save file to floppy
+
+if len(arguments) == 2 :
+	singleFileSaveToFloppy(arguments[1])
+
+#### 3 arguments - nothing
+if len(arguments) == 3:
+  sys.exit(helpMsg(arguments))
+
+#### 4 arguments various modes
+
+if len(arguments) == 4:
+  if arguments[1] == "-mode":
+
+    if arguments[2] == "loadin" :
+	copyAllFilesIn ( loadManifest(arguments[3]) )
+    else :
+      if arguments[2] == "saveout":
+	copyAllFilesOut ( loadManifest(arguments[3]) )
+      else :
+        sys.exit(helpMsg(arguments))
+  else : 
+    sys.exit(helpMsg(arguments))		
+
+#### > 4 arguments - nothing
+if len(arguments) > 4:
+  sys.exit(helpMsg(arguments))
