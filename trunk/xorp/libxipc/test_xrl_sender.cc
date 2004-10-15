91627165e39bcfcc4853e0512d2982e832ea5ab8
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2004-10-15 02:31:42 +0000

    Apply a patch:  * Remove various #define and replace them with command-line options  * Add support for sending/receiving XRLs with variable number of    list elements.  * Misc improvements and nits.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@4786 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxipc/test_xrl_sender.cc
+++ trunk/xorp/libxipc/test_xrl_sender.cc
@@ -12,8 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/libxipc/test_xrl_sender.cc,v 1.6 2004/09/28 21:01:04 pavlin Exp $"
-
+#ident "$XORP: xorp/libxipc/test_xrl_sender.cc,v 1.7 2004/10/13 05:41:48 pavlin Exp $"
 
 //
 // Test XRLs sender.
@@ -34,7 +33,6 @@
 #include "xrl/interfaces/test_xrls_xif.hh"
 #include "xrl/targets/test_xrls_base.hh"
 
-
 //
 // This is a sender program for testing XRL performance. It is used
 // as a pair with the "test_xrl_receiver" program.
@@ -46,34 +44,24 @@
 //
 // Use the definitions below to change the sender's behavior.
 //
-// Note that if both sender and receiver are compiled within same binary,
-// (e.g., if COMPILE_SINGLE_BINARY below is defined to 1), then there is
+// Note that it's possible to run both the sender and receiver are in same
+// binary, by supply -r as a command-line argument.  If so then there is
 // no need to start "./test_xrl_receiver".
 //
 
 //
-// Define to 1 to compile both sender and receiver within a single binary
+// Definitions for send style.
 //
-#define COMPILE_SINGLE_BINARY		0
-
-//
-// Define the maximum number of XRLs to send into a batch, and the
-// XRL pipe size.
-//
-#define MAX_XRL_ID			100000		// 100000
-#define XRL_PIPE_SIZE			100		// 100000
-
-//
-// Define to 1 to send short XRLs, otherwise send long XRLs.
-//
-#define SEND_SHORT_XRL			1
+#define SEND_SHORT_XRL                  0
+#define SEND_CMDLINE_VAR_XRL            1
+#define SEND_LARGE_XRL                  2
 
 //
 // Define the size of the string and vector arguments in the long XRLs.
 // Note that those are used only if we are sending long XRLs.
 //
-#define MY_STRING_SIZE			1024		// 1024
-#define MY_VECTOR_SIZE			4096		// 4096
+#define MY_STRING_SIZE			256		// 1024
+#define MY_VECTOR_SIZE			1024		// 4096
 
 //
 // Define to 1 to enable printing of debug output
@@ -83,16 +71,33 @@
 //
 // Define the sending method
 //
-#define SEND_METHOD_PIPELINE		1
-#define SEND_METHOD_NO_PIPELINE		2
-#define SEND_METHOD_START_PIPELINE	3
+#define SEND_METHOD_PIPELINE		0
+#define SEND_METHOD_NO_PIPELINE		1
+#define SEND_METHOD_START_PIPELINE	2
 #define SEND_METHOD			SEND_METHOD_PIPELINE
 
 //
 // Define to 1 to exit after end of transmission
 //
-#define SEND_DO_EXIT			0
-#define RECEIVE_DO_EXIT			0
+#define SEND_DO_EXIT			1
+#define RECEIVE_DO_EXIT			1
+#define TIMEOUT_EXIT_MS			20000
+
+//
+// Global variables
+//
+static uint32_t MAX_XRL_ID	= 10000;	// Number of Xrls in a test run
+static uint32_t XRL_PIPE_SIZE	= 40;		// Maximum in flight
+static uint32_t g_send_method   = SEND_METHOD_PIPELINE;
+static uint32_t g_send_style    = SEND_CMDLINE_VAR_XRL;
+static uint32_t g_atoms_per_xrl = 1;
+static bool     g_run_receiver	= false;
+
+static const char* send_methods[3] = {
+    "pipeline",
+    "no pipeline",
+    "start pipeline"
+};
 
 //
 // Local functions prototypes
@@ -121,8 +126,10 @@ public:
 	_my_mac = Mac("12:34:56:78:90:ab");
 	_my_string = string(MY_STRING_SIZE, 'a');
 	_my_vector.resize(MY_VECTOR_SIZE);
+#if 0
 	for (size_t i = 0; i < _my_vector.size(); i++)
 	    _my_vector[i] = i % 0xff;
+#endif
     }
     ~TestSender() {}
 
@@ -173,63 +180,94 @@ public:
 
 private:
 
-    bool transmit_xrl_next() {
-#if SEND_SHORT_XRL
-	return _test_xrls_client.send_add_xrl0(
-	    _receiver_target.c_str(),
-	    callback(this, &TestSender::send_next_cb));
-#else
-	return _test_xrls_client.send_add_xrl9(
-	    _receiver_target.c_str(),
-	    _my_bool,
-	    _my_int,
-	    _my_ipv4,
-	    _my_ipv4net,
-	    _my_ipv6,
-	    _my_ipv6net,
-	    _my_mac,
-	    _my_string,
-	    _my_vector,
-	    callback(this, &TestSender::send_next_cb));
-#endif
+    bool transmit_xrl_next()
+    {
+	switch (g_send_style) {
+	case SEND_SHORT_XRL:
+	    return _test_xrls_client.send_add_xrl0(
+			_receiver_target.c_str(),
+			callback(this, &TestSender::send_next_cb));
+
+	case SEND_LARGE_XRL:
+	    return _test_xrls_client.send_add_xrl9(
+			_receiver_target.c_str(),
+			_my_bool,
+			_my_int,
+			_my_ipv4,
+			_my_ipv4net,
+			_my_ipv6,
+			_my_ipv6net,
+			_my_mac,
+			_my_string,
+			_my_vector,
+			callback(this, &TestSender::send_next_cb));
+
+	case SEND_CMDLINE_VAR_XRL:
+	    XrlAtomList xal;
+	    for (uint32_t i = 1; i < g_atoms_per_xrl; i++) {
+		xal.append(XrlAtom(_my_int));
+	    }
+	    return _test_xrls_client.send_add_xrlx(
+	    		_receiver_target.c_str(), xal,
+			callback(this, &TestSender::send_next_cb)
+			);
+	}
+	return false;
     }
 
-    bool transmit_xrl_next_pipeline() {
-#if SEND_SHORT_XRL
-	return _test_xrls_client.send_add_xrl0(
-	    _receiver_target.c_str(),
-	    callback(this, &TestSender::send_next_pipeline_cb));
-#else
-	return _test_xrls_client.send_add_xrl9(
-	    _receiver_target.c_str(),
-	    _my_bool,
-	    _my_int,
-	    _my_ipv4,
-	    _my_ipv4net,
-	    _my_ipv6,
-	    _my_ipv6net,
-	    _my_mac,
-	    _my_string,
-	    _my_vector,
-	    callback(this, &TestSender::send_next_pipeline_cb));
-#endif
+    bool transmit_xrl_next_pipeline()
+    {
+	switch (g_send_style) {
+	case SEND_SHORT_XRL:
+	    return _test_xrls_client.send_add_xrl0(
+			_receiver_target.c_str(),
+			callback(this, &TestSender::send_next_pipeline_cb));
+
+	case SEND_LARGE_XRL:
+	    return _test_xrls_client.send_add_xrl9(
+			_receiver_target.c_str(),
+			_my_bool,
+			_my_int,
+			_my_ipv4,
+			_my_ipv4net,
+			_my_ipv6,
+			_my_ipv6net,
+			_my_mac,
+			_my_string,
+			_my_vector,
+			callback(this, &TestSender::send_next_pipeline_cb));
+
+	case SEND_CMDLINE_VAR_XRL:
+	    XrlAtomList xal;
+	    for (uint32_t i = 1; i < g_atoms_per_xrl; i++) {
+		xal.append(XrlAtom(_my_int));
+	    }
+	    return _test_xrls_client.send_add_xrlx(
+	    		_receiver_target.c_str(), xal,
+			callback(this, &TestSender::send_next_pipeline_cb)
+			);
+	}
+	return false;
     }
 
     void start_transmission_cb(const XrlError& xrl_error) {
+	printf("start_transmission_cb %s\n", xrl_error.str().c_str());
 	if (xrl_error != XrlError::OKAY()) {
 	    XLOG_FATAL("Cannot start transmission: %s",
 		       xrl_error.str().c_str());
 	}
 	// XXX: select pipeline method
-#if (SEND_METHOD == SEND_METHOD_PIPELINE)
-	send_next_pipeline();
-#elif (SEND_METHOD == SEND_METHOD_START_PIPELINE)
-	send_next_start_pipeline();
-#elif (SEND_METHOD == SEND_METHOD_NO_PIPELINE)
-	send_next();
-#else
-	#error "SEND_METHOD typo"
-#endif
+	switch (g_send_method) {
+	case SEND_METHOD_PIPELINE:
+	    send_next_pipeline();
+	    break;
+	case SEND_METHOD_START_PIPELINE:
+	    send_next_start_pipeline();
+	    break;
+	case SEND_METHOD_NO_PIPELINE:
+	    send_next();
+	    break;
+	}
     }
 
     void send_next() {
@@ -245,8 +283,15 @@ private:
 	    print_xrl_sent();
 	    _next_xrl_send_id++;
 	    return;
+	} else {
+	    _try_again_timer =
+		_eventloop.new_oneoff_after(
+		    TimeVal(0, 1), callback(this, &TestSender::send_next)
+		    );
 	}
+#if PRINT_DEBUG
 	printf("send_next() failure\n");
+#endif
     }
 
     void send_next_cb(const XrlError& xrl_error) {
@@ -268,12 +313,24 @@ private:
 	}
 
 	success = transmit_xrl_next_pipeline();
+#if PRINT_DEBUG
+	printf("send_next_start_pipeline() %s\n", success ? "pass" : "fail");
+#endif
 	if (success) {
 	    print_xrl_sent();
 	    _next_xrl_send_id++;
 	    return;
+	} else {
+	    _try_again_timer =
+		_eventloop.new_oneoff_after(
+			TimeVal(0, 1),
+			callback(this, &TestSender::send_next_start_pipeline)
+			);
+	    return;
 	}
+#if PRINT_DEBUG
 	printf("send_next_start_pipeline() failure\n");
+#endif
     }
 
     void send_next_pipeline() {
@@ -294,28 +351,38 @@ private:
 		print_xrl_sent();
 		_next_xrl_send_id++;
 		continue;
+	    } else {
+		_try_again_timer =
+		    _eventloop.new_oneoff_after(
+			TimeVal(0, 1),
+			callback(this, &TestSender::send_next_pipeline)
+			);
+		return;
 	    }
 	} while (true);
     }
 
     void send_next_pipeline_cb(const XrlError& xrl_error) {
 	if (xrl_error != XrlError::OKAY()) {
-	    XLOG_FATAL("Cannot end transmission: %s",
-		       xrl_error.str().c_str());
+	    //XLOG_FATAL("Cannot end transmission: %s",
+	    //xrl_error.str().c_str());
 	}
 	print_xrl_received();
 	_next_xrl_recv_id++;
 
-#if (SEND_METHOD != SEND_METHOD_START_PIPELINE)
-	// XXX: don't blast anymore
-	send_next();
-#else	// XXX: a hack to send the rest of the XRLs after the first one
-	send_next_pipeline();
-#endif
+        if (g_send_method != SEND_METHOD_START_PIPELINE) {
+	    // XXX: don't blast anymore
+	    send_next();
+	} else {
+	    // XXX: a hack to send the rest of the XRLs after the first one
+	    send_next_pipeline();
+	}
     }
 
     void end_transmission() {
 	bool success;
+
+	_try_again_timer.unschedule();
 
 	if (_sent_end_transmission || _done)
 	    return;
@@ -335,13 +402,18 @@ private:
 
     void end_transmission_cb(const XrlError& xrl_error) {
 	if (xrl_error != XrlError::OKAY()) {
-	    XLOG_FATAL("Cannot end transmission: %s",
-		       xrl_error.str().c_str());
+	    _end_transmission_timer = _eventloop.new_oneoff_after(
+		TimeVal(0, 0),
+		callback(this, &TestSender::end_transmission));
+	    return;
+	    //	    XLOG_FATAL("Cannot end transmission: %s",
+	    //		       xrl_error.str().c_str());
 	}
 	start_transmission();
 #if SEND_DO_EXIT
 	if (_exit_timer.scheduled() == false)
-	    _exit_timer = _eventloop.set_flag_after_ms(60000, &_done);
+	    _exit_timer = _eventloop.set_flag_after_ms(TIMEOUT_EXIT_MS,
+						       &_done);
 #endif
 	return;
     }
@@ -352,6 +424,7 @@ private:
 
     XorpTimer			_start_transmission_timer;
     XorpTimer			_end_transmission_timer;
+    XorpTimer			_try_again_timer;
     XorpTimer			_exit_timer;
     size_t			_max_xrl_id;
     size_t			_next_xrl_send_id;
@@ -488,6 +561,14 @@ private:
 	UNUSED(data9);
     }
 
+    XrlCmdError
+    test_xrls_0_1_add_xrlx(const XrlAtomList &)
+    {
+	print_xrl_received();
+	_received_xrls++;
+	return XrlCmdError::OKAY();
+    }
+
     void print_statistics() {
 	TimeVal delta_time = _end_time - _start_time;
 
@@ -511,7 +592,8 @@ private:
 #if RECEIVE_DO_EXIT
 	// XXX: if enabled, then exit after all XRLs have been received.
 	if (_exit_timer.scheduled() == false)
-	    _exit_timer = _eventloop.set_flag_after_ms(60000, &_done);
+	    _exit_timer = _eventloop.set_flag_after_ms(TIMEOUT_EXIT_MS,
+						       &_done);
 #endif
 
 	return;
@@ -554,11 +636,20 @@ usage(const char *argv0, int exit_value)
     else
 	output = stderr;
 
-    fprintf(output, "Usage: %s [-F <finder_hostname>[:<finder_port>]]\n",
+    // Low we persist with this totally unconvential usage message :-D
+    fprintf(output, "Usage: %s [-F <finder_hostname>[:<finder_port>]] [options]\n",
 	    progname);
     fprintf(output, "           -F <finder_hostname>[:<finder_port>]  : finder hostname and port\n");
     fprintf(output, "           -h                                    : usage (this message)\n");
-    fprintf(output, "\n");
+    fprintf(output, "           -m <M>                                : send method (see below)\n");
+    fprintf(output, "           -n <count>                            : number of XrlAtoms in each Xrl call\n");
+    fprintf(output, "           -r                                    : run receiver in same process");
+
+    fprintf(output, "Send methods <M> :\n");
+    for (uint32_t i = 0; i < sizeof(send_methods) / sizeof(send_methods[0]); i++)
+	fprintf(output, "\t%u - %s\n", i, send_methods[i]);
+
+    fprintf(output, "\n\n");
     fprintf(output, "Program name:   %s\n", progname);
     fprintf(output, "Module name:    %s\n", XORP_MODULE_NAME);
     fprintf(output, "Module version: %s\n", XORP_MODULE_VERSION);
@@ -589,12 +680,18 @@ test_xrls_sender_main(const char* finder_hostname, uint16_t finder_port)
     //
     // Receiver
     //
-#if COMPILE_SINGLE_BINARY
-    XrlStdRouter xrl_std_router_test_receiver(eventloop, "test_xrl_receiver",
-					      finder_hostname, finder_port);
-    TestReceiver test_receiver(eventloop, &xrl_std_router_test_receiver);
-    wait_until_xrl_router_is_ready(eventloop, xrl_std_router_test_receiver);
-#endif
+    XrlStdRouter* xrl_std_router_test_receiver = NULL;
+    TestReceiver* test_receiver = NULL;
+    if (g_run_receiver == true) {
+	xrl_std_router_test_receiver = new XrlStdRouter(eventloop,
+							"test_xrl_receiver",
+							finder_hostname,
+							finder_port);
+	test_receiver = new TestReceiver(eventloop,
+					 xrl_std_router_test_receiver);
+	wait_until_xrl_router_is_ready(eventloop,
+				       *xrl_std_router_test_receiver);
+    }
 
     //
     // Start transmission
@@ -604,15 +701,17 @@ test_xrls_sender_main(const char* finder_hostname, uint16_t finder_port)
     //
     // Run everything
     //
-#if COMPILE_SINGLE_BINARY
-    while (! (test_sender.done() && test_receiver.done())) {
-	eventloop.run();
+    if (g_run_receiver == true) {
+	while (! (test_sender.done() && test_receiver->done())) {
+	    eventloop.run();
+	}
+	delete test_receiver;
+	delete xrl_std_router_test_receiver;
+    } else {
+	while (! test_sender.done()) {
+	    eventloop.run();
+	}
     }
-#else
-    while (! test_sender.done()) {
-	eventloop.run();
-    }
-#endif
 }
 
 int
@@ -637,7 +736,7 @@ main(int argc, char *argv[])
     //
     // Get the program options
     //
-    while ((ch = getopt(argc, argv, "F:h")) != -1) {
+    while ((ch = getopt(argc, argv, "F:hm:n:r")) != -1) {
 	switch (ch) {
 	case 'F':
 	    // Finder hostname and port
@@ -653,6 +752,18 @@ main(int argc, char *argv[])
 		finder_port = static_cast<uint16_t>(atoi(p));
 		finder_hostname = finder_hostname.substr(0, idx);
 	    }
+	    break;
+	case 'm':
+	    g_send_method = atoi(optarg);
+	    if (g_send_method >= sizeof(send_methods)/sizeof(send_methods[0]))
+		usage(argv0, 1);
+		// NOT REACHED
+	    break;
+	case 'n':
+	    g_atoms_per_xrl = atoi(optarg);
+	    break;
+	case 'r':
+	    g_run_receiver = true;
 	    break;
 	case 'h':
 	case '?':
@@ -672,6 +783,9 @@ main(int argc, char *argv[])
 	usage(argv0, 1);
 	// NOTREACHED
     }
+
+    fprintf(stdout, "XrlAtoms per call = %u\n", g_atoms_per_xrl);
+    fprintf(stdout, "Send method = %s\n", send_methods[g_send_method]);
 
     //
     // Run everything
