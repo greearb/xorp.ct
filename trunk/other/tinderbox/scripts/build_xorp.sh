a1f5d44a0ece0105b342add383702ae5b1a3db92
bms <bms@26435e26-4b25-483a-942d-477236992e84>
2004-10-19 08:26:48 +0000

    Major maintenance updates:
    * Add a commented ident at the top of the file.
    
    * Remove the timeout() function as it is now unused.
    * Remove the funkster() function as it is unused.
    
    * Comment the completion token mechanism used in this script.
    * Beautify the whitespace around the build command
      subshell invocation.
    * Record and print the build subshell exit status.
    
    * Add a comment about how additional improvements might be
      made to the subshell invocation.
    * Miscellaneous comment improvements.
    
    * Use POSIX shell arithmetic expansion instead of
      backtick-escaping the expr(1) command.
    * Use POSIX shell command substitution syntax instead of
      legacy backticks.
    
    * Do not call the timeout() function from within the loop
      used to poll for subshell completion; instead, set a
      variable ${timed_out} and check it upon exit.
    * If the subshell times out, kill only the subshell which
      timed out, not this shell's process group.
    
    NOTE: This script could benefit from further review
    regarding the use of POSIX /bin/sh job control, and that
    the use of such job control on FreeBSD requires that the
    process have a controlling terminal (i.e. that /dev/tty
    point to a real file descriptor), and that this can be
    forcibly created by ssh(1) with the switches '-t -t'.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@4798 26435e26-4b25-483a-942d-477236992e84

--- trunk/other/tinderbox/scripts/build_xorp.sh
+++ trunk/other/tinderbox/scripts/build_xorp.sh
@@ -1,65 +1,96 @@
 #!/bin/sh
+#
+# $XORP$
+#
 
 CONFIG="$(dirname $0)/config"
 . $CONFIG
 
-timeout()
-{
-    local err bar
-    err="Process appears wedged. Timed out after ${TIMEOUT} seconds."
-    bar=`echo $err | sed 's/./=/g'`
-    printf "\n${bar}\n${err}\n${bar}\n"
-    kill 0
-}
-
-# A test "process" that can be used to appear wedged.
-funkster()
-{
-    local iter
-    iter=0
-    while [ 1 ] ; do
-	printf "."
-	sleep 1
-	iter=`expr $iter + 1`
-	if [ $iter -gt 30 ] ; then
-	    break
-	fi
-    done
-}
-
 ###############################################################################
 
 XORPDIR=${ROOTDIR}/xorp
-WAKEFILE=".wake-$$"
-
-cd ${XORPDIR}
-rm -f ${WAKEFILE} 2>/dev/null
+wakefile=".wake-$$"
+build_command="./configure && gmake -k $@"
 
 #
-# This little nasty run these commands in the background.  We use
-# ${WAKEFILE} both to detect the completion of these commands and to
-# propagate the return value.
+# Remove any old completion tokens.
 #
+cd ${XORPDIR} && rm -f .wake-* 2>/dev/null
 
-CMD="./configure && gmake -k $@"
-#CMD=funkster
-( ( eval $CMD ) 2>&1 ; echo $? > ${WAKEFILE} ) &
+#
+# Spawn a background pipeline to run the build command and capture both
+# its output and its exit status. 
+# Note that no job control is available in a strict POSIX Bourne Shell.
+#
+# XXX: Try removing inner subshell to save on a fork (replace with {}),
+# previous problems were due to a typo above.
+#
+( ( eval $build_command ) 2>&1 ; \
+	subshell_status=$? ; \
+	echo ${subshell_status} > ${wakefile} \
+) & subshell_pid=$!
 
-expiry=`date "+%s"`
-expiry=`expr $expiry + ${TIMEOUT}`
-while [ ! -f ${WAKEFILE} ] ; do
-    now=`date "+%s"`
+#
+# Announce the invocation of the background pipeline in the parent.
+# Get current UTC epoch, and calculate when the timer is due to expire.
+#
+echo "Subshell launched with pid ${subshell_pid} at $(date '+%H:%M:%S %Z')."
+expiry=$(date "+%s")
+expiry=$((${expiry} + ${TIMEOUT}))
+
+#
+# Wait for command completion by polling for the creation of ${wakefile}
+# and the completion of the subshell. Use -s instead of -f for the test
+# to avoid racing the subshell when it has not yet flushed its output
+# to ${wakefile}. If we time out, break out of the loop and set a flag.
+#
+while [ ! -s ${wakefile} ] ; do
+    now=$(date "+%s")
     if [ "$now" -gt "$expiry" ] ; then
-	timeout
+	timed_out="1"
 	break
     fi
     sleep ${TIMEOUT_CHECK}
 done
 
-if [ -f ${WAKEFILE} ] ; then
-    err=`cat ${WAKEFILE}`
-    rm -f ${WAKEFILE}
-    exit $err
+#
+# If we timed out on notification from the subshell, kill the subshell's
+# entire process group.
+# XXX: Need to test what happens here using 'funkster' to simulate wedge.
+# XXX: Do we need to wait for the child anyway to catch it?
+# XXX: What if we've raced the child and we have no file? Erroneous error?
+#
+if [ "x${timed_out}" = "x1" ] ; then
+	err="Process appears wedged. Timed out after ${TIMEOUT} seconds."
+	bar=$(echo $err | sed 's/./=/g')
+	printf "\n${bar}\n${err}\n${bar}\n"
+	killcmd="kill -KILL -- -${subshell_pid} 2>/dev/null"
+	eval ${killcmd}
+	#
+	# More elaborate stuff re: try background kill, then kill the
+	# killer, below:
+	#
+	#&& echo "FAIL: ${killcmd} & kill_pid=$!
+	#wait ${kill_pid}
+	#kill -KILL ${kill_pid} 2>/dev/null && echo "FAIL: killing the killer"
+	#
 fi
 
+#
+# If we have a non-zero completion token, return the exit status within
+# from the subshell to the parent by exiting with it.
+# We have to use $(cat file) in straight up POSIX shells.
+# $(<${file}) -> GNUism. $(<file) -> KSHism.
+# XXX: Need to test what happens here using 'funkster' to simulate wedge.
+#
+if [ -s ${wakefile} ] ; then
+    err=$(cat ${wakefile})
+    echo "Subshell ran to completion with exit status ${err}."
+    rm -f ${wakefile}
+    exit ${err}
+fi
+
+#
+# Exit, returning failure; the job timed out.
+#
 exit 100
