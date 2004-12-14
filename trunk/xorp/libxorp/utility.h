9adcf6741be0ea4b558a564ce7daf380642b8674
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2004-12-14 18:10:10 +0000

    XORP code uses typedefs (e.g. uint32_t, int32_t) rather than using the base types, because the 'C' language allows a compiler to use a natural size for base type. The XORP code is internally consistent in this usage, one problem arises with format strings in printf style functions.
    In order to print a size_t or uint32_t with "%u" it is necessary to
    cast to an unsigned int. On Mac OS X a size_t is not an unsigned
    int. On windows uint32_t is not an unsigned int.
    
    In order to print a int32_t with a "%d" it is necessary to cast to
    a signed int. On windows int32_t is not a signed int.
    
    Two convenience macros to help with the casting.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@5115 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxorp/utility.h
+++ trunk/xorp/libxorp/utility.h
@@ -15,7 +15,7 @@
  */
 
 /*
- * $XORP: xorp/libxorp/utility.h,v 1.7 2004/02/12 02:35:08 pavlin Exp $
+ * $XORP: xorp/libxorp/utility.h,v 1.8 2004/06/10 22:41:23 hodson Exp $
  */
 
 #ifndef __LIBXORP_UTILITY_H__
@@ -40,6 +40,27 @@
 
 #ifdef __cplusplus
 #define cstring(s) (s).str().c_str()
+#endif
+
+/*
+ * XORP code uses typedefs (e.g. uint32_t, int32_t) rather than using
+ * the base types, because the 'C' language allows a compiler to use a
+ * natural size for base type. The XORP code is internally consistent
+ * in this usage, one problem arises with format strings in printf
+ * style functions.
+ *
+ * In order to print a size_t or uint32_t with "%u" it is necessary to
+ * cast to an unsigned int. On Mac OS X a size_t is not an unsigned
+ * int. On windows uint32_t is not an unsigned int.
+ *
+ * In order to print a int32_t with a "%d" it is necessary to cast to
+ * a signed int. On windows int32_t is not a signed int.
+ *
+ * The two convenience macros are provided to perform the cast.
+ */
+#ifdef __cplusplus
+#define	XORP_UINT_CAST(x)	static_cast<unsigned int>(x)
+#define	XORP_INT_CAST(x)	static_cast<int>(x)
 #endif
 
 #define ADD_POINTER(pointer, size, type)				\
