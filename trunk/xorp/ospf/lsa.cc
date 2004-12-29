5823a3fc3cc801d80ca02309a0bb36d2291967b2
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2004-12-29 09:31:39 +0000

    LSA header encoder and decoder.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@5194 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/lsa.cc
+++ trunk/xorp/ospf/lsa.cc
@@ -34,8 +34,62 @@
 #include "ospf.hh"
 #include "lsa.hh"
 
+/**
+ * A LSA header is a fixed length assume that we have been passed our
+ * 20 bytes.
+ */
 Lsa_header
-Lsa_header::decode(uint8_t *ptr, size_t len) throw(BadPacket)
+Lsa_header::decode(uint8_t *ptr) throw(BadPacket)
 {
-    
+    OspfTypes::Version version = get_version();
+
+    Lsa_header header(version);
+
+    header.set_ls_age(extract_16(&ptr[0]));
+
+    switch(version) {
+    case OspfTypes::V2:
+	header.set_options(ptr[2]);
+	header.set_ls_type(ptr[3]);
+	break;
+    case OspfTypes::V3:
+	header.set_ls_type(extract_16(&ptr[2]));
+	break;
+    }
+
+    header.set_link_state_id(extract_32(&ptr[4]));
+    header.set_advertising_router(extract_32(&ptr[8]));
+    header.set_ls_sequence_number(extract_32(&ptr[12]));
+    header.set_ls_checksum(extract_16(&ptr[16]));
+    header.set_length(extract_16(&ptr[18]));
+
+    return header;
+}
+
+size_t
+Lsa_header::copy_out(uint8_t *ptr) const
+{
+    OspfTypes::Version version = get_version();
+
+    Lsa_header header(version);
+
+    embed_16(&ptr[0], get_ls_age());
+
+    switch(version) {
+    case OspfTypes::V2:
+	ptr[2] = get_options();
+	ptr[3] = get_ls_type();
+	break;
+    case OspfTypes::V3:
+	embed_16(&ptr[2], get_ls_type());
+	break;
+    }
+
+    embed_32(&ptr[4], get_link_state_id());
+    embed_32(&ptr[8], get_advertising_router());
+    embed_32(&ptr[12], get_ls_sequence_number());
+    embed_16(&ptr[16], get_ls_checksum());
+    embed_16(&ptr[18], get_length());
+
+    return 20;
 }
--- trunk/xorp/ospf/lsa.hh
+++ trunk/xorp/ospf/lsa.hh
@@ -22,7 +22,7 @@
  *
  * A generic LSA. All actual LSAs should be derived from this LSA.
  */
-
+#if	0
 class Lsa {
  public:
     /**
@@ -65,5 +65,162 @@ class LsaTransmit : class Transmit {
  private:
     LsaRef _lsaref;	// LSA.
 }
+#endif
+
+class BadPacket;
+
+/**
+ * LSA Header.
+ */
+class Lsa_header {
+ public:
+    Lsa_header(OspfTypes::Version version) :
+	_version(version), _LS_age(0), _options(0), _ls_type(0),
+	_link_state_id(0), _advertising_router(0), _ls_sequence_number(0),
+	_ls_checksum(0), _length(0)
+    {}
+
+    Lsa_header(const Lsa_header& rhs) {
+	copy(rhs);
+    }
+
+    Lsa_header operator=(const Lsa_header& rhs) {
+	if(&rhs == this)
+	    return *this;
+	copy(rhs);
+	return *this;
+    }
+
+#define	lsa_copy(var)	var = rhs.var;
+
+    void copy(const Lsa_header& rhs) {
+	lsa_copy(_version);
+	lsa_copy(_LS_age);
+	lsa_copy(_options);
+	lsa_copy(_ls_type);
+	lsa_copy(_link_state_id);
+	lsa_copy(_advertising_router);
+	lsa_copy(_ls_sequence_number);
+	lsa_copy(_ls_checksum);
+	lsa_copy(_length);
+    }
+#undef	lsa_copy
+
+    /**
+     * @return the length of an LSA header.
+     */
+    static size_t length() { return 20; }
+
+    /**
+     * Decode a LSA header and return a LSA header inline not a pointer.
+     */
+    Lsa_header
+    decode(uint8_t *ptr) throw(BadPacket);
+    
+    /**
+     * Copy a wire format representation to the pointer provided.
+     * @return the number of butes written.
+     */
+    size_t copy_out(uint8_t *to_uint8) const;
+
+    OspfTypes::Version get_version() const {
+	return _version;
+    }
+
+    // LS age
+    void set_ls_age(uint16_t ls_age) {
+	_LS_age = ls_age;
+    }
+
+    uint16_t get_ls_age() const {
+	return _LS_age;
+    }
+
+    // Options
+    void set_options(uint8_t options) {
+	XLOG_ASSERT(OspfTypes::V2 == get_version());
+	_options = options;
+    }
+
+    uint8_t get_options() const {
+	XLOG_ASSERT(OspfTypes::V2 == get_version());
+	return _options;
+    }
+
+    // LS type
+    void set_ls_type(uint16_t ls_type) {
+	switch(get_version()) {
+	case OspfTypes::V2:
+	    if (ls_type > 0xff)
+		XLOG_WARNING("Attempt to set %#x in an 8 bit field",
+			     ls_type);
+	    _ls_type = ls_type & 0xff;
+	    break;
+	case OspfTypes::V3:
+	    _ls_type = ls_type;
+	    break;
+	}
+    }
+
+    uint16_t get_ls_type() const {
+	return _ls_type;
+    }
+
+    // Link State ID
+    void set_link_state_id(uint32_t link_state_id) {
+	_link_state_id = link_state_id;
+    }
+
+    uint32_t get_link_state_id() const {
+	return _link_state_id;
+    }
+    
+    // Advertising Router
+    void set_advertising_router(uint32_t advertising_router) {
+	_advertising_router = advertising_router;
+    }
+
+    uint32_t get_advertising_router() const {
+	return _advertising_router;
+    }
+
+    // LS sequence number
+    void set_ls_sequence_number(uint32_t ls_sequence_number) {
+	_ls_sequence_number = ls_sequence_number;
+    }
+
+    uint32_t get_ls_sequence_number() const {
+	return _ls_sequence_number;
+    }
+
+    // LS checksum
+    void set_ls_checksum(uint16_t ls_checksum) {
+	_ls_checksum = ls_checksum;
+    }
+
+    uint16_t get_ls_checksum() const {
+	return _ls_checksum;
+    }
+
+    // Length
+    void set_length(uint16_t length) {
+	_length = length;
+    }
+
+    uint16_t get_length() const {
+	return _length;
+    }
+
+ private:
+    OspfTypes::Version _version;
+    uint16_t 	_LS_age;
+    uint8_t	_options;	// OSPF V2 Only  
+    uint16_t	_ls_type;	// OSPF V2 1 byte OSPF V3 2 bytes.
+    uint32_t	_link_state_id;
+    uint32_t	_advertising_router;
+    uint32_t	_ls_sequence_number;
+    uint16_t	_ls_checksum;
+    uint16_t	_length;
+};
 
 #endif // __OSPF_LSA_HH__
