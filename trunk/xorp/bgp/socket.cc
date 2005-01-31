ea282a0fa6d805454efc3414a4ec17b7ab3b4d9d
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2005-01-31 22:25:12 +0000

    Add XORP_UINT_CAST() casting as appropriate.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@5327 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/socket.cc
+++ trunk/xorp/bgp/socket.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/socket.cc,v 1.20 2004/12/14 19:59:11 atanu Exp $"
+#ident "$XORP: xorp/bgp/socket.cc,v 1.21 2005/01/28 23:56:15 bms Exp $"
 
 // #define DEBUG_LOGGING 
 // #define DEBUG_PRINT_FUNCTION_NAME 
@@ -271,8 +271,9 @@ SocketClient::async_read_message(AsyncFileWriter::Event ev,
 		const size_t buf_bytes,	// desired message size
 		const size_t offset)	// where we got so far (next free byte)
 {
-    debug_msg("async_read_message %d %u %u %s\n", ev, (uint32_t)buf_bytes,
-	      (uint32_t)offset, get_remote_host());
+    debug_msg("async_read_message %d %u %u %s\n", ev,
+	      XORP_UINT_CAST(buf_bytes), XORP_UINT_CAST(offset),
+	      get_remote_host());
 
     XLOG_ASSERT(_async_reader);
 
@@ -285,7 +286,8 @@ SocketClient::async_read_message(AsyncFileWriter::Event ev,
 	    size_t fh_length = ntohs(header->length);
 
 	    if (fh_length < MINPACKETSIZE || fh_length > sizeof(_read_buf)) {
-		XLOG_ERROR("Illegal length value %u", (uint32_t)fh_length);
+		XLOG_ERROR("Illegal length value %u",
+			   XORP_UINT_CAST(fh_length));
 		if (!_callback->dispatch(BGPPacket::ILLEGAL_MESSAGE_LENGTH,
 					buf, buf_bytes))
 		    return;
@@ -367,7 +369,8 @@ SocketClient::send_message(const uint8_t* buf,
 			   const size_t	cnt,
 			   SendCompleteCallback cb)
 {
-    debug_msg("peer %s bytes = %u\n", get_remote_host(), (uint32_t)cnt);
+    debug_msg("peer %s bytes = %u\n", get_remote_host(),
+	      XORP_UINT_CAST(cnt));
 
     if(!is_connected()) {
 #ifdef	DEBUG_LOGGING
