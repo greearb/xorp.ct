6dd59807d855e768026ee362f44a4d8cb70752ab
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2005-02-01 08:40:24 +0000

    Fix a typo from the previous commit.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@5362 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/rip/test_auth.cc
+++ trunk/xorp/rip/test_auth.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/rip/test_auth.cc,v 1.10 2005/01/07 03:07:36 atanu Exp $"
+#ident "$XORP: xorp/rip/test_auth.cc,v 1.11 2005/02/01 08:31:07 pavlin Exp $"
 
 #include "rip_module.h"
 
@@ -274,7 +274,7 @@ test_main()
 	}
 
 	if (check_auth_packet(pkt, nah, n, false)) {
-	    verbose_log("Failed null authentication with % entries\n",
+	    verbose_log("Failed null authentication with %u entries\n",
 			XORP_UINT_CAST(n));
 	    return 1;
 	}
