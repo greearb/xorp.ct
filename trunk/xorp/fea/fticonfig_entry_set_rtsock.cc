1a526be4f6011810ac036b7ea1981c5b00b4286d
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2005-03-31 19:32:51 +0000

    * Bug fix:   If we try to add an interface-specific route, then use   sockaddr_dl with the interface name as the RTA_GATEWAY information   in case of all OS-es, not only MacOS X.   Previously, the interface information for such routes was set   properly, but "netstat -rn" displayed 0.0.0.0 or :: instead of, say,   "link#4" as the Gateway information, which didn't seem right.
    * Bug fix:
      The size of all sockaddr* structures used by the routing sockets
      should be rounded-up to the next sizeof(long) boundary.
      This isn't an issue with sockaddr_in and sockaddr_in6, because
      their size is multiple of sizeof(long), but is not the case
      of sockaddr_dl on FreeBSD.
      Now the code appropriately rounds-up the size of all sockaddr*
      used by the routing sockets to the next sizeof(long) boundary.
      [Note: this bug was revealed by the previous bug fix after the
       introduction of sockaddr_dl for storing the RTA_GATEWAY information]
    
    * Some additional cleanup: e.g., return an error instead of XLOG_ASSERT
      if the interface name of the route to add is unknown, etc.
    
    The new fix is tested on {Free,Net,Open}BSD and MacOS X 10.3
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@5710 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/fticonfig_entry_set_rtsock.cc
+++ trunk/xorp/fea/fticonfig_entry_set_rtsock.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/fticonfig_entry_set_rtsock.cc,v 1.27 2005/03/25 02:53:03 pavlin Exp $"
+#ident "$XORP: xorp/fea/fticonfig_entry_set_rtsock.cc,v 1.28 2005/03/29 20:14:00 pavlin Exp $"
 
 #include "fea_module.h"
 
@@ -131,34 +131,61 @@ FtiConfigEntrySetRtsock::delete_entry(const FteX& )
 }
 
 #else // HAVE_ROUTING_SOCKETS
+
+//
+// XXX: Define a number of constants with the size of the corresponding
+// sockaddr structure rounded-up to the next sizeof(long) boundary.
+// This is needed, because the routing socket expects the sockaddr
+// structures to begin on the boundary of (long) words.
+//
+#define LONG_ROUNDUP_SIZEOF(type)					\
+	(sizeof(type) % sizeof(long)) ?					\
+	(sizeof(type) + sizeof(long) - (sizeof(type) % sizeof(long)))	\
+	: (sizeof(type))
+static const size_t SOCKADDR_IN_ROUNDUP_LEN = LONG_ROUNDUP_SIZEOF(struct sockaddr_in);
+#ifdef HAVE_IPV6
+static const size_t SOCKADDR_IN6_ROUNDUP_LEN = LONG_ROUNDUP_SIZEOF(struct sockaddr_in6);
+#endif
+#ifdef AF_LINK
+static const size_t SOCKADDR_DL_ROUNDUP_LEN = LONG_ROUNDUP_SIZEOF(struct sockaddr_dl);
+#endif
+#undef LONG_ROUNDUP_SIZEOF
+
 bool
 FtiConfigEntrySetRtsock::add_entry(const FteX& fte)
 {
     static const size_t	buffer_size = sizeof(struct rt_msghdr) + 512;
     char		buffer[buffer_size];
     struct rt_msghdr	*rtm;
-    struct sockaddr_in	*sin_dst, *sin_netmask, *sin_last;
+    struct sockaddr_in	*sin_dst, *sin_netmask;
     struct sockaddr_in	*sin_nexthop = NULL;
     RoutingSocket&	rs = *this;
     int			family = fte.net().af();
 #ifdef AF_LINK
     struct sockaddr_dl*	sdl = NULL;
+    size_t		sdl_len = 0;
 #endif
-    bool		is_nexthop_sockaddr_dl = false;
+    size_t		sin_dst_len = 0;
+    size_t		sin_nexthop_len = 0;
+    size_t		sin_netmask_len = 0;
+    bool		is_host_route = fte.is_host_route();
     bool		is_interface_route = false;
+    bool		is_nexthop_sockaddr_dl = false;
+    bool		is_discard_route = false;
+    IPvX		fte_nexthop = fte.nexthop();
 
     debug_msg("add_entry "
 	      "(network = %s nexthop = %s)",
-	      fte.net().str().c_str(), fte.nexthop().str().c_str());
+	      fte.net().str().c_str(), fte_nexthop.str().c_str());
 
     // Check that the family is supported
     do {
-	if (fte.nexthop().is_ipv4()) {
+	if (fte_nexthop.is_ipv4()) {
 	    if (! ftic().have_ipv4())
 		return false;
 	    break;
 	}
-	if (fte.nexthop().is_ipv6()) {
+	if (fte_nexthop.is_ipv6()) {
 	    if (! ftic().have_ipv6())
 		return false;
 	    break;
@@ -172,17 +199,39 @@ FtiConfigEntrySetRtsock::add_entry(const FteX& fte)
     if (! fte.ifname().empty())
 	is_interface_route = true;
 
+    do {
+	//
+	// Check for a discard route; the referenced ifname must have the
+	// discard property. The next-hop is forcibly rewritten to be the
+	// loopback address, in order for the RTF_BLACKHOLE flag to take
+	// effect on BSD platforms.
+	//
+	if (fte.ifname().empty())
+	    break;
+	IfTree& it = ftic().iftree();
+	IfTree::IfMap::const_iterator ii = it.get_if(fte.ifname());
+	if (ii == it.ifs().end()) {
+	    XLOG_ERROR("Invalid interface name: %s", fte.ifname().c_str());
+	    return false;
+	}
+	if (ii->second.discard()) {
+	    is_discard_route = true;
+	    fte_nexthop = IPvX::LOOPBACK(family);
+	}
+	break;
+    } while (false);
+
 #ifdef AF_LINK
-#ifdef HOST_OS_MACOSX
     //
-    // XXX: If we want to add an interface-specific route,
-    // then in case of Mac OS X the nexthop (RTA_GATEWAY) must be
-    // "struct sockaddr_dl" with the interface information.
+    // XXX: If we want to add an interface-specific route, and if
+    // there is no nexthop IP address, then the nexthop (RTA_GATEWAY)
+    // must be "struct sockaddr_dl" with the interface information.
+    // Note: this check must ba after the discard route check, because
+    // the discard route check may overwrite fte_nexthop.
     //
-    if (is_interface_route && (fte.nexthop() == IPvX::ZERO(family))) {
+    if (is_interface_route && (fte_nexthop == IPvX::ZERO(family))) {
 	is_nexthop_sockaddr_dl = true;
     }
-#endif // HOST_OS_MACOSX
 #endif // AF_LINK
 
     //
@@ -190,48 +239,53 @@ FtiConfigEntrySetRtsock::add_entry(const FteX& fte)
     //
     memset(buffer, 0, sizeof(buffer));
     rtm = (struct rt_msghdr *)buffer;
+    rtm->rtm_msglen = sizeof(*rtm);
     
     switch (family) {
     case AF_INET:
-	rtm->rtm_msglen = sizeof(*rtm) + 2 * sizeof(struct sockaddr_in);
 	sin_dst = (struct sockaddr_in *)(rtm + 1);
+	sin_dst_len = SOCKADDR_IN_ROUNDUP_LEN;
+	rtm->rtm_msglen += sin_dst_len;
 	if (is_nexthop_sockaddr_dl) {
 #ifdef AF_LINK
-	    sdl = ADD_POINTER(sin_dst, sizeof(struct sockaddr_in),
-			      struct sockaddr_dl *);
-	    sin_netmask = ADD_POINTER(sdl, sizeof(struct sockaddr_dl),
-				      struct sockaddr_in *);
-	    rtm->rtm_msglen += sizeof(struct sockaddr_dl);
+	    sdl = ADD_POINTER(sin_dst, sin_dst_len, struct sockaddr_dl *);
+	    sdl_len = SOCKADDR_DL_ROUNDUP_LEN;
+	    sin_netmask = ADD_POINTER(sdl, sdl_len, struct sockaddr_in *);
+	    sin_netmask_len = SOCKADDR_IN_ROUNDUP_LEN;
+	    rtm->rtm_msglen += sdl_len + sin_netmask_len;
 #endif // AF_LINK
 	} else {
-	    sin_nexthop = ADD_POINTER(sin_dst, sizeof(struct sockaddr_in),
+	    sin_nexthop = ADD_POINTER(sin_dst, sin_dst_len,
 				      struct sockaddr_in *);
-	    sin_netmask = ADD_POINTER(sin_nexthop, sizeof(struct sockaddr_in),
+	    sin_nexthop_len = SOCKADDR_IN_ROUNDUP_LEN;
+	    sin_netmask = ADD_POINTER(sin_nexthop, sin_nexthop_len,
 				      struct sockaddr_in *);
-	    rtm->rtm_msglen += sizeof(struct sockaddr_in);
+	    sin_netmask_len = SOCKADDR_IN_ROUNDUP_LEN;
+	    rtm->rtm_msglen += sin_nexthop_len + sin_netmask_len;
 	}
-	sin_last = sin_netmask;
 	break;
 #ifdef HAVE_IPV6
     case AF_INET6:
-	rtm->rtm_msglen = sizeof(*rtm) + 2 * sizeof(struct sockaddr_in6);
 	sin_dst = (struct sockaddr_in *)(rtm + 1);
+	sin_dst_len = SOCKADDR_IN6_ROUNDUP_LEN;
+	rtm->rtm_msglen += sin_dst_len;
 	if (is_nexthop_sockaddr_dl) {
 #ifdef AF_LINK
-	    sdl = ADD_POINTER(sin_dst, sizeof(struct sockaddr_in6),
-			      struct sockaddr_dl *);
-	    sin_netmask = ADD_POINTER(sdl, sizeof(struct sockaddr_dl),
-				      struct sockaddr_in *);
-	    rtm->rtm_msglen += sizeof(struct sockaddr_dl);
+	    sdl = ADD_POINTER(sin_dst, sin_dst_len, struct sockaddr_dl *);
+	    sdl_len = SOCKADDR_DL_ROUNDUP_LEN;
+	    sin_netmask = ADD_POINTER(sdl, sdl_len, struct sockaddr_in *);
+	    sin_netmask_len = SOCKADDR_IN6_ROUNDUP_LEN;
+	    rtm->rtm_msglen += sdl_len + sin_netmask_len;
 #endif // AF_LINK
 	} else {
-	    sin_nexthop = ADD_POINTER(sin_dst, sizeof(struct sockaddr_in6),
+	    sin_nexthop = ADD_POINTER(sin_dst, sin_dst_len,
 				      struct sockaddr_in *);
-	    sin_netmask = ADD_POINTER(sin_nexthop, sizeof(struct sockaddr_in6),
+	    sin_nexthop_len = SOCKADDR_IN6_ROUNDUP_LEN;
+	    sin_netmask = ADD_POINTER(sin_nexthop, sin_nexthop_len,
 				      struct sockaddr_in *);
-	    rtm->rtm_msglen += sizeof(struct sockaddr_in6);
+	    sin_netmask_len = SOCKADDR_IN6_ROUNDUP_LEN;
+	    rtm->rtm_msglen += sin_nexthop_len + sin_netmask_len;
 	}
-	sin_last = sin_netmask;
 	break;
 #endif // HAVE_IPV6
     default:
@@ -242,9 +296,11 @@ FtiConfigEntrySetRtsock::add_entry(const FteX& fte)
     rtm->rtm_type = RTM_ADD;
     rtm->rtm_index = 0;			// XXX: not used by the kernel (?)
     rtm->rtm_addrs = (RTA_DST | RTA_GATEWAY | RTA_NETMASK);
-    if (fte.is_host_route())
+    if (is_host_route)
 	rtm->rtm_flags |= RTF_HOST;
-    if ((fte.nexthop() != IPvX::ZERO(family)) && (! is_nexthop_sockaddr_dl))
+    if (is_discard_route)
+	rtm->rtm_flags |= RTF_BLACKHOLE;
+    if ((fte_nexthop != IPvX::ZERO(family)) && (! is_nexthop_sockaddr_dl))
 	rtm->rtm_flags |= RTF_GATEWAY;
     rtm->rtm_flags |= RTF_PROTO1;	// XXX: mark this as a XORP route
     rtm->rtm_flags |= RTF_UP;		// XXX: mark this route as UP
@@ -253,9 +309,19 @@ FtiConfigEntrySetRtsock::add_entry(const FteX& fte)
 
     // Copy the destination, the nexthop, and the netmask addresses
     fte.net().masked_addr().copy_out(*sin_dst);
-    if (sin_nexthop != NULL)
-	fte.nexthop().copy_out(*sin_nexthop);
+#ifdef HAVE_SIN_LEN
+    sin_dst->sin_len = sin_dst_len;
+#endif
+    if (sin_nexthop != NULL) {
+	fte_nexthop.copy_out(*sin_nexthop);
+#ifdef HAVE_SIN_LEN
+	sin_nexthop->sin_len = sin_nexthop_len;
+#endif
+    }
     fte.net().netmask().copy_out(*sin_netmask);
+#ifdef HAVE_SIN_LEN
+    sin_netmask->sin_len = sin_netmask_len;
+#endif
 
     if (is_interface_route) {
 	//
@@ -268,29 +334,15 @@ FtiConfigEntrySetRtsock::add_entry(const FteX& fte)
 	    //
 	    // Add extra space for data-link sockaddr_dl for the RTA_IFP flag
 	    //
-	    rtm->rtm_msglen += sizeof(struct sockaddr_dl);
+	    sdl = ADD_POINTER(sin_netmask, sin_netmask_len,
+			      struct sockaddr_dl *);
+	    sdl_len = SOCKADDR_DL_ROUNDUP_LEN;
+	    rtm->rtm_msglen += sdl_len;
 	    rtm->rtm_addrs |= RTA_IFP;
-	    switch (family) {
-	    case AF_INET:
-		sdl = ADD_POINTER(sin_last, sizeof(struct sockaddr_in),
-				  struct sockaddr_dl *);
-		sin_last = reinterpret_cast<struct sockaddr_in *>(sdl);
-		break;
-#ifdef HAVE_IPV6
-	    case AF_INET6:
-		sdl = ADD_POINTER(sin_last, sizeof(struct sockaddr_in6),
-				  struct sockaddr_dl*);
-		sin_last = reinterpret_cast<struct sockaddr_in *>(sdl);
-		break;
-#endif // HAVE_IPV6
-	    default:
-		XLOG_UNREACHABLE();
-		break;
-	    }
 	}
 
 	sdl->sdl_family = AF_LINK;
-	sdl->sdl_len = sizeof(struct sockaddr_dl);
+	sdl->sdl_len = sdl_len;
 	IfTree& it = ftic().iftree();
 	IfTree::IfMap::const_iterator ii = it.get_if(fte.ifname());
 	if (ii == it.ifs().end()) {
@@ -307,26 +359,6 @@ FtiConfigEntrySetRtsock::add_entry(const FteX& fte)
 	}
 #endif // AF_LINK
     }
-
-    do {
-	//
-	// Check for a discard route; the referenced ifname must have the
-	// discard property. The next-hop is forcibly rewritten to be the
-	// loopback address, in order for the RTF_BLACKHOLE flag to take
-	// effect on BSD platforms.
-	//
-	if (fte.ifname().empty())
-	    break;
-	IfTree& it = ftic().iftree();
-	IfTree::IfMap::const_iterator ii = it.get_if(fte.ifname());
-	XLOG_ASSERT(ii != it.ifs().end());
-
-	if (ii->second.discard()) {
-	    IPvX::LOOPBACK(family).copy_out(*sin_nexthop);
-	    rtm->rtm_flags |= (RTF_BLACKHOLE | RTF_GATEWAY);
-	}
-	break;
-    } while (false);
 
     if (rs.write(rtm, rtm->rtm_msglen) != rtm->rtm_msglen) {
 	XLOG_ERROR("Error writing to routing socket: %s", strerror(errno));
@@ -350,6 +382,9 @@ FtiConfigEntrySetRtsock::delete_entry(const FteX& fte)
     struct sockaddr_in	*sin_dst, *sin_netmask = NULL;
     RoutingSocket&	rs = *this;
     int			family = fte.net().af();
+    size_t		sin_dst_len = 0;
+    size_t		sin_netmask_len = 0;
+    bool		is_host_route = fte.is_host_route();
 
     debug_msg("delete_entry "
 	      "(network = %s nexthop = %s)",
@@ -378,25 +413,30 @@ FtiConfigEntrySetRtsock::delete_entry(const FteX& fte)
     //
     memset(buffer, 0, sizeof(buffer));
     rtm = (struct rt_msghdr *)buffer;
+    rtm->rtm_msglen = sizeof(*rtm);
     
     switch (family) {
     case AF_INET:
-	rtm->rtm_msglen = sizeof(*rtm) + sizeof(struct sockaddr_in);
 	sin_dst = (struct sockaddr_in *)(rtm + 1);
-	if (! fte.is_host_route()) {
-	    rtm->rtm_msglen += sizeof(struct sockaddr_in);
-	    sin_netmask = ADD_POINTER(sin_dst, sizeof(struct sockaddr_in),
+	sin_dst_len = SOCKADDR_IN_ROUNDUP_LEN;
+	rtm->rtm_msglen += sin_dst_len;
+	if (! is_host_route) {
+	    sin_netmask = ADD_POINTER(sin_dst, sin_dst_len,
 				      struct sockaddr_in *);
+	    sin_netmask_len = SOCKADDR_IN_ROUNDUP_LEN;
+	    rtm->rtm_msglen += sin_netmask_len;
 	}
 	break;
 #ifdef HAVE_IPV6
     case AF_INET6:
-	rtm->rtm_msglen = sizeof(*rtm) + sizeof(struct sockaddr_in6);
 	sin_dst = (struct sockaddr_in *)(rtm + 1);
-	if (! fte.is_host_route()) {
-	    rtm->rtm_msglen += sizeof(struct sockaddr_in6);
-	    sin_netmask = ADD_POINTER(sin_dst, sizeof(struct sockaddr_in6),
+	sin_dst_len = SOCKADDR_IN6_ROUNDUP_LEN;
+	rtm->rtm_msglen += sin_dst_len;
+	if (! is_host_route) {
+	    sin_netmask = ADD_POINTER(sin_dst, sin_dst_len,
 				      struct sockaddr_in *);
+	    sin_netmask_len = SOCKADDR_IN6_ROUNDUP_LEN;
+	    rtm->rtm_msglen += sin_netmask_len;
 	}
 	break;
 #endif // HAVE_IPV6
@@ -408,7 +448,7 @@ FtiConfigEntrySetRtsock::delete_entry(const FteX& fte)
     rtm->rtm_type = RTM_DELETE;
     rtm->rtm_index = 0;			// XXX: not used by the kernel (?)
     rtm->rtm_addrs = RTA_DST;
-    if (! fte.is_host_route())
+    if (! is_host_route)
 	rtm->rtm_addrs |= RTA_NETMASK;
     rtm->rtm_flags = 0;
     rtm->rtm_pid = rs.pid();
@@ -416,8 +456,16 @@ FtiConfigEntrySetRtsock::delete_entry(const FteX& fte)
     
     // Copy the destination, and the netmask addresses (if needed)
     fte.net().masked_addr().copy_out(*sin_dst);
-    if (! fte.is_host_route())
+#ifdef HAVE_SIN_LEN
+    sin_dst->sin_len = sin_dst_len;
+#endif
+
+    if (! is_host_route) {
 	fte.net().netmask().copy_out(*sin_netmask);
+#ifdef HAVE_SIN_LEN
+	sin_netmask->sin_len = sin_netmask_len;
+#endif
+    }
     
     if (rs.write(rtm, rtm->rtm_msglen) != rtm->rtm_msglen) {
 	XLOG_ERROR("Error writing to routing socket: %s", strerror(errno));
