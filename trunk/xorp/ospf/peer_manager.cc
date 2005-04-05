a2ef953ae6b380064cdf98127804fb49f7440a19
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-04-05 00:48:33 +0000

    The peer manager is now creating area routers and peers.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@5735 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/peer_manager.cc
+++ trunk/xorp/ospf/peer_manager.cc
@@ -21,6 +21,7 @@
 #include "config.h"
 #include <map>
 #include <list>
+#include <set>
 
 #include "ospf_module.h"
 
@@ -33,13 +34,181 @@
 
 #include "ospf.hh"
 #include "packet.hh"
+#include "peer.hh"
+#include "area_router.hh"
 #include "peer_manager.hh"
 
 template <typename A>
-void
-PeerManager<A>::incoming_packet(Packet *packet)
+PeerManager<A>::~PeerManager()
 {
-    debug_msg("%s\n", cstring((*packet)));
+    XLOG_UNFINISHED();
+}
+
+template <typename A>
+bool
+PeerManager<A>::create_area_router(OspfTypes::AreaID area,
+				   OspfTypes::AreaType area_type)
+{
+    debug_msg("Area %s Type %s\n", area.str().c_str(), 
+	      pp_area_type(area_type).c_str());
+
+    // Check this area doesn't already exist.
+    if (0 != _areas.count(area)) {
+	XLOG_ERROR("Area %s already exists\n", area.str().c_str());
+	return false;
+    }
+
+    _areas[area] = new AreaRouter<A>(_ospf, area, area_type);
+    
+    return true;
+}
+
+template <typename A>
+AreaRouter<A> *
+PeerManager<A>::get_area_router(OspfTypes::AreaID area)
+{
+    debug_msg("Area %s\n", area.str().c_str());
+
+    // Check this area exists.
+    if (0 == _areas.count(area)) {
+	XLOG_ERROR("Area %s doesn't exist\n", area.str().c_str());
+	return 0;
+    }
+
+    return _areas[area];
+}
+
+template <typename A>
+bool
+PeerManager<A>::destroy_area_router(OspfTypes::AreaID area)
+{
+    debug_msg("Area %s\n", area.str().c_str());
+
+    // Check this area doesn't already exist.
+    if (0 == _areas.count(area)) {
+	XLOG_ERROR("Area %s doesn't exist\n", area.str().c_str());
+	return false;
+    }
+
+    // Notify the peers that this area is being removed. If this is
+    // the only area that the peer belonged to the peer can signify
+    // this and the peer can be removed.
+    typename map<PeerID, Peer<A> *>::iterator i;
+    for(i = _peers.begin(); i != _peers.end(); i++)
+	if ((*i).second->remove_area(area))
+	    delete_peer((*i).first);
+
+    delete _areas[area];
+    _areas.erase(_areas.find(area));
+
+    return true;
+}
+
+template <typename A>
+PeerID
+PeerManager<A>::create_peerid(const string& interface, const string& vif)
+    throw(BadPeer)
+{
+    debug_msg("Interface %s Vif %s\n", interface.c_str(), vif.c_str());
+    string concat = interface + "/" + vif;
+
+    if (0 != _pmap.count(concat))
+	xorp_throw(BadPeer,
+		   c_format("Mapping for %s already exists", concat.c_str()));
+			    
+    PeerID peerid = _next_peerid++;
+    _pmap[concat] = peerid;
+
+    return peerid;
+}
+
+template <typename A>
+PeerID
+PeerManager<A>::get_peerid(const string& interface, const string& vif)
+    throw(BadPeer)
+{
+    debug_msg("Interface %s Vif %s\n", interface.c_str(), vif.c_str());
+    string concat = interface + "/" + vif;
+
+    if (0 == _pmap.count(concat))
+	xorp_throw(BadPeer, 
+		   c_format("No mapping for %s exists", concat.c_str()));
+			    
+    return _pmap[concat];
+}
+
+template <typename A>
+void
+PeerManager<A>::destroy_peerid(const string& interface, const string& vif)
+    throw(BadPeer)
+{
+    debug_msg("Interface %s Vif %s\n", interface.c_str(), vif.c_str());
+    string concat = interface + "/" + vif;
+
+    if (0 == _pmap.count(concat))
+	xorp_throw(BadPeer, 
+		   c_format("No mapping for %s exists", concat.c_str()));
+			    
+    _pmap.erase(_pmap.find(concat));
+}
+
+template <typename A>
+bool
+PeerManager<A>::create_peer(const string& interface, const string& vif,
+    OspfTypes::LinkType linktype, OspfTypes::AreaID area)
+    throw(BadPeer)
+{
+    debug_msg("Interface %s Vif %s linktype %u area %s\n",
+	      interface.c_str(), vif.c_str(), linktype, area.str().c_str());
+
+    AreaRouter<A> *area_router = get_area_router(area);
+
+    // Verify that we know about this area.
+    if (0 == area_router)
+	return false;
+
+    PeerID peerid = create_peerid(interface, vif);
+
+    // If we got this far create_peerid did not throw an exception so
+    // this interface/vif is unique.
+
+    _peers[peerid] = new Peer<A>(_ospf, linktype, area);
+
+    area_router->add_peer(peerid);
+
+    return true;
+}
+
+template <typename A>
+bool
+PeerManager<A>::delete_peer(const PeerID peerid)
+{
+    debug_msg("PeerID %u\n", peerid);
+
+    if (0 == _peers.count(peerid)) {
+	XLOG_ERROR("Unknown PeerID %u", peerid);
+	return false;
+    }
+
+    // Tell *all* area routers that this peer is being deleted.
+    // It is simpler to do this than hold the reverse mappings.
+    typename map<OspfTypes::AreaID, AreaRouter<A> *>::iterator i;
+    for(i = _areas.begin(); i != _areas.end(); i++)
+	(*i).second->delete_peer(peerid);
+
+    delete _peers[peerid];
+    _peers.erase(_peers.find(peerid));
+
+    return true;
+}
+
+template <typename A>
+void
+PeerManager<A>::incoming_packet(const string& interface, const string& vif,
+				Packet *packet)
+{
+    debug_msg("Interface %s Vif %s %s\n", interface.c_str(),
+	      vif.c_str(), cstring((*packet)));
     XLOG_WARNING("TBD");
 }
 
--- trunk/xorp/ospf/peer_manager.hh
+++ trunk/xorp/ospf/peer_manager.hh
@@ -24,6 +24,8 @@
 typedef uint32_t PeerID;
 
 template <typename A> class Ospf;
+template <typename A> class Peer;
+template <typename A> class AreaRouter;
 
 /**
  * Peer Manager:
@@ -44,16 +46,77 @@ class PeerManager {
 	: _ospf(ospf)
     {}
 
+    ~PeerManager();
+
+    /**
+     * Create an area router.
+     */
+    bool create_area_router(OspfTypes::AreaID area,
+			    OspfTypes::AreaType area_type);
+
+    AreaRouter<A> *get_area_router(OspfTypes::AreaID area);
+
+    /**
+     * Destroy an area router.
+     */
+    bool destroy_area_router(OspfTypes::AreaID area);
+
+    /**
+     * Generate PeerID.
+     * Internally we want to deal with peers as simple IDs not
+     * interface/vif.
+     * Throw an exception if we already have a mapping.
+     */
+    PeerID create_peerid(const string& interface, const string& vif)
+	throw(BadPeer);
+
+    /**
+     * Convert an interface/vif to a PeerID.
+     * Throw an exception if no mapping is found.
+     */
+    PeerID get_peerid(const string& interface, const string& vif) 
+	throw(BadPeer);
+
+    /**
+     * Get rid of this mapping.
+     */
+    void destroy_peerid(const string& interface, const string& vif)
+	throw(BadPeer);
+
+    /**
+     * Create a peer.
+     */
+    bool create_peer(const string& interface, const string& vif,
+		     OspfTypes::LinkType linktype, OspfTypes::AreaID area)
+	throw(BadPeer);
+	
+    /**
+     * Delete a peer.
+     */
+    bool delete_peer(const PeerID);
+
+    /**
+     * Take a peer up or down.
+     */
+    bool set_state_peer(const PeerID, bool state);
+
     /**
      * Demultiplex incoming packets to the associated peer. If the
      * packet contains LSAs send it to the LSA database manager if
      * appropriate.
      */
-    void incoming_packet(Packet *packet);
+    void incoming_packet(const string& interface, const string& vif,
+			 Packet *packet);
 
  private:
-    Ospf<A>& _ospf;		// Reference to the controlling class.
-    //    map<Area, Peer *> _peers;
+    Ospf<A>& _ospf;			// Reference to the controlling class.
+    
+    PeerID _next_peerid;		// Next PeerID to allocate.
+    map<string, PeerID> _pmap;		// Map from interface/vif to PeerID.
+
+    map<PeerID, Peer<A> *> _peers;	// All of our peers
+
+    map<OspfTypes::AreaID, AreaRouter<A> *> _areas;	// All the areas
 };
 
 #endif // __OSPF_PEER_MANAGER_HH__
