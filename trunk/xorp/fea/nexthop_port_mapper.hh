ba449663c8a18c8698154e1fcf5951e34abe8e3d
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2005-04-28 02:30:21 +0000

    Add virtual destrictors for the following abstract base classes:  - NexthopPortMapperObserver
    The reason for this addition is because gcc-4.1 does not like it if a
    class with virtual methods doesn't provide virtual destructors,
    and because this is the right thing to do.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6034 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/nexthop_port_mapper.hh
+++ trunk/xorp/fea/nexthop_port_mapper.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/fea/nexthop_port_mapper.hh,v 1.8 2005/03/25 02:53:11 pavlin Exp $
+// $XORP: xorp/fea/nexthop_port_mapper.hh,v 1.9 2005/03/25 05:41:55 pavlin Exp $
 
 #ifndef __FEA_NEXTHOP_PORT_MAPPER_HH__
 #define __FEA_NEXTHOP_PORT_MAPPER_HH__
@@ -221,6 +221,11 @@ private:
  */
 class NexthopPortMapperObserver {
 public:
+    /**
+     * Empty virtual destructor
+     */
+    virtual ~NexthopPortMapperObserver() {}
+
     /**
      * The next-hop port mapping has changed.
      *
