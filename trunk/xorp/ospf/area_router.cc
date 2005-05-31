a76a025faa27676af736013a285e86f6875edb26
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-05-31 23:48:54 +0000

    1) Mark the router LSA as self originating.
    2) When publishing an LSA bump the age and sequence number if its self
    originating.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6259 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/area_router.cc
+++ trunk/xorp/ospf/area_router.cc
@@ -54,6 +54,7 @@ AreaRouter<A>::AreaRouter(Ospf<A>& ospf, OspfTypes::AreaID area,
 {
     // Never need to delete this as the ref_ptr will tidy up.
     RouterLsa *rlsa = new RouterLsa(_ospf.get_version());
+    rlsa->set_self_originating(true);
     Lsa_header& header = rlsa->get_header();
 
     switch (ospf.get_version()) {
@@ -279,10 +280,14 @@ AreaRouter<A>::update_router_links(PeerStateRef /*psr*/)
 
     XLOG_WARNING("TBD: Set/Unset V,E and B bits");
 
-    router_lsa->encode();
-
+    // A new router LSA should have been generated before MaxAge was hit.
+    XLOG_ASSERT(router_lsa->get_header().get_ls_age() != OspfTypes::MaxAge);
+#if	0
     XLOG_WARNING("Update LS age");
     XLOG_WARNING("Update sequence number");
+#endif
+
+    router_lsa->encode();
 
     return true;
 }
@@ -292,6 +297,26 @@ void
 AreaRouter<A>::publish(Lsa::LsaRef lsar)
 {
     debug_msg("Publish: %s\n", cstring(*lsar));
+
+    TimeVal now;
+    _ospf.get_eventloop().current_time(now);
+
+    // This is an attempt to make sure that the first self originating
+    // LSA that is transmitted has an age of zero and a sequence
+    // number of InitialSequenceNumber.
+    if (lsar->get_self_originating()) {
+	if (OspfTypes::InitialSequenceNumber 
+	    == lsar->get_header().get_ls_sequence_number()) {
+	    lsar->record_creation_time(now);
+	} else {
+	    lsar->update_age(now);
+	}
+	if (OspfTypes::MaxSequenceNumber == lsar->get_ls_sequence_number())
+	    XLOG_FATAL("TBD: Flush this LSA and generate a new LSA");
+	lsar->increment_sequence_number();
+    } else {
+	lsar->update_age(now);
+    }
 
     XLOG_WARNING("TBD publish");
 }
