458e8af286291cec6535805c5672d463f09b7e13
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-07-05 23:57:47 +0000

    Change a printf to a XLOG_INFO.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6425 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/dump_iterators.cc
+++ trunk/xorp/bgp/dump_iterators.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/dump_iterators.cc,v 1.27 2005/03/25 02:52:40 pavlin Exp $"
+#ident "$XORP: xorp/bgp/dump_iterators.cc,v 1.28 2005/04/07 15:23:46 atanu Exp $"
 
 //#define DEBUG_LOGGING
 //#define DEBUG_PRINT_FUNCTION_NAME
@@ -626,7 +626,8 @@ DumpIterator<A>::iterator_got_moved(IPNet<A> new_net) const
     if (new_net == _last_dumped_net) {
 	return false;
     } else {
-	printf("iterator has moved; was %s now %s\n", _last_dumped_net.str().c_str(), new_net.str().c_str());
+	XLOG_INFO("iterator has moved; was %s now %s",
+		  _last_dumped_net.str().c_str(), new_net.str().c_str());
 	return true;
     }
 }
