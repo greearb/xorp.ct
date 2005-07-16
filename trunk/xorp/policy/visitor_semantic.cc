7d5304fed592ad735a0902fd9b265754a68771fe
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2005-07-16 00:27:28 +0000

    Use XORP_UINT_CAST() to fix a compilation error on MacOS X 10.4 with gcc-4.0
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6520 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/policy/visitor_semantic.cc
+++ trunk/xorp/policy/visitor_semantic.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/policy/visitor_semantic.cc,v 1.5 2005/07/12 22:07:23 abittau Exp $"
+#ident "$XORP: xorp/policy/visitor_semantic.cc,v 1.6 2005/07/15 02:27:07 abittau Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -67,7 +67,7 @@ VisitorSemantic::visit(Term& term)
 
     // go through the source block
     bool empty_source = true;
-    debug_msg("[POLICY] source size: %d\n", source.size());
+    debug_msg("[POLICY] source size: %u\n", XORP_UINT_CAST(source.size()));
     for (i = source.begin(); i != source.end(); ++i) {
         (i->second)->accept(*this);
 	empty_source = false;
