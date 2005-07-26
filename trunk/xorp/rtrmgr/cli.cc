70c68a20189cba4ab33525ebd0ead939ee8f21c1
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2005-07-26 07:17:29 +0000

    Rebuild the whole command tree after we load new configuration. This fixes Bugzilla entry #120.
    Bugzilla URL:	http://www.xorp.org/bugzilla/show_bug.cgi?id=120
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6601 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/rtrmgr/cli.cc
+++ trunk/xorp/rtrmgr/cli.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/rtrmgr/cli.cc,v 1.79 2005/07/26 04:32:57 pavlin Exp $"
+#ident "$XORP: xorp/rtrmgr/cli.cc,v 1.80 2005/07/26 05:20:50 pavlin Exp $"
 
 #include <pwd.h>
 
@@ -2689,6 +2689,15 @@ RouterCLI::load_done(bool success, string errmsg)
 	cli_client().cli_print("Load done.\n");
     }
     _xorpsh.set_mode(XorpShellBase::MODE_IDLE);
+
+    //
+    // XXX: Reset the path, because the current node may be gone after
+    // we have loaded the new configuration.
+    // After that rebuild the whole command tree because the configuration
+    // has changed.
+    //
+    reset_path();
+    apply_path_change();
     reenable_ui();
 }
 
