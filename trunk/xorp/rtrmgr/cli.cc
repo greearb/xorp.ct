f8ca037d3d1ef5f644bbfbde0701002af812261e
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2005-07-26 07:51:50 +0000

    If we set the value of an existing node by using the "create" command, set the flag that changes have been made to the configuration tree.
    This fixes bugzilla entry #126.
    
    Bugzilla URL:	http://www.xorp.org/bugzilla/show_bug.cgi?id=126
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6602 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/rtrmgr/cli.cc
+++ trunk/xorp/rtrmgr/cli.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/rtrmgr/cli.cc,v 1.80 2005/07/26 05:20:50 pavlin Exp $"
+#ident "$XORP: xorp/rtrmgr/cli.cc,v 1.81 2005/07/26 07:17:29 pavlin Exp $"
 
 #include <pwd.h>
 
@@ -1876,6 +1876,7 @@ RouterCLI::text_entry_func(const string& ,
 		    ctn->set_value(value, getuid());
 		    // default operator
 		    ctn->set_operator(OP_ASSIGN, getuid());
+		    _changes_made = true;
 		    value_expected = false;
 		} else {
 		    string errmsg = c_format("ERROR: argument \"%s\" "
