ad4326faedb67ae1097edbf7411ca9395af3debc
bms <bms@26435e26-4b25-483a-942d-477236992e84>
2005-08-01 15:32:27 +0000

    STATIC_ROUTES: Diff reduction with XORP/Win32.
    getopt.h
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6654 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/static_routes/xorp_static_routes.cc
+++ trunk/xorp/static_routes/xorp_static_routes.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/static_routes/xorp_static_routes.cc,v 1.10 2005/03/05 02:08:43 pavlin Exp $"
+#ident "$XORP: xorp/static_routes/xorp_static_routes.cc,v 1.11 2005/03/25 02:54:42 pavlin Exp $"
 
 //
 // XORP StaticRoutes module implementation.
@@ -29,6 +29,9 @@
 
 #include "xrl_static_routes_node.hh"
 
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif
 
 //
 // Local functions prototypes
