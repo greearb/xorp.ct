e20d8196ccd8fa5dec72cc81d59473f6a2786211
bms <bms@26435e26-4b25-483a-942d-477236992e84>
2005-08-03 13:15:23 +0000

    Resolve runit sleep(1) path issue.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6665 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/utils/runit.cc
+++ trunk/xorp/utils/runit.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/utils/runit.cc,v 1.11 2005/03/25 02:54:43 pavlin Exp $"
+#ident "$XORP: xorp/utils/runit.cc,v 1.12 2005/08/01 15:35:39 bms Exp $"
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -88,8 +88,10 @@ typedef pid_t PID_T;
 #ifdef HOST_OS_WINDOWS
 static HANDLE hTimer = INVALID_HANDLE_VALUE;
 #define DEVNULL "NUL:"
+#define SLEEP_CMD "sleep 2"
 #else
 #define DEVNULL "/dev/null"
+#define SLEEP_CMD "/bin/sleep 2"
 #endif
 
 /**
@@ -530,7 +532,7 @@ main(int argc, char *argv[])
 	switch (tokens.size()) {
 	case 1:
 	    {
-		Command c(tokens[0], "sleep 2");
+		Command c(tokens[0], SLEEP_CMD);
 		commands.push_back(c);
 	    }
 	    break;
