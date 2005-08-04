448eebf607fa619d92af098c99bbb5ce604fa378
bms <bms@26435e26-4b25-483a-942d-477236992e84>
2005-08-04 10:39:25 +0000

    Diff reduction with XORP/Win32.  Introduce the following methods for general convenience:   int32_t TimeVal::to_ms()  Introduce the following methods for convenience on Windows only:   explicit TimeVal(const FILETIME& ft)   size_t TimeVal::copy_in(const FILETIME& filetime)   size_t TimeVal::copy_out(FILETIME& filetime)
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6677 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxorp/timeval.hh
+++ trunk/xorp/libxorp/timeval.hh
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/libxorp/timeval.hh,v 1.20 2005/03/25 02:53:48 pavlin Exp $
+// $XORP$
 
 #ifndef __LIBXORP_TIMEVAL_HH__
 #define __LIBXORP_TIMEVAL_HH__
@@ -36,6 +36,17 @@
 class TimeVal {
 public:
     static const int32_t ONE_MILLION = 1000000;
+#ifdef HOST_OS_WINDOWS
+    /*
+     * The difference between the beginning of the Windows epoch
+     * (1601-01-01-00-00) and the UNIX epoch (1970-01-01-00-00)
+     * is 134,774 days on the Julian calendar. Compute this
+     * difference in seconds and use it as a constant.
+     * XXX: This does not take account of leap seconds.
+     */
+    static const int64_t UNIX_WIN32_EPOCH_DIFF = (134774*24*60*60);
+    static const int32_t UNITS_100NS_PER_1US = 10;
+#endif
 
 public:
     /**
@@ -51,6 +62,8 @@ public:
      */
     TimeVal(int32_t sec, int32_t usec) : _sec(sec), _usec(usec) {}
 
+#ifndef HOST_OS_WINDOWS
+
     /**
      * Constructor for given "struct timeval".
      *
@@ -59,6 +72,17 @@ public:
      */
     explicit TimeVal(const timeval& timeval)
 	: _sec(timeval.tv_sec), _usec(timeval.tv_usec) {}
+
+#else /* HOST_OS_WINDOWS */
+
+    /**
+     * Constructor for given "FILETIME".
+     *
+     * @param ft the "FILETIME" time value to initialize this object with.
+     */
+    explicit TimeVal(const FILETIME& ft) { copy_in(ft); }
+
+#endif /* !HOST_OS_WINDOWS */
 
     /**
      * Constructor for given double-float time value.
@@ -142,6 +166,35 @@ public:
      * @return the number of copied octets.
      */
     inline size_t copy_out(timeval& timeval) const;
+
+    /**
+     * Return an int32_t containing the total number of milliseconds
+     * in the underlying structure.
+     * This is intended for convenience when working with Win32 APIs.
+     * XXX: This may overflow if _sec is too big.
+     *
+     * @return the number of milliseconds in total.
+     */
+    inline int32_t to_ms() const;
+
+#ifdef HOST_OS_WINDOWS
+    /**
+     * Copy the time value from a FILETIME structure.
+     *
+     * @param filetime the storage to copy the time from.
+     * @return the number of copied octets.
+     */
+    inline size_t copy_in(const FILETIME& filetime);
+
+    /**
+     * Copy the time value to a FILETIME structure.
+     *
+     * @param filetime the storage to copy the time to.
+     * @return the number of copied octets.
+     */
+    inline size_t copy_out(FILETIME& filetime) const;
+
+#endif /* HOST_OS_WINDOWS */
 
     /**
      * Convert a TimeVal value to a double-float value.
@@ -281,6 +334,59 @@ TimeVal::copy_out(timeval& timeval) const
     timeval.tv_usec = _usec;
     return (sizeof(_sec) + sizeof(_usec));
 }
+
+inline int32_t
+TimeVal::to_ms() const
+{
+    int32_t ms = _usec / 1000;
+    // Round a truncated fraction of <1ms to 1ms, not zero.
+    if (_sec == 0 && ms == 0)
+	return (1);
+    ms += _sec * 1000;
+    return (ms);
+}
+
+#ifdef HOST_OS_WINDOWS
+
+/*
+ * Convert Windows time to a BSD struct timeval using 64-bit integer
+ * arithmetic, by the following steps:
+ * 1. Scale Windows' 100ns resolution to BSD's 1us resolution.
+ * 2. Subtract the difference since the beginning of their respective epochs.
+ * 3. Extract the appropriate fractional parts from the 64-bit
+ *    integer used to represent Windows time.
+ * Both UNIX and NT time systems correspond to UTC, therefore leap second
+ * correction is NTP's problem.
+ */
+inline size_t
+TimeVal::copy_in(const FILETIME& filetime)
+{
+    ULARGE_INTEGER ui;
+
+    ui.LowPart = filetime.dwLowDateTime;
+    ui.HighPart = filetime.dwHighDateTime;
+    ui.QuadPart /= UNITS_100NS_PER_1US;
+    ui.QuadPart -= UNIX_WIN32_EPOCH_DIFF * ONE_MILLION;
+    _usec = ui.QuadPart % ONE_MILLION;
+    _sec = ui.QuadPart / ONE_MILLION;
+    return (sizeof(filetime.dwLowDateTime) + sizeof(filetime.dwHighDateTime));
+}
+
+inline size_t
+TimeVal::copy_out(FILETIME& filetime) const
+{
+    ULARGE_INTEGER ui;
+
+    ui.QuadPart = _sec + UNIX_WIN32_EPOCH_DIFF;
+    ui.QuadPart *= ONE_MILLION;
+    ui.QuadPart += _usec;
+    ui.QuadPart *= UNITS_100NS_PER_1US;
+    filetime.dwLowDateTime = ui.LowPart;
+    filetime.dwHighDateTime = ui.HighPart;
+    return (sizeof(filetime.dwLowDateTime) + sizeof(filetime.dwHighDateTime));
+}
+
+#endif /* HOST_OS_WINDOWS */
 
 inline TimeVal&
 TimeVal::operator=(const TimeVal& other)
