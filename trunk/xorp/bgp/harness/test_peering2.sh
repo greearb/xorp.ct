6bc453432f9d2ea738997763bf0ea39d7261ff78
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-08-16 21:04:33 +0000

    test17 has been consistently failing in the tinderbox runs but has been difficult to reproduce when running manually. Temporarily comment it out until we have an opportunity to do further analysis.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6749 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/harness/test_peering2.sh
+++ trunk/xorp/bgp/harness/test_peering2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# $XORP: xorp/bgp/harness/test_peering2.sh,v 1.42 2005/03/20 21:24:37 atanu Exp $
+# $XORP: xorp/bgp/harness/test_peering2.sh,v 1.43 2005/03/20 23:29:45 atanu Exp $
 #
 
 #
@@ -999,9 +999,9 @@ test_work_in_progress()
     rm -f $SENT $RECV $XRLS
 }
 
-TESTS_NOT_FIXED='test10 test11 test12'
+TESTS_NOT_FIXED='test10 test11 test12 test17'
 TESTS='test1 test2 test3 test4 test5 test6 test7 test8 test9 test13 test14
-    test15 test16 test17'
+    test15 test16'
 
 # Include command line
 . ${srcdir}/args.sh
