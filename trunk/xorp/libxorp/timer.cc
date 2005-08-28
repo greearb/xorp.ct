48710217a52069338fc062730e9097f04086ab46
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-08-28 20:02:57 +0000

    In the construct below system_gettimeofday() is static so with gcc29 it looks like the construtor is not called.
    TimerList(&s).system_gettimeofday(tv);
    
    Change the code to instantiate a TimerList and then call the
    system_gettimeofday() method.
    
    TimerList timer = TimerList(&s);
    timer.system_gettimeofday(tv);
    
    Previously the system_gettimeofday() was being called
    recursively. This problem only manifests itself in regression tests
    that don't have an EventLoop and --enable-callback-debug has used as
    an argument to configure.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6860 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxorp/timer.cc
+++ trunk/xorp/libxorp/timer.cc
@@ -28,7 +28,7 @@
 // notice is a summary of the Click LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/libxorp/timer.cc,v 1.24 2005/08/04 10:20:41 bms Exp $"
+#ident "$XORP: xorp/libxorp/timer.cc,v 1.25 2005/08/23 01:49:34 atanu Exp $"
 
 #include "xorp.h"
 #include "timer.hh"
@@ -247,7 +247,8 @@ TimerList::system_gettimeofday(TimeVal* tv)
     TimerList* instance = TimerList::instance();
     if (!instance) {
 	SystemClock s;
-	TimerList(&s).system_gettimeofday(tv);
+	TimerList timer = TimerList(&s);
+	timer.system_gettimeofday(tv);
     } else {
 	instance->advance_time();
 	instance->current_time(*tv);
