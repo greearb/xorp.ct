523e79819c99e15e043c0fa70f11a3ff0f23e34d
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2005-09-01 20:52:19 +0000

    Use "%s" instead of "%p" as the conversion specifiers to print a string.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6925 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/bgp.hh
+++ trunk/xorp/bgp/bgp.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/bgp/bgp.hh,v 1.42 2005/07/20 01:29:21 abittau Exp $
+// $XORP: xorp/bgp/bgp.hh,v 1.43 2005/08/18 15:58:05 bms Exp $
 
 #ifndef __BGP_MAIN_HH__
 #define __BGP_MAIN_HH__
@@ -465,7 +465,7 @@ private:
 	    return *this;
 	}
 	string str() {
-	    return c_format("fd(%p)", _serverfd.str().c_str());
+	    return c_format("fd(%s)", _serverfd.str().c_str());
 
 	}
 	XorpFd _serverfd;
