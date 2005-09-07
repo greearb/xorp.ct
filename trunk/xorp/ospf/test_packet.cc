e1d6b38a3923235173d648a6ef67d89e10681e5c
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-09-07 05:21:36 +0000

    AS-External-LSA.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6975 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/test_packet.cc
+++ trunk/xorp/ospf/test_packet.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/test_packet.cc,v 1.27 2005/09/06 22:34:27 atanu Exp $"
+#ident "$XORP: xorp/ospf/test_packet.cc,v 1.28 2005/09/07 05:06:53 atanu Exp $"
 
 #define DEBUG_LOGGING
 #define DEBUG_PRINT_FUNCTION_NAME
@@ -547,6 +547,7 @@ link_state_update_packet_compare(TestInfo& info, OspfTypes::Version version)
     lsa_decoder.register_decoder(new NetworkLsa(version));
     lsa_decoder.register_decoder(new SummaryNetworkLsa(version));
     lsa_decoder.register_decoder(new SummaryRouterLsa(version));
+    lsa_decoder.register_decoder(new ASExternalLsa(version));
 
     LinkStateUpdatePacket *lsup1;
     lsup1 = new LinkStateUpdatePacket(version, lsa_decoder);
