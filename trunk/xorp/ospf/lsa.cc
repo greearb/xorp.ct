d2d919338c92f49aa4d8485b566a6a7bda4d3751
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-09-08 04:22:38 +0000

    The number of bytes that make up the link information in an OSPFv2 Router-LSA is 12 not 16.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@6989 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/lsa.cc
+++ trunk/xorp/ospf/lsa.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/lsa.cc,v 1.51 2005/09/06 22:34:27 atanu Exp $"
+#ident "$XORP: xorp/ospf/lsa.cc,v 1.52 2005/09/07 05:06:53 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -469,7 +469,7 @@ RouterLink::length() const
 {
     switch (get_version()) {
     case OspfTypes::V2:
-	return 16;
+	return 12;
 	break;
     case OspfTypes::V3:
 	return 16;
