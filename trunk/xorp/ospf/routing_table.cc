ef3dba85601b65c6bcb2f03e564ed2e7db5dc463
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-09-09 12:32:41 +0000

    Routing table.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7007 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/routing_table.cc
+++ trunk/xorp/ospf/routing_table.cc
@@ -13,5 +13,86 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP$"
+#ident "$XORP: xorp/ospf/routing_table.cc,v 1.1 2005/08/05 04:02:41 atanu Exp $"
+// #define DEBUG_LOGGING
+// #define DEBUG_PRINT_FUNCTION_NAME
 
+#include "config.h"
+#include <map>
+#include <list>
+#include <set>
+#include <deque>
+
+#include "ospf_module.h"
+
+#include "libxorp/debug.h"
+#include "libxorp/xlog.h"
+#include "libxorp/callback.hh"
+
+#include "libxorp/ipv4.hh"
+#include "libxorp/ipv6.hh"
+#include "libxorp/ipnet.hh"
+
+#include "libxorp/status_codes.h"
+#include "libxorp/eventloop.hh"
+
+#include "libproto/spt.hh"
+
+#include "ospf.hh"
+#include "delay_queue.hh"
+#include "vertex.hh"
+#include "area_router.hh"
+
+template <typename A>
+void
+RoutingTable<A>::begin()
+{
+    delete _previous;
+    _previous = _current;
+    _current = new Trie<A, RouteEntry<A> >;
+}
+
+template <typename A>
+bool
+RoutingTable<A>::add_entry(IPNet<A> net, RouteEntry<A>& rt)
+{
+    _current->insert(net, rt);
+
+    return true;
+}
+
+template <typename A>
+void
+RoutingTable<A>::end()
+{
+    // Sweep through the previous table looking up routes in the
+    // current table. If no route is found then: delete route.
+
+    // Sweep through the current table looking up entries in the
+    // previous table.
+    // - No route found: add route.
+    // - Route Found
+    //		- If the routes match do nothing.
+    //		- If the routes are different: replace route.
+
+    typename Trie<A, RouteEntry<A> >::iterator tip;
+    typename Trie<A, RouteEntry<A> >::iterator tic;
+    for (tip = _previous->begin(); tip != _previous->end(); tip++) {
+	if (_current->end() == _current->lookup_node(tip.key())) {
+	    _ospf.delete_route(/*tip.key()*/);
+	}
+    }
+
+    for (tic = _current->begin(); tic != _current->begin(); tic++) {
+	tip = _previous->lookup_node(tic.key());
+// 	RouteEntry rt = *tic;
+// 	uint32_t cost = rt._cost;
+	if (_previous->end() == tip) {
+	    _ospf.add_route(/*tip.key(), */);
+	} else {
+	}
+    }
+}
+
+template class RoutingTable<IPv4>;
+template class RoutingTable<IPv6>;
--- trunk/xorp/ospf/routing_table.hh
+++ trunk/xorp/ospf/routing_table.hh
@@ -13,13 +13,60 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP$
+// $XORP: xorp/ospf/routing_table.hh,v 1.3 2005/08/16 21:52:45 atanu Exp $
 
 #ifndef __OSPF_ROUTING_TABLE_HH__
 #define __OSPF_ROUTING_TABLE_HH__
 
 #include <libxorp/trie.hh>
 
+template <typename A>
+class RouteEntry {
+ public:
+    enum Path_type {
+	intra_area,
+	inter_area,
+	type1,
+	type2
+    };
+    
+    OspfTypes::VertexType _destination_type;
+    uint32_t _address;			// If dest type is Network
+    OspfTypes:: RouterID _id;		// If dest type is Router
+
+//     uint32_t _prefix_length;		// Not convinced this is required.
+    OspfTypes::AreaID _area;		// Associated area.
+    Path_type _path_type;
+    uint32_t _cost;
+    uint32_t _type_2_cost;
+
+    A _nexthop;
+    uint32_t	_advertising_router;
+};
+
+template <typename A>
+class RoutingTable {
+ public:
+    RoutingTable(Ospf<A> &ospf)
+	: _ospf(ospf), _current(0), _previous(0)
+    {}
+
+    void begin();
+
+    bool add_entry(IPNet<A> net, RouteEntry<A>& rt);
+
+    bool delete_entry(IPNet<A> net);
+
+    void end();
+    
+ private:
+    Ospf<A>& _ospf;			// Reference to the controlling class.
+
+    Trie<A, RouteEntry<A> > *_current;
+    Trie<A, RouteEntry<A> > *_previous;
+};
+
+#if	0
 template <typename A>
 class RouteEntry {
  public:
@@ -83,4 +130,6 @@ class RoutingTable {
 
     Trie<A, RouteEntry<A> > _trie;
 };
+#endif
+
 #endif // __OSPF_ROUTING_TABLE_HH__
