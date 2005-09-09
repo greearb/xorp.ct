00a347dcf325eaa85621b300a71c7dfce14b501c
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-09-09 08:02:36 +0000

    On broadcast networks the DR or BDR send to AllSPFRouters other states send to AllDRouters. The logic was reversed and became apparent the first time more than two routers were involved.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7003 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/peer.cc
+++ trunk/xorp/ospf/peer.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/peer.cc,v 1.142 2005/09/09 01:53:58 atanu Exp $"
+#ident "$XORP: xorp/ospf/peer.cc,v 1.143 2005/09/09 02:16:13 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -2626,9 +2626,9 @@ Neighbour<A>::send_link_state_update_packet(LinkStateUpdatePacket& lsup)
     case OspfTypes::BROADCAST: {
 	A dest;
 	if (is_DR_or_BDR()) {
-	    dest = A::OSPFIGP_DESIGNATED_ROUTERS();
-	} else {
 	    dest = A::OSPFIGP_ROUTERS();
+	} else {
+	    dest = A::OSPFIGP_DESIGNATED_ROUTERS();
 	}
 	transmit = new SimpleTransmit<A>(pkt,
 					 dest, 
