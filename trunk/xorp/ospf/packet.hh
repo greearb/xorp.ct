00d557970b8467c78083ce900c3369466d1718e8
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-09-11 03:44:08 +0000

    Update a comment.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7022 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/packet.hh
+++ trunk/xorp/ospf/packet.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/packet.hh,v 1.19 2005/08/11 21:35:32 atanu Exp $
+// $XORP: xorp/ospf/packet.hh,v 1.20 2005/09/06 08:43:18 atanu Exp $
 
 #ifndef __OSPF_PACKET_HH__
 #define __OSPF_PACKET_HH__
@@ -614,7 +614,7 @@ class LinkStateAcknowledgementPacket : public Packet {
 };
 
 /**
- * Helper class to manipulate the options field in packets.
+ * Helper class to manipulate the options field in packets and LSAs.
  */
 class Options {
  public:
