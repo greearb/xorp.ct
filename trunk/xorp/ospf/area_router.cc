4a229157ec1e270fb444e563c53922595984580f
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-09-11 09:53:50 +0000

    The comparison functions for LSA incorrectly dealt with the case that both LSAs were MaxAge. This was not a problem previously because we didn't store MaxAge LSAs.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7026 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/area_router.cc
+++ trunk/xorp/ospf/area_router.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/area_router.cc,v 1.85 2005/09/10 00:25:19 atanu Exp $"
+#ident "$XORP: xorp/ospf/area_router.cc,v 1.86 2005/09/11 09:07:04 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -771,6 +771,9 @@ AreaRouter<A>::compare_lsa(const Lsa_header& candidate,
 	return OLDER;
     if (current.get_ls_checksum() < candidate.get_ls_checksum())
 	return NEWER;
+
+    if (current.get_ls_age() == candidate.get_ls_age())
+	return EQUIVALENT;
 
     if (current.get_ls_age() == OspfTypes::MaxAge)
 	return OLDER;
