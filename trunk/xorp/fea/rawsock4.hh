9cc60e4e82e48b17b21cf58b6cffb1d841c1307e
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2005-09-29 19:00:49 +0000

    * A second attempt to fix the "operator<" for   FilterRawSocket4::JoinedMulticastGroup.   This fixes a join/leave multicast group bug.
    * Sync the FilterRawSocket6 implementation with FilterRawSocket4:
      - update the "operator<"
      - add "operator=="
    
    Bug found by:	Atanu Ghosh
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7114 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/rawsock4.hh
+++ trunk/xorp/fea/rawsock4.hh
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/fea/rawsock4.hh,v 1.10 2005/09/07 20:15:44 pavlin Exp $
+// $XORP: xorp/fea/rawsock4.hh,v 1.11 2005/09/08 13:02:04 pavlin Exp $
 
 #ifndef __FEA_RAWSOCK4_HH__
 #define __FEA_RAWSOCK4_HH__
@@ -111,10 +111,10 @@ public:
 	 * than the right-hand operand.
 	 */
 	bool operator<(const JoinedMulticastGroup& other) const {
-	    if (_if_name > other._if_name)
-		return (false);
-	    if (_vif_name > other._vif_name)
-		return (false);
+	    if (_if_name != other._if_name)
+		return (_if_name < other._if_name);
+	    if (_vif_name != other._vif_name)
+		return (_vif_name < other._vif_name);
 	    return (_group_address < other._group_address);
 	}
 
--- trunk/xorp/fea/rawsock6.hh
+++ trunk/xorp/fea/rawsock6.hh
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/fea/rawsock6.hh,v 1.10 2005/08/18 15:45:51 bms Exp $
+// $XORP: xorp/fea/rawsock6.hh,v 1.11 2005/09/07 20:15:44 pavlin Exp $
 
 #ifndef __FEA_RAWSOCK6_HH__
 #define __FEA_RAWSOCK6_HH__
@@ -110,9 +110,24 @@ public:
 	 * than the right-hand operand.
 	 */
 	bool operator<(const JoinedMulticastGroup& other) const {
-	    return ((_if_name < other._if_name) &&
-		    (_vif_name < other._vif_name) &&
-		    (_group_address < other._group_address));
+	    if (_if_name != other._if_name)
+		return (_if_name < other._if_name);
+	    if (_vif_name != other._vif_name)
+		return (_vif_name < other._vif_name);
+	    return (_group_address < other._group_address);
+	}
+
+	/**
+	 * Equality Operator
+	 *
+	 * @param other the right-hand operand to compare against.
+	 * @return true if the left-hand operand is numerically same as the
+	 * right-hand operand.
+	 */
+	bool operator==(const JoinedMulticastGroup& other) const {
+	    return ((_if_name == other._if_name) &&
+		    (_vif_name == other._vif_name) &&
+		    (_group_address == other._group_address));
 	}
 
 	/**
