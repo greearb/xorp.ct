3753fdfbc8cbcbfd549c487dc4cd94f93c0d7d2d
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-10-01 03:25:01 +0000

    Fix a typo in a comment.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7119 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/routing_table.hh
+++ trunk/xorp/ospf/routing_table.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/routing_table.hh,v 1.6 2005/10/01 03:09:19 atanu Exp $
+// $XORP: xorp/ospf/routing_table.hh,v 1.7 2005/10/01 03:22:16 atanu Exp $
 
 #ifndef __OSPF_ROUTING_TABLE_HH__
 #define __OSPF_ROUTING_TABLE_HH__
@@ -21,7 +21,7 @@
 #include <libxorp/trie.hh>
 
 /**
- * Externally view of a routing entry.
+ * External view of a routing entry.
  */
 template <typename A>
 class RouteEntry {
