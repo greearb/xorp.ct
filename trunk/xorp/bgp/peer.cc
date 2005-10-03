e5a9703b15e8e697bc438a29be3a3a2073e2e68c
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-10-03 18:34:33 +0000

    The still connected assert was being triggered in the AcceptSession destructor, which as the very least catches a file descriptor being leaked.
    If the connection is closed while waiting from an open from the peer
    the socket is now disconnected.
    
    Problem caught in load tests on xorp12.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7132 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/peer.cc
+++ trunk/xorp/bgp/peer.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/peer.cc,v 1.95 2005/08/18 15:58:06 bms Exp $"
+#ident "$XORP: xorp/bgp/peer.cc,v 1.96 2005/09/23 17:02:55 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -2194,6 +2194,7 @@ void
 AcceptSession::event_closed_accept()
 {
     // The socket is closed so out of here.
+    _socket_client->disconnect();
     remove();
 }
 
