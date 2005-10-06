c126d2ea7ca2e656e1b10e11e2046619a366c952
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-10-06 07:24:19 +0000

    When an area is being shutdown it now notifies the routing table which can remove routes from this area and notify the other areas.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7160 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/area_router.cc
+++ trunk/xorp/ospf/area_router.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/area_router.cc,v 1.103 2005/10/04 19:25:55 atanu Exp $"
+#ident "$XORP: xorp/ospf/area_router.cc,v 1.104 2005/10/05 03:16:21 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -121,6 +121,7 @@ template <typename A>
 void
 AreaRouter<A>::shutdown()
 {
+    _ospf.get_routing_table().remove_area(_area);
     shutdown_complete();
 }
 
