6646b68e31f0ec1c221cac71a61b39c8aeac8e26
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-10-16 23:32:00 +0000

    Typo.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7260 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/xrl_target.cc
+++ trunk/xorp/ospf/xrl_target.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/xrl_target.cc,v 1.10 2005/10/13 16:39:04 atanu Exp $"
+#ident "$XORP: xorp/ospf/xrl_target.cc,v 1.11 2005/10/14 17:53:53 atanu Exp $"
 
 #define DEBUG_LOGGING
 #define DEBUG_PRINT_FUNCTION_NAME
@@ -223,7 +223,7 @@ XrlOspfV2Target::policy_backend_0_1_push_routes()
 XrlCmdError
 XrlOspfV2Target::policy_redist4_0_1_add_route4(const IPv4Net& /*network*/,
 					       const bool& /*unicast*/,
-					       const bool& /*multaicast*/,
+					       const bool& /*multicast*/,
 					       const IPv4& /*nexthop*/,
 					       const uint32_t& /*metric*/,
 					       const XrlAtomList& /*policytags*/)
