5873d7de44eb5970063033b97c2bda36617222b0
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-10-22 18:44:52 +0000

    Put in the hooks to get the list of configured areas.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7324 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/xrl_target.cc
+++ trunk/xorp/ospf/xrl_target.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/xrl_target.cc,v 1.14 2005/10/17 08:03:58 atanu Exp $"
+#ident "$XORP: xorp/ospf/xrl_target.cc,v 1.15 2005/10/17 22:22:16 atanu Exp $"
 
 #define DEBUG_LOGGING
 #define DEBUG_PRINT_FUNCTION_NAME
@@ -582,5 +582,11 @@ XrlOspfV2Target::XrlOspfV2Target::ospfv2_0_1_get_lsa(const IPv4& a,
     debug_msg("area %s index %u valid %s toohigh %s self %s\n",
 	      pr_id(area).c_str(), index, pb(valid), pb(toohigh), pb(self));
 
+    return XrlCmdError::OKAY();
+}
+
+XrlCmdError
+XrlOspfV2Target::ospfv2_0_1_get_area_list(XrlAtomList& /*areas*/)
+{
     return XrlCmdError::OKAY();
 }
--- trunk/xorp/ospf/xrl_target.hh
+++ trunk/xorp/ospf/xrl_target.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/xrl_target.hh,v 1.8 2005/10/13 16:39:04 atanu Exp $
+// $XORP: xorp/ospf/xrl_target.hh,v 1.9 2005/10/14 17:53:54 atanu Exp $
 
 #ifndef __OSPF_XRL_TARGET_HH__
 #define __OSPF_XRL_TARGET_HH__
@@ -344,6 +344,13 @@ class XrlOspfV2Target : XrlOspfv2TargetBase {
 	bool&	toohigh,
 	bool&	self,
 	vector<uint8_t>&	lsa);
+
+    /**
+     *  Get a list of all the configured areas.
+     */
+    XrlCmdError ospfv2_0_1_get_area_list(
+					 // Output values,
+					 XrlAtomList& areas);
 
  private:
     Ospf<IPv4>& _ospf;
