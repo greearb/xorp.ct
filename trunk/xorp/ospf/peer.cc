4f7bbccedcbfea3754712fbe04494ea1f03f95fa
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-11-05 09:32:49 +0000

    Remove an unused variable.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7473 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/peer.cc
+++ trunk/xorp/ospf/peer.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/peer.cc,v 1.174 2005/11/05 06:20:38 atanu Exp $"
+#ident "$XORP: xorp/ospf/peer.cc,v 1.175 2005/11/05 08:49:40 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -3973,7 +3973,6 @@ Neighbour<A>::change_state(State state)
     State previous_state = get_state();
     set_state(state);
 
-    TimeVal now;
     if (Full == state)
 	_ospf.get_eventloop().current_time(_adjacency_time);
 
