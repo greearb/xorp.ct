bf31684efab73c0bfd9a784d0d63e3e27834dfdd
zec <zec@26435e26-4b25-483a-942d-477236992e84>
2005-11-11 17:52:37 +0000

    Remove a _really_ unnecessary printf() in write_aggr_pref_len()
    Requested by:	Pavlin
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7538 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/bgp_varrw.cc
+++ trunk/xorp/bgp/bgp_varrw.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/bgp_varrw.cc,v 1.21 2005/11/09 02:46:35 pavlin Exp $"
+#ident "$XORP: xorp/bgp/bgp_varrw.cc,v 1.22 2005/11/11 15:23:28 zec Exp $"
 
 #include "bgp_module.h"
 #include "libxorp/xorp.h"
@@ -453,7 +453,6 @@ template <class A>
 void
 BGPVarRW<A>::write_aggr_pref_len(const Element& e)
 {
-printf("XXX MARKO BGPVarRW<A>::write_aggr_pref_len\n");
     // We should not set the aggr_pref_len if already set by someone else!
     if (_aggr_prefix_len != SR_AGGR_IGNORE)
 	return;
