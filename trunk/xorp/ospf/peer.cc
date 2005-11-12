d936278baf40b58f45b4dbc0f48ebbd3c2df5924
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-11-12 23:09:19 +0000

    The type OspfTypes::RouterID was originally IPv4 it was later changed to unsigned integer. A construct of the form OspfTypes::RouterID("0.0.0.0") used to yield zero. It now returns the the address of the string "0.0.0.0" as it is now a cast, the joys of C++.
    Bugzilla URL:	http://www.xorp.org/bugzilla/show_bug.cgi?id=354
    Submitted by:	Hasso Tepper
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7548 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/peer.cc
+++ trunk/xorp/ospf/peer.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/peer.cc,v 1.179 2005/11/11 11:06:12 atanu Exp $"
+#ident "$XORP: xorp/ospf/peer.cc,v 1.180 2005/11/11 22:16:07 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -3220,8 +3220,7 @@ Neighbour<A>::event_hello_received(HelloPacket *hello)
     // allowed in state Waiting.
     if (Peer<A>::Waiting == _peer.get_state()) {
 	if ((get_candidate_id() == hello->get_designated_router() &&
-	    OspfTypes::RouterID("0.0.0.0") == 
-	     hello->get_backup_designated_router()) ||
+	    set_id("0.0.0.0") == hello->get_backup_designated_router()) ||
 	    get_candidate_id() == hello->get_backup_designated_router()) {
 	    _peer.schedule_event("BackupSeen");
 	}
