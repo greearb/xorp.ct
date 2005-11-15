9a9766a1549ff1dd16c2d74e48b8cdc37f95e031
zec <zec@26435e26-4b25-483a-942d-477236992e84>
2005-11-15 18:04:29 +0000

    Initial, very spartanic and still incomplete implementation of a BGP aggregation table.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7589 26435e26-4b25-483a-942d-477236992e84

---
+++ trunk/xorp/bgp/route_table_aggregation.cc
@@ -0,0 +1,545 @@
+// -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*-
+
+// Copyright (c) 2001-2005 International Computer Science Institute
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software")
+// to deal in the Software without restriction, subject to the conditions
+// listed in the XORP LICENSE file. These conditions include: you must
+// preserve this copyright notice, and you cannot mention the copyright
+// holders in advertising related to the Software without their permission.
+// The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+// notice is a summary of the XORP LICENSE file; the license in that file is
+// legally binding.
+
+#ident "$XORP: Exp $"
+
+//#define DEBUG_LOGGING
+//#define DEBUG_PRINT_FUNCTION_NAME
+
+#include "bgp_module.h"
+#include "libxorp/xlog.h"
+#include "path_attribute.hh"
+#include "bgp.hh"
+#include "route_table_aggregation.hh"
+
+
+template<class A>
+AggregationTable<A>::AggregationTable(string table_name,
+				      BGPPlumbing& master,
+				      BGPRouteTable<A> *parent_table)
+    : BGPRouteTable<A>("AggregationTable-" + table_name, master.safi()),
+      _master_plumbing(master)
+{
+    _parent = parent_table;
+}
+
+
+template<class A>
+AggregationTable<A>::~AggregationTable()
+{
+    if (_aggregates_table.begin() != _aggregates_table.end()) {
+        XLOG_WARNING("AggregatesTable trie was not empty on deletion\n");
+    }
+}
+
+
+template<class A>
+int
+AggregationTable<A>::add_route(const InternalMessage<A> &rtmsg,
+			     BGPRouteTable<A> *caller)
+{
+    debug_msg("\n         %s\n caller: %s\n rtmsg: %p route: %p\n%s\n",
+	      tablename().c_str(),
+	      caller ? caller->tablename().c_str() : "NULL",
+	      &rtmsg,
+	      rtmsg.route(),
+	      rtmsg.str().c_str());
+
+    const SubnetRoute<A> *orig_route = rtmsg.route();
+    XLOG_ASSERT(caller == _parent);
+    XLOG_ASSERT(_next_table != NULL);
+    XLOG_ASSERT(orig_route->nexthop_resolved());
+    bool must_push = false;
+
+    /*
+     * If not marked as aggregation candidate, pass the request
+     * unmodified downstream.  DONE.
+     */
+    uint32_t aggr_prefix_len = rtmsg.route()->aggr_prefix_len();
+    debug_msg("\naggr_prefix_len=%d\n", aggr_prefix_len);
+    if (aggr_prefix_len == SR_AGGR_IGNORE)
+	return _next_table->add_route(rtmsg, (BGPRouteTable<A>*)this);
+
+    /*
+     * If route has less specific prefix length then the requested
+     * aggregate, pass the request downstream.  Since we have to modify
+     * the aggr_prefix_len field of the route, we must operate on a
+     * copy, not on the original route.
+     */
+    const IPNet<A> orig_net = rtmsg.net();
+    const IPNet<A> aggr_net = IPNet<A>(orig_net.masked_addr(),
+				       aggr_prefix_len);
+    SubnetRoute<A> *ibgp_r = new SubnetRoute<A>(orig_net,
+						orig_route->attributes(),
+						orig_route->original_route(),
+						orig_route->igp_metric());
+    // propagate route flags
+    ibgp_r->set_filtered(orig_route->is_filtered());
+    ibgp_r->set_is_winner(orig_route->igp_metric());
+    ibgp_r->set_in_use(orig_route->in_use());
+    ibgp_r->set_nexthop_resolved(orig_route->nexthop_resolved());
+
+    InternalMessage<A> ibgp_msg(ibgp_r, rtmsg.origin_peer(), rtmsg.genid());
+    ibgp_msg.set_changed();
+
+    // propagate internal message flags
+    if (rtmsg.push())
+        must_push = true;
+    if (rtmsg.from_previous_peering())
+        ibgp_msg.set_from_previous_peering();
+
+    if (orig_net.prefix_len() < aggr_prefix_len) {
+	// Send the "original" version downstream, and we are DONE.
+	if (must_push)
+	    ibgp_msg.set_push();
+	ibgp_r->set_aggr_prefix_len(SR_AGGR_IGNORE);
+	int res = _next_table->add_route(ibgp_msg, (BGPRouteTable<A>*)this);
+	ibgp_r->unref();
+	return res;
+    }
+
+    debug_msg("\nXXX *** XXX\nMARKO add_route orig=%s aggr=%s\n",
+		orig_net.str().c_str(), aggr_net.str().c_str());
+    // Find existing or create a new aggregate route
+    typename RefTrie<A, const AggregateRoute<A> >::iterator ai;
+    ai = _aggregates_table.lookup_node(aggr_net);
+    if (ai == _aggregates_table.end()) {
+        const AggregateRoute<A> *new_aggr_route =
+	    new AggregateRoute<A>(aggr_net, orig_route->aggr_brief_mode(),
+				  _master_plumbing.main().get_local_data());
+	ai = _aggregates_table.insert(aggr_net, *new_aggr_route);
+    }
+    AggregateRoute<A> *aggr_route =
+	const_cast<AggregateRoute<A> *> (&ai.payload());
+
+    // Check we don't already have the original route stored
+    XLOG_ASSERT(aggr_route->components_table()->lookup_node(orig_net) == aggr_route->components_table()->end());
+    ComponentRoute<A> *comp_route = new ComponentRoute<A>(rtmsg.route());
+    aggr_route->components_table()->insert(orig_net, *comp_route);
+
+    /*
+     * If our component route holds a more specific prefix than the
+     * aggregate, announce it to EBGP peering branches.
+     */
+    if (aggr_route->net() != orig_net) {
+	SubnetRoute<A> *ebgp_r =
+	    new SubnetRoute<A>(orig_net,
+			       orig_route->attributes(),
+			       orig_route->original_route(),
+			       orig_route->igp_metric());
+	// propagate route flags
+	ebgp_r->set_filtered(orig_route->is_filtered());
+	ebgp_r->set_is_winner(orig_route->igp_metric());
+	ebgp_r->set_in_use(orig_route->in_use());
+	ebgp_r->set_nexthop_resolved(orig_route->nexthop_resolved());
+
+	InternalMessage<A> ebgp_msg(ebgp_r, rtmsg.origin_peer(), rtmsg.genid());
+	ebgp_msg.set_changed();
+
+	// propagate internal message flags
+	if (rtmsg.from_previous_peering())
+	    ebgp_msg.set_from_previous_peering();
+
+	if (aggr_route->is_suppressed())
+	    ebgp_r->set_aggr_prefix_len(SR_AGGR_EBGP_NOT_AGGREGATED);
+	else
+	    ebgp_r->set_aggr_prefix_len(SR_AGGR_EBGP_WAS_AGGREGATED);
+	_next_table->add_route(ebgp_msg, (BGPRouteTable<A>*)this);
+	ebgp_r->unref();
+    }
+
+    /*
+     * Recompute the aggregate.  If pa_list different from the old one,
+     * check whether we have to withdraw the old and / or announce the
+     * new aggregate and / or all the component routes.
+     */
+    aggr_route->reevaluate(this);
+
+    /*
+     * Send the "original" version downstream.  SR_AGGR_IBGP_ONLY marker
+     * will instruct the post-fanout static filters to propagate this
+     * route only to IBGP peering and localRIB.
+     */
+    ibgp_r->set_aggr_prefix_len(SR_AGGR_IBGP_ONLY);
+    int res = _next_table->add_route(ibgp_msg, (BGPRouteTable<A>*)this);
+    ibgp_r->unref();
+    if (must_push)
+	_next_table->push(this);
+    return res;
+}
+
+
+template<class A>
+int
+AggregationTable<A>::delete_route(const InternalMessage<A> &rtmsg,
+				  BGPRouteTable<A> *caller)
+{
+    debug_msg("\n         %s\n caller: %s\n rtmsg: %p route: %p\n%s\n",
+	      tablename().c_str(),
+	      caller ? caller->tablename().c_str() : "NULL",
+	      &rtmsg,
+	      rtmsg.route(),
+	      rtmsg.str().c_str());
+
+    const SubnetRoute<A> *orig_route = rtmsg.route();
+    XLOG_ASSERT(caller == _parent);
+    XLOG_ASSERT(_next_table != NULL);
+    XLOG_ASSERT(orig_route->nexthop_resolved());
+    bool must_push = false;
+
+    /*
+     * If not marked as aggregation candidate, pass the request
+     * unmodified downstream.  DONE.
+     */
+    uint32_t aggr_prefix_len = orig_route->aggr_prefix_len();
+    debug_msg("\naggr_prefix_len=%d\n", aggr_prefix_len);
+    if (aggr_prefix_len == SR_AGGR_IGNORE)
+	return _next_table->delete_route(rtmsg, (BGPRouteTable<A>*)this);
+
+    /*
+     * If route has less specific prefix length then the requested
+     * aggregate, pass the request downstream.  Since we have to modify
+     * the aggr_prefix_len field of the route, we must operate on a
+     * copy, not on the original route.
+     */
+    const IPNet<A> orig_net = rtmsg.net();
+    const IPNet<A> aggr_net = IPNet<A>(orig_net.masked_addr(),
+				       aggr_prefix_len);
+    SubnetRoute<A> *ibgp_r = new SubnetRoute<A>(orig_net,
+						orig_route->attributes(),
+						orig_route->original_route(),
+						orig_route->igp_metric());
+    // propagate route flags
+    ibgp_r->set_filtered(orig_route->is_filtered());
+    ibgp_r->set_is_winner(orig_route->igp_metric());
+    ibgp_r->set_in_use(orig_route->in_use());
+    ibgp_r->set_nexthop_resolved(orig_route->nexthop_resolved());
+
+    InternalMessage<A> ibgp_msg(ibgp_r, rtmsg.origin_peer(), rtmsg.genid());
+    ibgp_msg.set_changed();
+
+    // propagate internal message flags
+    if (rtmsg.push())
+        must_push = true;
+    if (rtmsg.from_previous_peering())
+        ibgp_msg.set_from_previous_peering();
+
+    if (orig_net.prefix_len() < aggr_prefix_len) {
+	// Send the "original" version downstream, and we are DONE.
+	if (must_push)
+	    ibgp_msg.set_push();
+	ibgp_r->set_aggr_prefix_len(SR_AGGR_IGNORE);
+	int res = _next_table->delete_route(ibgp_msg, (BGPRouteTable<A>*)this);
+	ibgp_r->unref();
+	return res;
+    }
+
+    debug_msg("\nXXX *** XXX\nMARKO delete_route orig=%s aggr=%s\n",
+		orig_net.str().c_str(), aggr_net.str().c_str());
+    // Find the appropriate aggregate route
+    typename RefTrie<A, const AggregateRoute<A> >::iterator ai;
+    ai = _aggregates_table.lookup_node(aggr_net);
+    // Check that the aggregate exists, otherwise something's gone wrong.
+    XLOG_ASSERT(ai != _aggregates_table.end());
+    AggregateRoute<A> *aggr_route =
+	const_cast<AggregateRoute<A> *> (&ai.payload());
+
+    /*
+     * If our component route holds a more specific prefix than the
+     * aggregate, announce it to EBGP peering branches.
+     */
+    if (aggr_route->net() != orig_net) {
+	SubnetRoute<A> *ebgp_r =
+	    new SubnetRoute<A>(orig_net,
+			       orig_route->attributes(),
+			       orig_route->original_route(),
+			       orig_route->igp_metric());
+	// propagate route flags
+	ebgp_r->set_filtered(orig_route->is_filtered());
+	ebgp_r->set_is_winner(orig_route->igp_metric());
+	ebgp_r->set_in_use(orig_route->in_use());
+	ebgp_r->set_nexthop_resolved(orig_route->nexthop_resolved());
+
+	InternalMessage<A> ebgp_msg(ebgp_r, rtmsg.origin_peer(), rtmsg.genid());
+	ebgp_msg.set_changed();
+
+	// propagate internal message flags
+	if (rtmsg.from_previous_peering())
+	    ebgp_msg.set_from_previous_peering();
+
+	if (aggr_route->is_suppressed())
+	    ebgp_r->set_aggr_prefix_len(SR_AGGR_EBGP_NOT_AGGREGATED);
+	else
+	    ebgp_r->set_aggr_prefix_len(SR_AGGR_EBGP_WAS_AGGREGATED);
+	_next_table->delete_route(ebgp_msg, (BGPRouteTable<A>*)this);
+	ebgp_r->unref();
+    }
+
+    // XXX Be EXTRA carefull no to leak memory here!  REVISIT!!!
+    aggr_route->components_table()->erase(orig_net);
+
+    /*
+     * Recompute the aggregate.  If pa_list different from the old one,
+     * check whether we have to withdraw the old and /or announce the
+     * new aggregate.
+     */
+    aggr_route->reevaluate(this);
+
+    // XXX Again, be EXTRA carefull no to leak memory!  REVISIT!!!
+    if (aggr_route->components_table()->route_count() == 0)
+	_aggregates_table.erase(aggr_net);
+
+    /*
+     * Send the "original" version downstream.  SR_AGGR_IBGP_ONLY marker
+     * will instruct the post-fanout static filters to propagate this
+     * route only to IBGP peering and localRIB.
+     */
+    ibgp_r->set_aggr_prefix_len(SR_AGGR_IBGP_ONLY);
+    int res = _next_table->delete_route(ibgp_msg, (BGPRouteTable<A>*)this);
+    ibgp_r->unref();
+    if (must_push)
+	_next_table->push(this);
+    return res;
+}
+
+
+template<class A>
+void
+AggregateRoute<A>::reevaluate(AggregationTable<A> *parent)
+{
+    RefTrie<A, const ComponentRoute<A> >::PostOrderIterator comp_iter;
+    uint32_t med = 0;
+    bool must_set_atomic_aggr = false;
+    bool old_was_suppressed = _is_suppressed;
+    _is_suppressed = false;
+    const PathAttributeList<A> *old_pa_list = _pa_list;
+    _pa_list = new PathAttributeList<A>(NextHopAttribute<A>(A::ZERO()),
+					AsPath(),
+					OriginAttribute(IGP));
+    /*
+     * PHASE 1:
+     *
+     * Iterate through all component routes in order to compute new
+     * path attributes and determine whether we are allowed to announce
+     * the aggregate or not.
+     */
+    for (comp_iter = _components_table.begin();
+         comp_iter != _components_table.end(); comp_iter++) {
+	const PathAttributeList<A>
+		*comp_pa_list(comp_iter.payload().route()->attributes());
+	debug_msg("comp_route: %s\n    %s\n",
+		  comp_iter.payload().route()->net().str().c_str(),
+		  comp_pa_list->aspath().str().c_str());
+
+	if (comp_iter == _components_table.begin()) {
+	    if (comp_pa_list->med_att()) {
+		med = comp_pa_list->med_att()->med();
+	    }
+	    _pa_list->replace_AS_path(comp_pa_list->aspath());
+	    _pa_list->replace_origin((OriginType)comp_pa_list->origin());
+	    _pa_list->rehash();
+	} else {
+	    if (comp_pa_list->med_att() &&
+		med != comp_pa_list->med_att()->med()) {
+		_is_suppressed = true;
+	    }
+
+	    // XXX Don't forget origin processing here!  REVISIT!!!
+
+	    /*
+	     * The simplest possible yet seemingly legal option:
+	     * we originate an empty aspath!
+	     */
+	    if (_pa_list->aspath() != comp_pa_list->aspath()) {
+		_pa_list->replace_AS_path(AsPath());
+		_pa_list->rehash();
+		must_set_atomic_aggr = true;
+	    }
+	}
+
+	/*
+	 * If aggregating a received aggregate, we must propagate the
+	 * atomic aggregate attribute
+	 */
+	if (comp_pa_list->atomic_aggregate_att())
+	    must_set_atomic_aggr = true;
+    }
+
+    if (must_set_atomic_aggr) {
+	AtomicAggAttribute aa_attr;
+	_pa_list->add_path_attribute(aa_attr);
+    }
+
+    _pa_list->add_path_attribute(*_aggregator_attribute);
+    _pa_list->rehash();
+
+    debug_msg("OLD ATTRIBUTES:%s\n", old_pa_list->str().c_str());
+    debug_msg("NEW ATTRIBUTES:%s\n", _pa_list->str().c_str());
+
+    /*
+     * Phase 2:
+     *
+     * If the new aggregate must be suppressed, but was active before,
+     * we should reannounce all the component routes with proper marking, i.e.
+     * SR_AGGR_EBGP_NOT_AGGREGATED instead of SR_AGGR_EBGP_WAS_AGGREGATED.
+     * Similarly, if previously the aggregate was suppressed, but now it
+     * must not be, we should reannounce all the component routes marked as
+     * SR_AGGR_EBGP_WAS_AGGREGATED.
+     *
+     * Observe the true meaning of the word SHOULD.  We _should_ do the
+     * described action, but currently we do nothing.  This means we
+     * are violating the BGP RFC draft, section 9.2.2.2, paragraph 3:
+     * "Routes that have different MED attribute SHALL NOT be aggregated."
+     */
+    UNUSED(old_was_suppressed)
+
+    /*
+     * Phase 3:
+     *
+     * If the new path attributes are different from the old ones, delete
+     * the old version of the aggregate route and announce a fresh one,
+     * unless _is_suppressed flag was set.
+     */
+    if (*old_pa_list != *_pa_list) {
+	/*
+	 * The new aggregate is different from the old one.
+	 */
+	if (_was_announced) {
+	    /*
+	     * We need to send a delete request for the old aggregate
+	     * before proceeding any further.  Construct a temporary
+	     * subnet route / message and send downstream.
+	     */
+	     SubnetRoute<A>* tmp_route =
+			new SubnetRoute<A>(_net,
+					   old_pa_list,
+					   NULL,
+					   0);
+	     tmp_route->set_nexthop_resolved(true);	// Cheating
+	     tmp_route->set_aggr_prefix_len(SR_AGGR_EBGP_AGGREGATE);
+	     InternalMessage<A>
+		tmp_rtmsg(tmp_route,
+			  parent->_master_plumbing.rib_handler(),
+			  GENID_UNKNOWN);
+	     parent->_next_table->delete_route(tmp_rtmsg, parent);
+	     tmp_route->unref();
+	}
+
+	if (!_is_suppressed && _components_table.route_count() != 0) {
+	     SubnetRoute<A>* tmp_route =
+			new SubnetRoute<A>(_net,
+					   _pa_list,
+					   NULL,
+					   0);
+	     tmp_route->set_nexthop_resolved(true);	// Cheating
+	     tmp_route->set_aggr_prefix_len(SR_AGGR_EBGP_AGGREGATE);
+	     InternalMessage<A>
+		tmp_rtmsg(tmp_route,
+			  parent->_master_plumbing.rib_handler(),
+			  GENID_UNKNOWN);
+	     parent->_next_table->add_route(tmp_rtmsg, parent);
+	     tmp_route->unref();
+	     _was_announced = true;
+	} else
+	     _was_announced = false;
+    }
+
+    delete old_pa_list;
+}
+
+
+template<class A>
+int
+AggregationTable<A>::replace_route(const InternalMessage<A> &old_rtmsg,
+				   const InternalMessage<A> &new_rtmsg,
+				   BGPRouteTable<A> *caller)
+{
+    debug_msg("\n         %s\n"
+	      "caller: %s\n"
+	      "old rtmsg: %p new rtmsg: %p "
+	      "old route: %p"
+	      "new route: %p"
+	      "old: %s\n new: %s\n",
+	      tablename().c_str(),
+	      caller->tablename().c_str(),
+	      &old_rtmsg,
+	      &new_rtmsg,
+	      old_rtmsg.route(),
+	      new_rtmsg.route(),
+	      old_rtmsg.str().c_str(),
+	      new_rtmsg.str().c_str());
+
+    XLOG_ASSERT(caller == _parent);
+    XLOG_ASSERT(old_rtmsg.route()->nexthop_resolved());
+    XLOG_ASSERT(new_rtmsg.route()->nexthop_resolved());
+
+    _next_table->delete_route(old_rtmsg, (BGPRouteTable<A>*)this);
+    return _next_table->add_route(old_rtmsg, (BGPRouteTable<A>*)this);
+}
+
+
+template<class A>
+int
+AggregationTable<A>::push(BGPRouteTable<A> *caller)
+{
+    debug_msg("Push\n");
+    XLOG_ASSERT(caller == _parent);
+    return _next_table->push((BGPRouteTable<A>*)this);
+}
+
+// XXX keep those in place to trap calls to methods bellow
+#ifndef DEBUG_LOGGING
+#define DEBUG_LOGGING
+#define DEBUG_PRINT_FUNCTION_NAME
+#endif
+
+template<class A>
+const SubnetRoute<A>*
+AggregationTable<A>::lookup_route(const IPNet<A> &net, uint32_t& genid) const
+{
+    debug_msg("Lookup_route\n");
+    // XXX Can this ever be called?  REVISIT!!!
+    return _parent->lookup_route(net, genid);
+}
+
+template<class A>
+void
+AggregationTable<A>::route_used(const SubnetRoute<A>* rt, bool in_use)
+{
+    debug_msg("route_used\n");
+    // XXX Can this ever be called?  REVISIT!!!
+    _parent->route_used(rt, in_use);
+}
+
+template<class A>
+string
+AggregationTable<A>::str() const
+{
+    string s = "AggregationTable<A>" + tablename();
+    return s;
+}
+
+template<class A>
+bool
+AggregationTable<A>::get_next_message(BGPRouteTable<A> *next_table)
+{
+    debug_msg("next table: %s\n", next_table->tablename().c_str());
+    // XXX Can this ever be called?  REVISIT!!!
+    return 0; // XXX
+}
+
+
+template class AggregationTable<IPv4>;
+template class AggregationTable<IPv6>;
+
---
+++ trunk/xorp/bgp/route_table_aggregation.hh
@@ -0,0 +1,171 @@
+// -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*-
+
+// Copyright (c) 2001-2005 International Computer Science Institute
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software")
+// to deal in the Software without restriction, subject to the conditions
+// listed in the XORP LICENSE file. These conditions include: you must
+// preserve this copyright notice, and you cannot mention the copyright
+// holders in advertising related to the Software without their permission.
+// The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+// notice is a summary of the XORP LICENSE file; the license in that file is
+// legally binding.
+
+// $XORP: Exp $
+
+#ifndef __BGP_ROUTE_TABLE_AGGREGATION_HH__
+#define __BGP_ROUTE_TABLE_AGGREGATION_HH__
+
+#include "route_table_base.hh"
+#include "libxorp/ref_trie.hh"
+
+
+/**
+ * @short classes used for BGP prefix aggregation.
+ *
+ * The main aggregation processing occurs and all the state is kept
+ * in an AggregationTable stage that is plumbed between the decission
+ * and fanout tables.  Additionally, static filters immediately following
+ * the fanout table may filter out certain routes marked by the
+ * AggregationTable, depending on whether the outbound branch is of IBGP
+ * or EBGP type.
+ *                                              +---------+
+ *                                            ->| Filters |->IBGP...
+ *  \                                        /  +---------+
+ *   ->+-----------+  +--------+  +--------+/   +---------+
+ *  -->| Decission |->| Aggreg.|->| Fanout |--->| Filters |->IBGP...
+ *   ->+-----------+  +--------+  +--------+\   +---------+
+ *  /                                        \  +---------+
+ *                    Processing,             ->| Filters |->EBGP...
+ *                     state,                   +---------+
+ *                     marking                 Static filters
+ * 
+ * Routes can be marked as candidates for aggregation using the generic
+ * policy framework before they enter the Aggregation stage.  Routes not
+ * marked for aggregation, or those improperly marked, will be
+ * propagated through both the aggregation and static filtering stages
+ * without any processing whatsoever, regardless on the downstream
+ * peering type (IBGP or EBGP). 
+ *
+ * Routes that can trigger instantiation of an aggregate route (those
+ * that have been properly marked) will be copied and copies stored in
+ * the AggregationTable for further processing.  Regardless of the
+ * outcome of the aggregation processing, every original route will be
+ * marked as IBGP_ONLY and propagated downstream.  Filtering stages
+ * residing on IBGP paths will propagate such routes further downstream,
+ * while on EBGP paths all routes marked as IBGP_ONLY will be discarded.
+ *
+ * In addition to original routes marked as described in the previous
+ * paragraph, the AggregationStage can emit both the aggregate and
+ * copies of the original routes.  Any such routes will be marked with
+ * an appropriate EBGP_ONLY_* marker, instructing the filtering stages
+ * on IBGP branches to unconditionally drop them.  On EBGP branches...
+ *
+ *
+ * Open issues:
+ *
+ * - return values for add/delete/replace routes
+ *
+ * - aggregate route tagging / dumping when a new EBGP peering comes up
+ *
+ * - {de|re}aggragetion of large aggregates -> timer based vs. atomic
+ */
+
+template<class A>
+class AggregationTable;
+
+template<class A>
+class ComponentRoute {
+public:
+    ComponentRoute(const SubnetRoute<A>* route) :
+	_route(route) {
+    };
+    inline const SubnetRoute<A>* route() const { return _route.route(); }
+private:
+    SubnetRouteConstRef<A> _route;
+};
+
+
+template<class A>
+class AggregateRoute {
+public:
+    AggregateRoute(IPNet<A> net, bool brief_mode, LocalData *local_data)
+        : _net(net), _brief_mode(brief_mode),
+	  _was_announced(0), _is_suppressed(0) {
+	_pa_list = new PathAttributeList<A>(NextHopAttribute<A>(A::ZERO()),
+					    AsPath(),
+					    OriginAttribute(IGP));
+	_pa_list->rehash();
+	_aggregator_attribute = new AggregatorAttribute(local_data->id(),
+							local_data->as());
+    }
+    ~AggregateRoute() {
+	if (_components_table.begin() != _components_table.end())
+	    XLOG_WARNING("ComponentsTable trie was not empty on deletion\n");
+	delete _pa_list;
+	delete _aggregator_attribute;
+    }
+    inline const PathAttributeList<A> *pa_list() const { return _pa_list; }
+    inline const IPNet<A> net() const { return _net; }
+    inline const bool was_announced() const { return _was_announced; }
+    inline const bool is_suppressed() const { return _is_suppressed; }
+    inline const bool brief_mode() const { return _brief_mode; }
+    inline void was_announced(bool value) { _was_announced = value; }
+    inline void is_suppressed(bool value) { _is_suppressed = value; }
+    inline void brief_mode(bool value) { _brief_mode = value; }
+    void reevaluate(AggregationTable<A> *parent);
+    RefTrie<A, const ComponentRoute<A> > *components_table() {
+	return &_components_table;
+    }
+private:
+    const IPNet<A> _net;
+    bool _brief_mode;
+    RefTrie<A, const ComponentRoute<A> > _components_table;
+    PathAttributeList<A> *_pa_list;	// Changes over time
+    bool _was_announced;
+    bool _is_suppressed;
+
+    AggregatorAttribute *_aggregator_attribute;
+};
+
+
+template<class A>
+class AggregationTable : public BGPRouteTable<A>  {
+public:
+    AggregationTable(string tablename,
+		     BGPPlumbing& master,
+		     BGPRouteTable<A> *parent);
+    ~AggregationTable();
+    int add_route(const InternalMessage<A> &rtmsg,
+                  BGPRouteTable<A> *caller);
+    int replace_route(const InternalMessage<A> &old_rtmsg,
+                      const InternalMessage<A> &new_rtmsg,
+                      BGPRouteTable<A> *caller);
+    int delete_route(const InternalMessage<A> &rtmsg,
+                     BGPRouteTable<A> *caller);
+    int push(BGPRouteTable<A> *caller);
+
+    const SubnetRoute<A> *lookup_route(const IPNet<A> &net,
+                                       uint32_t& genid) const;
+    void route_used(const SubnetRoute<A>* route, bool in_use);
+
+    RouteTableType type() const {return AGGREGATION_TABLE;}
+    string str() const;
+
+    /* mechanisms to implement flow control in the output plumbing */
+    bool get_next_message(BGPRouteTable<A> *next_table);
+
+    int route_count() const {
+        return _aggregates_table.route_count(); // XXX is this OK?
+    }
+
+private:
+    friend class AggregateRoute<A>;
+
+    RefTrie<A, const AggregateRoute<A> > _aggregates_table;
+    BGPPlumbing& _master_plumbing;
+};
+
+
+#endif __BGP_ROUTE_TABLE_AGGREGATION_HH__
