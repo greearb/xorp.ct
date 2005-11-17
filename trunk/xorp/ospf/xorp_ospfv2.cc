b19730a96c396e7149b8b1e23d5694b5033234f8
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-11-17 17:47:50 +0000

    NOOP.
    +//     xlog_level_set_verbose(XLOG_LEVEL_TRACE, XLOG_VERBOSE_HIGH);
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7636 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/xorp_ospfv2.cc
+++ trunk/xorp/ospf/xorp_ospfv2.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/xorp_ospfv2.cc,v 1.8 2005/09/09 15:32:35 atanu Exp $"
+#ident "$XORP: xorp/ospf/xorp_ospfv2.cc,v 1.9 2005/10/23 09:29:48 atanu Exp $"
 
 #include "config.h"
 #include <set>
@@ -48,6 +48,7 @@ main(int /*argc*/, char **argv)
     xlog_level_set_verbose(XLOG_LEVEL_ERROR, XLOG_VERBOSE_HIGH);
     xlog_level_set_verbose(XLOG_LEVEL_WARNING, XLOG_VERBOSE_HIGH);
     xlog_level_set_verbose(XLOG_LEVEL_INFO, XLOG_VERBOSE_HIGH);
+//     xlog_level_set_verbose(XLOG_LEVEL_TRACE, XLOG_VERBOSE_HIGH);
     xlog_add_default_output();
     xlog_start();
 
