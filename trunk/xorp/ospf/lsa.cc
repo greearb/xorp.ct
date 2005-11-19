73615462ef4b9a79767ac0a9e1bd85a567ec58a1
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-11-19 03:35:18 +0000

    Correction when printing a type 2 router link the link ID field is the IP address of the designated routers not its router ID.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7650 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/lsa.cc
+++ trunk/xorp/ospf/lsa.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/lsa.cc,v 1.60 2005/11/07 09:15:52 atanu Exp $"
+#ident "$XORP: xorp/ospf/lsa.cc,v 1.61 2005/11/10 09:03:21 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -608,7 +608,7 @@ RouterLink::str() const
 	    break;
 	case transit:
 	    output += c_format(" Transit network");
-	    output += c_format(" Designated Router ID %s",
+	    output += c_format(" IP address of Designated router %s",
 			       pr_id(get_link_id()).c_str());
 	    output += c_format(" Routers interface address %s",
 			       pr_id(get_link_data()).c_str());
