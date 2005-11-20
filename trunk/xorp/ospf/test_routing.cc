c0f320a44bf258d4d26856aedfe74b53d14ecd18
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-11-20 19:16:30 +0000

    OSPF no longer sends host routes through to the RIB, therefore host routes to loopback interfaces are also not being sent to the RIB.
    Temporarily comment out the test for loopback interfaces.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7659 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/test_routing.cc
+++ trunk/xorp/ospf/test_routing.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/test_routing.cc,v 1.9 2005/10/31 07:58:40 atanu Exp $"
+#ident "$XORP: xorp/ospf/test_routing.cc,v 1.10 2005/11/04 20:54:36 atanu Exp $"
 
 #include "config.h"
 #include <map>
@@ -822,7 +822,7 @@ main(int argc, char **argv)
     } tests[] = {
 	{"r1V2", callback(routing1<IPv4>, OspfTypes::V2)},
 	{"r1V3", callback(routing1<IPv6>, OspfTypes::V3)},
-	{"r2", callback(routing2)},
+	/*	{"r2", callback(routing2)},*/
 	{"r3V2", callback(routing3<IPv4>, OspfTypes::V2, fname)},
 	{"r3V3", callback(routing3<IPv6>, OspfTypes::V3, fname)},
 	{"r4", callback(routing4)},
