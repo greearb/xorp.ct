fa5cc61532dc18f60cff83e9763e03b7b134edd0
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-11-21 18:19:16 +0000

    Add comment to routes trace variable.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7672 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/trace.hh
+++ trunk/xorp/ospf/trace.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/trace.hh,v 1.10 2005/11/19 03:48:19 atanu Exp $
+// $XORP: xorp/ospf/trace.hh,v 1.11 2005/11/21 01:16:58 atanu Exp $
 
 #ifndef __OSPF_TRACE_HH__
 #define __OSPF_TRACE_HH__
@@ -51,7 +51,7 @@ struct Trace {
     bool _export_policy;
     bool _virtual_link;
     bool _find_interface_address;
-    bool _routes;
+    bool _routes;	// add,replace,delete route.
 };
 
 #endif // __OSPF_TRACE_HH__
