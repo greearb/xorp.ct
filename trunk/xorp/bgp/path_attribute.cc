76f1f1458554fe7c52931ab09d1ed2d2b7bf9f04
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2005-12-19 19:14:27 +0000

    Print community path attributes in hex not decimal.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7833 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/path_attribute.cc
+++ trunk/xorp/bgp/path_attribute.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/path_attribute.cc,v 1.73 2005/12/13 03:59:46 atanu Exp $"
+#ident "$XORP: xorp/bgp/path_attribute.cc,v 1.74 2005/12/13 06:26:16 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -474,7 +474,7 @@ CommunityAttribute::str() const
     string s = "Community Attribute ";
     const_iterator i = _communities.begin();
     for ( ; i != _communities.end();  ++i)
-	s += c_format("%u ", XORP_UINT_CAST(*i));
+	s += c_format("%#x ", XORP_UINT_CAST(*i));
     return s;
 }
 
