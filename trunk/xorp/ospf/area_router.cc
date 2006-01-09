1f4ee446a1b46b18142673dda5d768c3ef50af68
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2006-01-09 12:01:53 +0000

    When processing AS-External-LSAs one forwarding address has been looked up in the routing table and use the nexthop associated with this entry as long as it isn't directly connected.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7906 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/area_router.cc
+++ trunk/xorp/ospf/area_router.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/area_router.cc,v 1.160 2006/01/09 10:22:30 atanu Exp $"
+#ident "$XORP: xorp/ospf/area_router.cc,v 1.161 2006/01/09 10:32:39 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -2747,6 +2747,8 @@ AreaRouter<IPv4>::routing_as_externalV2()
 	RouteEntry<IPv4> rtf;
 	if (!routing_table.longest_match_entry(forwarding, rtf))
 	    continue;
+	if (!rtf.get_directly_connected())
+	    forwarding = rtf.get_nexthop();
 	if (aselsa->external()) {
 	    if (RouteEntry<IPv4>::intra_area != rtf.get_path_type() &&
 		RouteEntry<IPv4>::inter_area != rtf.get_path_type())
