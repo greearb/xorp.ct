4350d841e24bc743eecf74c05b3fa3eb024c3730
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2006-01-16 03:33:47 +0000

    If a route is a discard route print this information.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7976 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/routing_table.hh
+++ trunk/xorp/ospf/routing_table.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/routing_table.hh,v 1.35 2006/01/15 11:02:42 atanu Exp $
+// $XORP: xorp/ospf/routing_table.hh,v 1.36 2006/01/15 11:05:08 atanu Exp $
 
 #ifndef __OSPF_ROUTING_TABLE_HH__
 #define __OSPF_ROUTING_TABLE_HH__
@@ -186,6 +186,7 @@ class RouteEntry {
 	output = c_format("RouteEntry: ");
 	output += c_format("%s ", OspfTypes::Router == _destination_type ?
 			  "Router" : "Network");
+	output += c_format("%s", _discard ? "discard " : "");
 	output += c_format("%s", _direct ? "direct " : "");
 	if (OspfTypes::Network == _destination_type)
 	    output += c_format("Address %s ", pr_id(_address).c_str());
