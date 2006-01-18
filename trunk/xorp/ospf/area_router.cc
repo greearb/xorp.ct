2b822d250a5f540023ac5b087955fd2869e35282
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2006-01-18 09:23:34 +0000

    When responding to a self originated LSA if the LSA is in the database then the response should be with the newly modified database copy. If the LSA is not in the database then the incoming LSA should be sent back set to MaxAge instead of accessing an illegal database index.
    .
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@7990 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/area_router.cc
+++ trunk/xorp/ospf/area_router.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/area_router.cc,v 1.188 2006/01/16 07:38:25 atanu Exp $"
+#ident "$XORP: xorp/ospf/area_router.cc,v 1.189 2006/01/16 13:35:35 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -1436,7 +1436,10 @@ AreaRouter<A>::receive_lsas(PeerID peerid,
 	    if (NEWER == search)
 		match = _db[index]->get_self_originating();
 	    if (self_originated((*i), match, index)) {
-		publish_all(_db[index]);
+		if (NOMATCH == search)
+		    publish_all((*i));
+		else
+		    publish_all(_db[index]);
 		continue;
 	    }
 
