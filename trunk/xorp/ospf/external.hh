a7ec3e06ba58581dbfec03da46cfee8bea35e4a0
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2006-02-07 23:34:48 +0000

    Clarify the documentation string for unique_find_lsa and make it clear that this method may modify the LSA argument.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@8075 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/external.hh
+++ trunk/xorp/ospf/external.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/external.hh,v 1.9 2006/02/03 21:26:19 atanu Exp $
+// $XORP: xorp/ospf/external.hh,v 1.10 2006/02/07 22:20:09 atanu Exp $
 
 #ifndef __OSPF_EXTERNAL_HH__
 #define __OSPF_EXTERNAL_HH__
@@ -138,6 +138,10 @@ class External {
      * Networks with same network number but different prefix lengths
      * can generate the same link state ID. When looking for an LSA
      * make sure that there the lsar that matches the net is found.
+     *
+     * @param lsar search for this LSA in the database. WARNING this
+     * LSA may have its link state ID field modifed by the search.
+     * @param net that must match the LSA. 
      */
     ASExternalDatabase::iterator unique_find_lsa(Lsa::LsaRef lsar,
 						 const IPNet<A>& net);
