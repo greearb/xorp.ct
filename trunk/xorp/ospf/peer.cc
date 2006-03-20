cbb3e680d848fb4131210a642f9c87c4cbc75e72
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2006-03-20 10:24:06 +0000

    If during database exchange the headers of the LSAs did not fit into a packet, due to an initialised variable the exchange could terminate without exchanging all headers.
    Bug found by:	Pavlin
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@8407 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/peer.cc
+++ trunk/xorp/ospf/peer.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/peer.cc,v 1.232 2006/03/06 07:48:47 atanu Exp $"
+#ident "$XORP: xorp/ospf/peer.cc,v 1.233 2006/03/10 06:55:28 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -3169,12 +3169,12 @@ Neighbour<A>::build_data_description_packet()
 	return;
 
     // Open the database if the handle is invalid.
-    bool empty;
-    if (!_database_handle.valid())
+    if (!_database_handle.valid()) {
+	bool empty;
 	_database_handle = get_area_router()->open_database(empty);
-
-    if (empty)
-	goto out;
+	if (empty)
+	    goto out;
+    }
 
     bool last;
     do {
