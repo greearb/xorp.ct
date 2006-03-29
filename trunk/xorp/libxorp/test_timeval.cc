07cd86343fb75ee61206a60079380b722e854816
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2006-03-29 09:12:03 +0000

    * Move the test inside test_random_uniform.cc to test_timeval.cc,   because there is no reason it shouldn't be in test_timeval.cc
    * Cleanup test_timeval.cc
    
    Approved by:	Atanu Ghosh
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@8474 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxorp/test_timeval.cc
+++ trunk/xorp/libxorp/test_timeval.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/libxorp/test_timeval.cc,v 1.5 2005/08/18 15:28:42 bms Exp $"
+#ident "$XORP: xorp/libxorp/test_timeval.cc,v 1.6 2006/03/16 00:04:35 pavlin Exp $"
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -40,8 +40,14 @@ static const char *program_copyright    = "See file LICENSE.XORP";
 static const char *program_return_value = "0 on success, 1 if test error, 2 if internal error";
 
 static bool s_verbose = false;
-bool verbose()                  { return s_verbose; }
-void set_verbose(bool v)        { s_verbose = v; }
+bool verbose()			{ return s_verbose; }
+void set_verbose(bool v)	{ s_verbose = v; }
+
+static int s_failures = 0;
+bool failures()			{ return s_failures; }
+void incr_failures()		{ s_failures++; }
+
+
 
 //
 // printf(3)-like facility to conditionally print a message if verbosity
@@ -49,17 +55,59 @@ void set_verbose(bool v)        { s_verbose = v; }
 //
 #define verbose_log(x...) _verbose_log(__FILE__,__LINE__, x)
 
-#define _verbose_log(file, line, x...)                                  \
-do {                                                                    \
-    if (verbose()) {                                                    \
-        printf("From %s:%d: ", file, line);                             \
-        printf(x);                                                      \
-    }                                                                   \
+#define _verbose_log(file, line, x...)					\
+do {									\
+    if (verbose()) {							\
+	printf("From %s:%d: ", file, line);				\
+	printf(x);							\
+    }									\
 } while(0)
+
+
+//
+// Test and print a message whether two strings are lexicographically same.
+// The strings can be either C or C++ style.
+//
+#define verbose_match(s1, s2)						\
+    _verbose_match(__FILE__, __LINE__, s1, s2)
+
+bool
+_verbose_match(const char* file, int line, const string& s1, const string& s2)
+{
+    bool match = s1 == s2;
+
+    _verbose_log(file, line, "Comparing %s == %s : %s\n",
+		 s1.c_str(), s2.c_str(), match ? "OK" : "FAIL");
+    if (match == false)
+	incr_failures();
+    return match;
+}
+
+
+//
+// Test and print a message whether a condition is true.
+//
+// The first argument is the condition to test.
+// The second argument is a string with a brief description of the tested
+// condition.
+//
+#define verbose_assert(cond, desc) 					\
+    _verbose_assert(__FILE__, __LINE__, cond, desc)
+
+bool
+_verbose_assert(const char* file, int line, bool cond, const string& desc)
+{
+    _verbose_log(file, line,
+		 "Testing %s : %s\n", desc.c_str(), cond ? "OK" : "FAIL");
+    if (cond == false)
+	incr_failures();
+    return cond;
+}
+
 
 /**
  * Print program info to output stream.
- * 
+ *
  * @param stream the output stream the print the program info to.
  */
 static void
@@ -75,7 +123,7 @@ print_program_info(FILE *stream)
 
 /**
  * Print program usage information to the stderr.
- * 
+ *
  * @param progname the name of the program.
  */
 static void
@@ -87,40 +135,71 @@ usage(const char* progname)
     fprintf(stderr, "       -v          : verbose output\n");
 }
 
-static int
-run_test()
+static void
+test_timeval_constants()
 {
-    if (TimeVal::MAXIMUM() <= TimeVal::ZERO()) {
-	verbose_log("bad maximum");
-	return 1;
-    }
+    //
+    // Test TimeVal::MAXIMUM()
+    //
+    verbose_assert(TimeVal::ZERO() < TimeVal::MAXIMUM(),
+		   "compare TimeVal::ZERO() < TimeVal::MAXIMUM()");
 
-    if (TimeVal::MINIMUM() >= TimeVal::ZERO()) {
-	verbose_log("bad minimum");
-	return 1;
-    }
+    //
+    // Test TimeVal::MINIMUM()
+    //
+    verbose_assert(TimeVal::MINIMUM() < TimeVal::ZERO(),
+		   "compare TimeVal::MINIMUM() < TimeVal::ZERO()");
+}
 
-    TimeVal t (100, 100);
-    
+static void
+test_timeval_operators()
+{
+    TimeVal t(100, 100);
     TimeVal mt = -t;
-    if (t + mt != TimeVal::ZERO()) {
-	verbose_log("bad addition or unary negation");
-	return 1;
-    }
 
-    if (t - mt != 2 * t) {
-	verbose_log("bad subtraction or multiplication");
-	return 1;
-    }
-    verbose_log("Pass.\n");
-    return 0;
-};
+    //
+    // Test adding time values
+    //
+    verbose_assert(t + mt == TimeVal::ZERO(), "time addition");
+
+    //
+    // Test subtraction and multiplication
+    //
+    verbose_assert(t - mt == 2 * t, "time subtraction and multiplication");
+}
+
+static void
+test_timeval_random_uniform()
+{
+    static const int TEST_INTERVAL = 30;
+    static const double LOWER_BOUND = 0.75F;
+    TimeVal interval(TEST_INTERVAL, 0);
+    TimeVal tmin((TEST_INTERVAL * LOWER_BOUND) - 1);	// Double expression
+    TimeVal tmax(TEST_INTERVAL + 1, 0);
+
+    //
+    // Calculate jitter which is uniformly distributed as a fraction
+    // of TEST_INTERVAL between LOWER_BOUND (0.75) and 1.0.
+    //
+    TimeVal lower_bound_timeval = TimeVal(interval.get_double() * LOWER_BOUND);
+    TimeVal result = random_uniform(lower_bound_timeval, interval);
+
+    //
+    // Thest that the output is not outside of expected range
+    //
+    string msg = c_format("time random uniform distribution: "
+			  "whether time %s is in range [%s, %s]",
+			  result.str().c_str(),
+			  tmin.str().c_str(),
+			  tmax.str().c_str());
+    verbose_assert((tmin <= result) && (result <= tmax), msg.c_str());
+}
 
 int
 main(int argc, char * const argv[])
 {
     int ret_value = 0;
-    
+
     //
     // Initialize and start xlog
     //
@@ -130,42 +209,45 @@ main(int argc, char * const argv[])
     xlog_level_set_verbose(XLOG_LEVEL_ERROR, XLOG_VERBOSE_HIGH);
     xlog_add_default_output();
     xlog_start();
-    
+
     int ch;
     while ((ch = getopt(argc, argv, "hv")) != -1) {
-        switch (ch) {
-        case 'v':
-            set_verbose(true);
-            break;
-        case 'h':
-        case '?':
-        default:
-            usage(argv[0]);
-            xlog_stop();
-            xlog_exit();
-            if (ch == 'h')
-                return (0);
-            else
-                return (1);
-        }
+	switch (ch) {
+	case 'v':
+	    set_verbose(true);
+	    break;
+	case 'h':
+	case '?':
+	default:
+	    usage(argv[0]);
+	    xlog_stop();
+	    xlog_exit();
+	    if (ch == 'h')
+		return (0);
+	    else
+		return (1);
+	}
     }
     argc -= optind;
     argv += optind;
-    
+
     XorpUnexpectedHandler x(xorp_unexpected_handler);
     try {
-	ret_value = run_test();
+	test_timeval_constants();
+	test_timeval_operators();
+	test_timeval_random_uniform();
+	ret_value = failures() ? 1 : 0;
     } catch (...) {
-        // Internal error
-        xorp_print_standard_exceptions();
-        ret_value = 2;
+	// Internal error
+	xorp_print_standard_exceptions();
+	ret_value = 2;
     }
-    
+
     //
     // Gracefully stop and exit xlog
     //
     xlog_stop();
     xlog_exit();
-    
+
     return (ret_value);
 }
