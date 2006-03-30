b4986e5c1fbc6b6b2c43b0a749eb0b7138e178dd
bms <bms@26435e26-4b25-483a-942d-477236992e84>
2006-03-30 15:23:09 +0000

    Fix a problem with withdrawing routes from the Windows FIB. The problem is only exposed when multiple routing protocols are run which exchange exactly the same route prefixes.
    Make sure that the FEA bottom half completely fills out
    the MIB_IPFORWARDROW structure before passing it to
    DeleteIpForwardEntry(), on Windows Server.
    
    This change relies on the recent changes to the redist
    XRL interface to pass interface name and next-hop information
    to the FEA when withdrawing routes.
    
    While we're here, reintroduce the debug_msg() which instruments
    this path, and kill the old code which relied on GetBestRoute()
    for constructing the MIB_IPFORWARDROW structure.
    
    Significantly update code comments.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@8487 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/fticonfig_entry_set_iphelper.cc
+++ trunk/xorp/fea/fticonfig_entry_set_iphelper.cc
@@ -1,4 +1,5 @@
 // -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*-
+// vim:set sts=4 ts=8:
 
 // Copyright (c) 2001-2006 International Computer Science Institute
 // // Permission is hereby granted, free of charge, to any person obtaining a
@@ -11,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/fticonfig_entry_set_iphelper.cc,v 1.7 2006/03/22 00:41:10 pavlin Exp $"
+#ident "$XORP: xorp/fea/fticonfig_entry_set_iphelper.cc,v 1.8 2006/03/29 17:25:35 bms Exp $"
 
 #include "fea_module.h"
 
@@ -268,45 +269,84 @@ FtiConfigEntrySetIPHelper::delete_entry(const FteX& fte)
     }
 
     //
-    // Windows needs to know the next-hop. We don't always know it.
-    // Try to figure it out via GetBestRoute().
+    // Fully fill out the MIB_IPFORWARDROW members in order.
     //
-    DWORD result;
-    IPAddr tmpdest;
-    IPAddr tmpmask;
-    IPAddr tmpnexthop;
 
     memset(&ipfwdrow, 0, sizeof(ipfwdrow));
 
     fte.net().masked_addr().get_ipv4().copy_out(reinterpret_cast<uint8_t*>(
-						&tmpdest));
+						&ipfwdrow.dwForwardDest));
     fte.net().netmask().get_ipv4().copy_out(reinterpret_cast<uint8_t*>(
-					    &tmpmask));
-    fte.nexthop().get_ipv4().copy_out(reinterpret_cast<uint8_t*>(&tmpnexthop));
+					    &ipfwdrow.dwForwardMask));
 
-    result = GetBestRoute(tmpdest, 0, &ipfwdrow);
-    if (result == NO_ERROR &&
-	ipfwdrow.dwForwardDest == tmpdest &&
-	ipfwdrow.dwForwardMask == tmpmask &&
-	ipfwdrow.dwForwardNextHop == tmpnexthop) {
-	// Don't change any fields
-    } else {
-	ipfwdrow.dwForwardDest = tmpdest;
-	ipfwdrow.dwForwardMask = tmpmask;
-	ipfwdrow.dwForwardNextHop = tmpnexthop;
-        const IfTree& iftree = ftic().iftree();
-        IfTree::IfMap::const_iterator ii = iftree.get_if(fte.ifname());
-	if (ii != iftree.ifs().end()) {
-	    ipfwdrow.dwForwardIfIndex = ii->second.pif_index();
-	} else {
-	    XLOG_WARNING("Failed to lookup iftree object for '%s'",
-			 fte.ifname().c_str());
-	    ipfwdrow.dwForwardIfIndex = 0;
-	}
-        ipfwdrow.dwForwardProto = PROTO_IP_NETMGMT;
-    }
+    ipfwdrow.dwForwardPolicy = 0;
 
-    result = DeleteIpForwardEntry(&ipfwdrow);
+    fte.nexthop().get_ipv4().copy_out(reinterpret_cast<uint8_t*>(
+					    &ipfwdrow.dwForwardNextHop));
+
+    //
+    // If the RIB did not tell us which interface this route was
+    // previously added on, there is a programming error.
+    //
+    XLOG_ASSERT(!fte.ifname().empty());
+
+    //
+    // Look up the interface in the live IfTree.
+    // The FIB needs to know the physical IfIndex for removal.
+    // If the FEA does not know about this interface, there is a
+    // programming error.
+    //
+    const IfTree& iftree = ftic().iftree();
+    IfTree::IfMap::const_iterator ii = iftree.get_if(fte.ifname());
+    XLOG_ASSERT(ii != iftree.ifs().end());
+    ipfwdrow.dwForwardIfIndex = ii->second.pif_index();
+
+    //
+    // The FIB fills out the dwForwardType field strictly according
+    // to RFC 1354. Because we don't specify a specific ForwardType
+    // on creation, specify the non-specific manifest constant
+    // MIB_IPROUTE_TYPE_OTHER when removing the route.
+    //
+    ipfwdrow.dwForwardType = MIB_IPROUTE_TYPE_OTHER;
+    ipfwdrow.dwForwardProto = PROTO_IP_NETMGMT;
+
+    //
+    // It seems to be OK to ignore the age field when deleting an
+    // PROTO_IP_NETMGMT route.
+    //
+    ipfwdrow.dwForwardAge = 0;
+    ipfwdrow.dwForwardNextHopAS = 0;
+
+    //
+    // Don't specify any of the metric fields.
+    //
+    // We are supposed to specify the dwForwardMetric1 field for
+    // PROTO_IP_NETMGMT and leave the others as MIB_IPROUTE_METRIC_UNUSED.
+    //
+    // Currently we don't do this, as to do so would require maintaining
+    // multipath state here. We don't specify it for FIB entry creation-
+    // so Windows will pick the metric from the interface's default metric.
+    //
+    ipfwdrow.dwForwardMetric1 = ipfwdrow.dwForwardMetric2 =
+ipfwdrow.dwForwardMetric3 = ipfwdrow.dwForwardMetric4 =
+ipfwdrow.dwForwardMetric5 = MIB_IPROUTE_METRIC_UNUSED;
+
+    // XXX: debug_msg() does not support >=12 arguments at this time
+    debug_msg("DeleteIpForwardEntry(%p) : %08lx %08lx %lu %08lx %08lx %lu "
+"%lu %lu %lu %lu\n",
+		&ipfwdrow,
+		ipfwdrow.dwForwardDest,
+		ipfwdrow.dwForwardMask,
+		ipfwdrow.dwForwardPolicy,
+		ipfwdrow.dwForwardNextHop,
+		ipfwdrow.dwForwardIfIndex,
+		ipfwdrow.dwForwardType, // format string break above
+		ipfwdrow.dwForwardProto,
+		ipfwdrow.dwForwardAge,
+		ipfwdrow.dwForwardNextHopAS,
+		ipfwdrow.dwForwardMetric1);
+
+    DWORD result = DeleteIpForwardEntry(&ipfwdrow);
     if (result != NO_ERROR) {
 	XLOG_ERROR("DeleteIpForwardEntry() failed, error: %s\n",
 		   win_strerror(result));
