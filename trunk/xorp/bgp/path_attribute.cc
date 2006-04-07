5ef5023b44c0c867315b01ed587c3d715562517f
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2006-04-07 19:38:22 +0000

    If a community attribute is well known print its name as well as its numeric value.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@8565 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/path_attribute.cc
+++ trunk/xorp/bgp/path_attribute.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/path_attribute.cc,v 1.77 2006/01/24 17:25:47 zec Exp $"
+#ident "$XORP: xorp/bgp/path_attribute.cc,v 1.78 2006/03/16 00:03:30 pavlin Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -473,8 +473,20 @@ CommunityAttribute::str() const
 {
     string s = "Community Attribute ";
     const_iterator i = _communities.begin();
-    for ( ; i != _communities.end();  ++i)
+    for ( ; i != _communities.end();  ++i) {
+	switch (*i) {
+	case NO_EXPORT:
+	    s += "NO_EXPORT ";
+	    break;
+	case NO_ADVERTISE:
+	    s += "NO_ADVERTISE ";
+	    break;
+	case NO_EXPORT_SUBCONFED:
+	    s += "NO_EXPORT_SUBCONFED ";
+	    break;
+	}
 	s += c_format("%#x ", XORP_UINT_CAST(*i));
+    }
     return s;
 }
 
