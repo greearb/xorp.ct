0ea25415779df5a86ebc09a880b6dd719f5bb8d2
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2006-04-26 04:38:39 +0000

    Don't use GLOB_TILDE flag when using glob(3). The reasons for its removal are:
    * GLOB_TILDE is not POSIX.
    * Supporting '~' in front of the path to the executable modules
      is questionably useful. E.g., typically the path to the executables
      should be relative to the XORP top-level directory.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@8659 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/rtrmgr/module_manager.cc
+++ trunk/xorp/rtrmgr/module_manager.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/rtrmgr/module_manager.cc,v 1.60 2006/03/14 13:34:10 bms Exp $"
+#ident "$XORP: xorp/rtrmgr/module_manager.cc,v 1.61 2006/03/16 00:06:00 pavlin Exp $"
 
 #include "rtrmgr_module.h"
 
@@ -388,7 +388,7 @@ ModuleManager::expand_execution_path(const string& path, string& expath,
 	    }
 	}
 	glob_t pglob;
-	glob(expath.c_str(), GLOB_TILDE, NULL, &pglob);
+	glob(expath.c_str(), 0, NULL, &pglob);
 	if (pglob.gl_pathc != 1) {
 	    error_msg = c_format("%s: file does not exist", expath.c_str());
 	    return XORP_ERROR;
