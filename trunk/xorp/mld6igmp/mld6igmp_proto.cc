8cb2fb6223556fd2d383812dacfcf8ff70573380
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2006-05-06 07:11:32 +0000

    Mark parameters "dst" and "message_type" as UNUSED(). This fixes a compilation error in case the OS doesn't support IPv4 multicast routing.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@8690 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/mld6igmp/mld6igmp_proto.cc
+++ trunk/xorp/mld6igmp/mld6igmp_proto.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/mld6igmp/mld6igmp_proto.cc,v 1.19 2006/03/16 00:04:44 pavlin Exp $"
+#ident "$XORP: xorp/mld6igmp/mld6igmp_proto.cc,v 1.20 2006/05/05 23:19:50 pavlin Exp $"
 
 
 //
@@ -174,7 +174,9 @@ Mld6igmpVif::mld6igmp_membership_query_recv(const IPvX& src,
 	    break;
 	}
     }
-    
+
+    UNUSED(dst);
+    UNUSED(message_type);
     UNUSED(buffer);
     
     return (XORP_OK);
