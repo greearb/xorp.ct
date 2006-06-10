3a77c5b2d54c31af29a093f3acde8cc2be1fcc27
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2006-06-10 00:15:30 +0000

    * Add new class Mld6igmpSourceSet to store the information about a set   of source addresses.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@8749 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/mld6igmp/mld6igmp_source_record.cc
+++ trunk/xorp/mld6igmp/mld6igmp_source_record.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/mld6igmp/mld6igmp_group_record.cc,v 1.1 2006/06/07 00:01:54 pavlin Exp $"
+#ident "$XORP: xorp/mld6igmp/mld6igmp_source_record.cc,v 1.1 2006/06/07 05:36:34 pavlin Exp $"
 
 //
 // Multicast source record information used by IGMPv3 (RFC 3376) and
@@ -77,4 +77,193 @@ Mld6igmpSourceRecord::Mld6igmpSourceRecord(Mld6igmpGroupRecord& group_record,
 Mld6igmpSourceRecord::~Mld6igmpSourceRecord()
 {
 
+}
+
+Mld6igmpSourceSet::Mld6igmpSourceSet(Mld6igmpGroupRecord& group_record)
+    : _group_record(group_record)
+{
+
+}
+
+Mld6igmpSourceSet::~Mld6igmpSourceSet()
+{
+    // XXX: don't delete the payload, because it might be used elsewhere
+}
+
+/**
+ * Delete the payload of the set, and clear the set itself.
+ */
+void
+Mld6igmpSourceSet::delete_payload_and_clear()
+{
+    Mld6igmpSourceSet::iterator iter;
+
+    //
+    // Delete the payload of the set
+    //
+    for (iter = this->begin(); iter != this->end(); ++iter) {
+	Mld6igmpSourceRecord* source_record = iter->second;
+	delete source_record;
+    }
+
+    //
+    // Clear the set itself
+    //
+    this->clear();
+}
+
+/**
+ * UNION operator for sets.
+ *
+ * @param other the right-hand operand.
+ * @return the union of two sets. Note that if an element is in
+ * both sets, we use the value from the first set.
+ */
+Mld6igmpSourceSet
+Mld6igmpSourceSet::operator+(const Mld6igmpSourceSet& other)
+{
+    Mld6igmpSourceSet result(*this);	// XXX: all elements from the first set
+    Mld6igmpSourceSet::const_iterator iter;
+
+    //
+    // Insert all elements from the second set that are not in the first set
+    //
+    for (iter = other.begin(); iter != other.end(); ++iter) {
+	const IPvX& ipvx = iter->first;
+	if (result.find(ipvx) == result.end())
+	    result.insert(make_pair(iter->first, iter->second));
+    }
+
+    return (result);
+}
+
+/**
+ * UNION operator for sets when the second operand is a set of IPvX
+ * addresses.
+ *
+ * @param other the right-hand operand.
+ * @return the union of two sets. Note that if an element is not in the
+ * first set, then it is created (see @ref Mld6igmpSourceRecord).
+ */
+Mld6igmpSourceSet
+Mld6igmpSourceSet::operator+(const set<IPvX>& other)
+{
+    Mld6igmpSourceSet result(*this);	// XXX: all elements from the first set
+    set<IPvX>::const_iterator iter;
+    Mld6igmpSourceRecord* source_record;
+
+    //
+    // Insert all elements from the second set that are not in the first set
+    //
+    for (iter = other.begin(); iter != other.end(); ++iter) {
+	const IPvX& ipvx = *iter;
+	if (result.find(ipvx) == result.end()) {
+	    source_record = new Mld6igmpSourceRecord(_group_record, ipvx);
+	    result.insert(make_pair(ipvx, source_record));
+	}
+    }
+
+    return (result);
+}
+
+/**
+ * INTERSECTION operator for sets.
+ *
+ * @param other the right-hand operand.
+ * @return the intersection of two sets. Note that we use the values
+ * from the first set.
+ */
+Mld6igmpSourceSet
+Mld6igmpSourceSet::operator*(const Mld6igmpSourceSet& other)
+{
+    Mld6igmpSourceSet result(_group_record);
+    Mld6igmpSourceSet::const_iterator iter;
+
+    //
+    // Insert all elements from the first set that are also in the second set
+    //
+    for (iter = this->begin(); iter != this->end(); ++iter) {
+	const IPvX& ipvx = iter->first;
+	if (other.find(ipvx) != other.end())
+	    result.insert(make_pair(iter->first, iter->second));
+    }
+
+    return (result);
+}
+
+/**
+ * INTERSECTION operator for sets when the second operand is a set of IPvX
+ * addresses.
+ *
+ * @param other the right-hand operand.
+ * @return the intersection of two sets. Note that we use the values
+ * from the first set.
+ */
+Mld6igmpSourceSet
+Mld6igmpSourceSet::operator*(const set<IPvX>& other)
+{
+    Mld6igmpSourceSet result(_group_record);
+    Mld6igmpSourceSet::const_iterator iter;
+
+    //
+    // Insert all elements from the first set that are also in the second set
+    //
+    for (iter = this->begin(); iter != this->end(); ++iter) {
+	const IPvX& ipvx = iter->first;
+	if (other.find(ipvx) != other.end())
+	    result.insert(make_pair(iter->first, iter->second));
+    }
+
+    return (result);
+}
+
+/**
+ * REMOVAL operator for sets.
+ *
+ * @param other the right-hand operand.
+ * @return the elements from the first set (after the elements from
+ * the right-hand set have been removed).
+ */
+Mld6igmpSourceSet
+Mld6igmpSourceSet::operator-(const Mld6igmpSourceSet& other)
+{
+    Mld6igmpSourceSet result(_group_record);
+    Mld6igmpSourceSet::const_iterator iter;
+
+    //
+    // Insert all elements from the first set that are not in the second set
+    //
+    for (iter = this->begin(); iter != this->end(); ++iter) {
+	const IPvX& ipvx = iter->first;
+	if (other.find(ipvx) == other.end())
+	    result.insert(make_pair(iter->first, iter->second));
+    }
+
+    return (result);
+}
+
+/**
+ * REMOVAL operator for sets when the second operand is a set of IPvX
+ * addresses.
+ *
+ * @param other the right-hand operand.
+ * @return the elements from the first set (after the elements from
+ * the right-hand set have been removed).
+ */
+Mld6igmpSourceSet
+Mld6igmpSourceSet::operator-(const set<IPvX>& other)
+{
+    Mld6igmpSourceSet result(_group_record);
+    Mld6igmpSourceSet::const_iterator iter;
+
+    //
+    // Insert all elements from the first set that are not in the second set
+    //
+    for (iter = this->begin(); iter != this->end(); ++iter) {
+	const IPvX& ipvx = iter->first;
+	if (other.find(ipvx) == other.end())
+	    result.insert(make_pair(iter->first, iter->second));
+    }
+
+    return (result);
 }
--- trunk/xorp/mld6igmp/mld6igmp_source_record.hh
+++ trunk/xorp/mld6igmp/mld6igmp_source_record.hh
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP$
+// $XORP: xorp/mld6igmp/mld6igmp_source_record.hh,v 1.1 2006/06/07 05:36:34 pavlin Exp $
 
 #ifndef __MLD6IGMP_MLD6IGMP_SOURCE_RECORD_HH__
 #define __MLD6IGMP_MLD6IGMP_SOURCE_RECORD_HH__
@@ -22,6 +22,9 @@
 // IGMP and MLD source record.
 //
 
+
+#include <map>
+#include <set>
 
 #include "libxorp/ipvx.hh"
 #include "libxorp/timer.hh"
@@ -81,6 +84,90 @@ public:
 private:
     Mld6igmpGroupRecord& _group_record;	// The group record we belong to
     IPvX	_source;		// The source address
+    XorpTimer	_source_timer;		// The source timer
+};
+
+/**
+ * @short A class to store information about a set of sources.
+ */
+class Mld6igmpSourceSet : public map<IPvX, Mld6igmpSourceRecord *> {
+public:
+    /**
+     * Constructor for a given group record
+     *
+     * @param group_record the group record this set belongs to.
+     */
+    Mld6igmpSourceSet(Mld6igmpGroupRecord& group_record);
+
+    /**
+     * Destructor
+     */
+    ~Mld6igmpSourceSet();
+
+    /**
+     * Delete the payload of the set, and clear the set itself.
+     */
+    void delete_payload_and_clear();
+
+    /**
+     * UNION operator for sets.
+     *
+     * @param other the right-hand operand.
+     * @return the union of two sets. Note that if an element is in
+     * both sets, we use the value from the first set.
+     */
+    Mld6igmpSourceSet operator+(const Mld6igmpSourceSet& other);
+
+    /**
+     * UNION operator for sets when the second operand is a set of IPvX
+     * addresses.
+     *
+     * @param other the right-hand operand.
+     * @return the union of two sets. Note that if an element is not in the
+     * first set, then it is created (see @ref Mld6igmpSourceRecord).
+     */
+    Mld6igmpSourceSet operator+(const set<IPvX>& other);
+
+    /**
+     * INTERSECTION operator for sets.
+     *
+     * @param other the right-hand operand.
+     * @return the intersection of two sets. Note that we use the values
+     * from the first set.
+     */
+    Mld6igmpSourceSet operator*(const Mld6igmpSourceSet& other);
+
+    /**
+     * INTERSECTION operator for sets when the second operand is a set of IPvX
+     * addresses.
+     *
+     * @param other the right-hand operand.
+     * @return the intersection of two sets. Note that we use the values
+     * from the first set.
+     */
+    Mld6igmpSourceSet operator*(const set<IPvX>& other);
+
+    /**
+     * REMOVAL operator for sets.
+     *
+     * @param other the right-hand operand.
+     * @return the elements from the first set (after the elements from
+     * the right-hand set have been removed).
+     */
+    Mld6igmpSourceSet operator-(const Mld6igmpSourceSet& other);
+
+    /**
+     * REMOVAL operator for sets when the second operand is a set of IPvX
+     * addresses.
+     *
+     * @param other the right-hand operand.
+     * @return the elements from the first set (after the elements from
+     * the right-hand set have been removed).
+     */
+    Mld6igmpSourceSet operator-(const set<IPvX>& other);
+
+private:
+    Mld6igmpGroupRecord& _group_record;	// The group record this set belongs to
 };
 
 
