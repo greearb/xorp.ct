1a7e605200db94583c3a1027dc71d9acec7d03eb
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2006-08-02 08:06:43 +0000

    Fix a typo inside an error message.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@8991 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/ifconfig_get_proc_linux.cc
+++ trunk/xorp/fea/ifconfig_get_proc_linux.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/ifconfig_get_proc_linux.cc,v 1.23 2005/10/16 07:10:35 pavlin Exp $"
+#ident "$XORP: xorp/fea/ifconfig_get_proc_linux.cc,v 1.24 2006/03/16 00:03:54 pavlin Exp $"
 
 #include "fea_module.h"
 
@@ -57,7 +57,7 @@ IfConfigGetProcLinux::~IfConfigGetProcLinux()
     string error_msg;
 
     if (stop(error_msg) != XORP_OK) {
-	XLOG_ERROR("Cannot stop the Lunux's /proc mechanism to get "
+	XLOG_ERROR("Cannot stop the Linux's /proc mechanism to get "
 		   "information about network interfaces from the underlying "
 		   "system: %s",
 		   error_msg.c_str());
