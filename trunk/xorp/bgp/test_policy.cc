2f7b74388c9f6b5ce0bc61d24143e890f6447adc
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2006-09-08 23:55:51 +0000

    Update the copyright year, because it should be starting with 2006 when the file was created first.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9199 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/test_policy.cc
+++ trunk/xorp/bgp/test_policy.cc
@@ -1,6 +1,6 @@
 // -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*-
 
-// Copyright (c) 2001-2006 International Computer Science Institute
+// Copyright (c) 2006 International Computer Science Institute
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software")
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/test_policy.cc,v 1.1 2006/09/08 18:45:58 mjh Exp $"
+#ident "$XORP: xorp/bgp/test_policy.cc,v 1.2 2006/09/08 22:55:24 mjh Exp $"
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
