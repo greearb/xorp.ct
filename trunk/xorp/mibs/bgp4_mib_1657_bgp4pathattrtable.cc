e432e7317d3a92941455307fe42e014e76aa8e81
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2006-10-09 07:51:16 +0000

    Fix byte ordering when asserting the values of the following fields:  - bgp4PathAttrTable_context_s.bgp4PathAttrPeer  - bgp4PathAttrTable_context_s.bgp4PathAttrIpAddrPrefix
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9261 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/mibs/bgp4_mib_1657_bgp4pathattrtable.cc
+++ trunk/xorp/mibs/bgp4_mib_1657_bgp4pathattrtable.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/mibs/bgp4_mib_1657_bgp4pathattrtable.cc,v 1.17 2006/03/16 00:04:41 pavlin Exp $"
+#ident "$XORP: xorp/mibs/bgp4_mib_1657_bgp4pathattrtable.cc,v 1.18 2006/07/05 22:46:57 atanu Exp $"
 
 
 #include <stack>
@@ -619,9 +619,9 @@ get_v4_route_list_next_done(const XrlError& e,
     
     // Asserting that bgp4PathAttrTable_create_row has correctly initialized
     // the values for the index columns
-    XLOG_ASSERT(row->bgp4PathAttrPeer == peer_id->addr());
+    XLOG_ASSERT(htonl(row->bgp4PathAttrPeer) == peer_id->addr());
     XLOG_ASSERT(row->bgp4PathAttrIpAddrPrefixLen == net->prefix_len());
-    XLOG_ASSERT(row->bgp4PathAttrIpAddrPrefix == net->masked_addr().addr());
+    XLOG_ASSERT(htonl(row->bgp4PathAttrIpAddrPrefix) == net->masked_addr().addr());
 
 
     row->bgp4PathAttrOrigin = (*best_and_origin) & 0xFF;
