2bc5817d2facfa1e5a641768de549ac160ec3a3c
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2006-10-11 23:15:27 +0000

    * Add missing XORP copyright header
    * Replace #include "config.h" with #include "libxorp/xorp.h"
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9268 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/utils/flower_malloc/flower_malloc.c
+++ trunk/xorp/utils/flower_malloc/flower_malloc.c
@@ -1,3 +1,22 @@
+/* -*- c-basic-offset: 4; tab-width: 8; indent-tabs-mode: t -*- */
+/* vim:set sts=4 ts=8: */
+
+/*
+ * Copyright (c) 2001-2006 International Computer Science Institute
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software")
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the XORP LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the XORP LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#ident "$XORP$"
+
 /*
  * Library interposer to collect malloc/calloc/realloc statistics 
  * 
@@ -53,7 +72,8 @@
  * SIGUSR1 can be used to reset all trackers.
  */
 
-#include "config.h"
+#include "libxorp/xorp.h"
+
 #ifdef HOST_OS_FREEBSD   
 
 #include <dlfcn.h>
