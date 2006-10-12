f33a13fda9c725f6521e1d7ca6716a9054383905
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2006-10-12 00:41:54 +0000

    Hold a router ID as an IPv4 not an int.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9272 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/xrl_target.cc
+++ trunk/xorp/ospf/xrl_target.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/xrl_target.cc,v 1.37 2006/05/15 18:58:57 pavlin Exp $"
+#ident "$XORP: xorp/ospf/xrl_target.cc,v 1.38 2006/06/15 06:07:27 pavlin Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -349,7 +349,7 @@ XrlOspfV2Target::ospfv2_0_1_set_ip_router_alert(const bool& ip_router_alert)
 }
 
 XrlCmdError
-XrlOspfV3Target::ospfv3_0_1_set_router_id(const uint32_t& /*id*/)
+XrlOspfV3Target::ospfv3_0_1_set_router_id(const IPv4& /*id*/)
 {
     XLOG_UNFINISHED();
 
--- trunk/xorp/ospf/xrl_target.hh
+++ trunk/xorp/ospf/xrl_target.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/xrl_target.hh,v 1.26 2006/03/28 03:06:56 atanu Exp $
+// $XORP: xorp/ospf/xrl_target.hh,v 1.27 2006/06/15 06:07:27 pavlin Exp $
 
 #ifndef __OSPF_XRL_TARGET_HH__
 #define __OSPF_XRL_TARGET_HH__
@@ -727,7 +727,7 @@ class XrlOspfV3Target : XrlOspfv3TargetBase {
      */
     XrlCmdError ospfv3_0_1_set_router_id(
 	// Input values,
-	const uint32_t&	id);
+	const IPv4&	id);
 
  private:
     Ospf<IPv4>& _ospf_ipv4;
