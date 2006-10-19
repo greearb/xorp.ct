6d56bbd768f3b0d3dda5e4f16d1423ee2c5551f5
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2006-10-19 18:47:02 +0000

    Enable get_status for OSPFv3.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9328 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/xrl_target.cc
+++ trunk/xorp/ospf/xrl_target.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/xrl_target.cc,v 1.46 2006/10/17 00:06:08 atanu Exp $"
+#ident "$XORP: xorp/ospf/xrl_target.cc,v 1.47 2006/10/17 23:49:16 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -132,10 +132,10 @@ XrlOspfV2Target::common_0_1_get_status(uint32_t& status,
 }
 
 XrlCmdError
-XrlOspfV3Target::common_0_1_get_status(uint32_t& /*status*/,
-				       string& /*reason*/)
+XrlOspfV3Target::common_0_1_get_status(uint32_t& status,
+				       string& reason)
 {
-    XLOG_UNFINISHED();
+    status = _ospf_ipv6.status(reason);
 
     return XrlCmdError::OKAY();
 }
