b0f41809102c4c6bd8ad31ecde85c4d7dde04a05
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2006-12-01 02:21:53 +0000

    * Minor refactoring of the list of included header files. * Editorial nits.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9433 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/policy/code_list.cc
+++ trunk/xorp/policy/code_list.cc
@@ -12,82 +12,82 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/policy/code_list.cc,v 1.5 2006/08/09 16:00:07 pavlin Exp $"
-
-#include "libxorp/xorp.h"
+#ident "$XORP: xorp/policy/code_list.cc,v 1.6 2006/11/12 23:37:37 pavlin Exp $"
 
 #include "policy_module.h"
+
+#include "libxorp/xorp.h"
+#include "libxorp/xlog.h"
+
 #include "code_list.hh"
 #include "policy/common/policy_utils.hh"
 
-CodeList::CodeList(const string& p) : _policy(p) {
+CodeList::CodeList(const string& policy)
+    : _policy(policy)
+{
 }
 
-CodeList::~CodeList() {
+CodeList::~CodeList()
+{
     policy_utils::clear_container(_codes);
 }
 
-
-void 
-CodeList::push_back(Code* c) {
+void
+CodeList::push_back(Code* c)
+{
     _codes.push_back(c);
 }
 
-string 
-CodeList::str() const {
+string
+CodeList::str() const
+{
     string ret = "Policy: " + _policy + "\n";
 
-    for(ListCode::const_iterator i = _codes.begin();
-	i != _codes.end(); ++i)
-
+    for (ListCode::const_iterator i = _codes.begin(); i != _codes.end(); ++i) {
         ret += (*i)->str();
+    }
 
-    return ret;    
+    return ret;
 }
 
-void 
-CodeList::link_code(Code& c) const {
+void
+CodeList::link_code(Code& c) const
+{
     // go through all the code we have, and link it to c.
-    for(ListCode::const_iterator i = _codes.begin();
-        i != _codes.end(); ++i) {
-	
+    for (ListCode::const_iterator i = _codes.begin(); i != _codes.end(); ++i) {
 	const Code* tmp = *i;
 
 	// the += operator will check for target correctness.
 	c += *tmp;
-    }    
+    }
 }
 
-void 
-CodeList::get_targets(Code::TargetSet& targets) const {
-
+void
+CodeList::get_targets(Code::TargetSet& targets) const
+{
     // go through all our code and see what targets the code is for
-    for(ListCode::const_iterator i = _codes.begin(); 
-	i != _codes.end() ; ++i) {
-	
+    for (ListCode::const_iterator i = _codes.begin(); i != _codes.end(); ++i) {
 	const Code* c = *i;
-
 	targets.insert(c->_target);
     }
 }
 
-void 
-CodeList::get_tags(const string& protocol, Code::TagSet& tagset) const {
-    
+void
+CodeList::get_tags(const string& protocol, Code::TagSet& tagset) const
+{
     // go through all the code we have.
-    for(ListCode::const_iterator i = _codes.begin(); 
-	i != _codes.end(); ++i) {
-	
+    for (ListCode::const_iterator i = _codes.begin(); i != _codes.end(); ++i) {
 	const Code* c = *i;
 
 	// we only want tags for specific protocols.
-	if(c->_target.protocol != protocol)
+	if (c->_target.protocol != protocol)
 	    continue;
 
 	const Code::TagSet& ts = c->_tags;
 
 	// insert the tags for this protocol.
-	for(Code::TagSet::const_iterator j = ts.begin(); j != ts.end(); ++j) 
+	for (Code::TagSet::const_iterator j = ts.begin(); j != ts.end(); ++j) {
 	    tagset.insert(*j);
+	}
     }
 }
