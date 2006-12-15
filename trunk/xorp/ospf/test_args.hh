15f8887d8d7ef151225e163771b3f2c88f341273
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2006-12-15 00:49:09 +0000

    Allow words to be pushed back to be re-read later.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9491 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/test_args.hh
+++ trunk/xorp/ospf/test_args.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/devnotes/template.hh,v 1.6 2006/03/16 00:03:47 pavlin Exp $
+// $XORP: xorp/ospf/test_args.hh,v 1.1 2006/10/12 20:11:19 atanu Exp $
 
 #ifndef __OSPF_TEST_ARGS_HH__
 #define __OSPF_TEST_ARGS_HH__
@@ -40,6 +40,11 @@ class Args {
 	word = _words[_pos++];
 
 	return true;
+    }
+
+    void push_back() {
+	if (_pos > 1)
+	    _pos--;
     }
 
     const string& original_line() const {
