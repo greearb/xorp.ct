e407af347458472929bd00665980e6f8cd0e60c5
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2006-12-17 04:32:49 +0000

    The AS-external-LSAs and NSSA-LSAs have a Referenced Link State ID that should only be set if the Reference LS Type is non-zero. There was an assert in the code to enforce this condition, change it to a warning as was possible for a badly formed input packet to cause an assert.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9517 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/lsa.hh
+++ trunk/xorp/ospf/lsa.hh
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/lsa.hh,v 1.88 2006/12/17 02:48:23 atanu Exp $
+// $XORP: xorp/ospf/lsa.hh,v 1.89 2006/12/17 03:30:37 atanu Exp $
 
 #ifndef __OSPF_LSA_HH__
 #define __OSPF_LSA_HH__
@@ -1622,13 +1622,15 @@ class ASExternalLsa : public Lsa {
 
     void set_referenced_link_state_id(uint32_t referenced_link_state_id) {
 	XLOG_ASSERT(OspfTypes::V3 == get_version());
-	XLOG_ASSERT(0 != _referenced_ls_type);
+	if (0 == _referenced_ls_type)
+	    XLOG_WARNING("Referenced LS Type is zero");
 	_referenced_link_state_id = referenced_link_state_id;
     }
 
     uint32_t get_referenced_link_state_id() const {
 	XLOG_ASSERT(OspfTypes::V3 == get_version());
-	XLOG_ASSERT(0 != _referenced_ls_type);
+	if (0 == _referenced_ls_type)
+	    XLOG_WARNING("Referenced LS Type is zero");
 	return _referenced_link_state_id;
     }
 
