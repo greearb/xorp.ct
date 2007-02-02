d4f677bed55b65789caed4d93d3ed9d57170c7ae
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-02-02 03:37:33 +0000

    Remove redundant compiler directive.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9567 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/vertex.hh
+++ trunk/xorp/ospf/vertex.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/vertex.hh,v 1.7 2007/01/31 00:57:38 atanu Exp $
+// $XORP: xorp/ospf/vertex.hh,v 1.8 2007/02/02 02:26:47 atanu Exp $
 
 #ifndef __OSPF_VERTEX_HH__
 #define __OSPF_VERTEX_HH__
@@ -48,12 +48,10 @@ class Vertex {
 	return _nodeid < other.get_nodeid();
     }
 
-#if	1
     bool operator==(const Vertex& other) const {
 	XLOG_ASSERT(get_version() == other.get_version());
 	return _nodeid == other.get_nodeid() && _t ==  other.get_type();
     }
-#endif
 
     void set_version(OspfTypes::Version v) {
 	_version = v;
