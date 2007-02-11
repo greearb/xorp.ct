47d01a8b5e8bca008b6b6e55a62dce77a606ef55
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-02-11 04:32:29 +0000

    While scanning through the list of attached routers in a Network-LSA if one of the Router-LSAs has reached MaxAge don't return, continue processing the list.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9574 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/area_router.cc
+++ trunk/xorp/ospf/area_router.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/area_router.cc,v 1.216 2007/02/01 22:49:33 atanu Exp $"
+#ident "$XORP: xorp/ospf/area_router.cc,v 1.217 2007/02/07 04:07:54 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -4084,7 +4084,7 @@ AreaRouter<A>::routing_router_link_transitV2(Spt<Vertex>& spt,
 	    // This can probably never happen
 	    if (lsapeer->maxage()) {
 		XLOG_WARNING("LSA in database MaxAge\n%s", cstring(*lsapeer));
-		return;
+		continue;
 	    }
 
 	    uint32_t interface_address;
