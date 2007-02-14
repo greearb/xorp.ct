e3b0c284ff8786355bc2be65e619787ed9e8678a
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-02-14 21:31:11 +0000

    Fix a typo in a comment.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9600 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/io.hh
+++ trunk/xorp/ospf/io.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/io.hh,v 1.20 2006/03/28 03:06:53 atanu Exp $
+// $XORP: xorp/ospf/io.hh,v 1.21 2006/11/23 01:25:31 atanu Exp $
 
 #ifndef __OSPF_IO_HH__
 #define __OSPF_IO_HH__
@@ -148,7 +148,7 @@ class IO : public ServiceBase {
     }
 
     /**
-     * @return prefix length for this address.
+     * @return the interface id for this interface.
      */
     virtual bool get_interface_id(const string& interface,
 				  uint32_t& interface_id) = 0;
