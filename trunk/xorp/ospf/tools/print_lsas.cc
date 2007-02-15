cd006d3111fdaebeddbf71f6fe55047e7d163736
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-02-15 22:57:30 +0000

    The program that prints LSAs takes a filter argument that selects the type of the LSA to print. In OSPFv2 the LSA values range from 1 to 7, in OSPFv3 they are in the 0x2001 .. range. The filter argument could only be expressed in decimal it can now be expressed in octal and hexadecimal.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9604 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/tools/print_lsas.cc
+++ trunk/xorp/ospf/tools/print_lsas.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/tools/print_lsas.cc,v 1.17 2006/12/06 01:34:35 atanu Exp $"
+#ident "$XORP: xorp/ospf/tools/print_lsas.cc,v 1.18 2006/12/11 21:32:14 atanu Exp $"
 
 // Get LSAs (in raw binary) from OSPF and print them.
 
@@ -516,7 +516,13 @@ main(int argc, char **argv)
 	    area = optarg;
 	    break;
 	case 'f':
-	    filter.add(atoi(optarg));
+	    char *endptr;
+	    uint32_t number = strtoul(optarg, &endptr, 0);
+	    if (0 != *endptr) {
+		XLOG_ERROR("<%s> is not a number", optarg);
+		return -1;
+	    }
+	    filter.add(number);
 	    break;
 	case 's':
 	    pstyle = SUMMARY;
