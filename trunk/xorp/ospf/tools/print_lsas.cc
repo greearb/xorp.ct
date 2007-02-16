62faf9599a76b93380dc0bfdf14e88251811d5e1
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-02-16 10:09:29 +0000

    Add some brackets in a switch statement to satisfy gcc 2.95.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9613 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/tools/print_lsas.cc
+++ trunk/xorp/ospf/tools/print_lsas.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/tools/print_lsas.cc,v 1.19 2007/02/15 22:57:30 atanu Exp $"
+#ident "$XORP: xorp/ospf/tools/print_lsas.cc,v 1.20 2007/02/16 02:07:14 atanu Exp $"
 
 // Get LSAs (in raw binary) from OSPF and print them.
 
@@ -522,7 +522,7 @@ main(int argc, char **argv)
 	case 'a':
 	    area = optarg;
 	    break;
-	case 'f':
+	case 'f': {
 	    char *endptr;
 	    uint32_t number = strtoul(optarg, &endptr, 0);
 	    if (0 != *endptr) {
@@ -530,6 +530,7 @@ main(int argc, char **argv)
 		return -1;
 	    }
 	    filter.add(number);
+	}
 	    break;
 	case 's':
 	    pstyle = SUMMARY;
