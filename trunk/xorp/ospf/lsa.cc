29c2d7e1350dd61d5c2e16ec4b2818962338d532
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-02-26 23:25:17 +0000

    Fix the field width when printing a options in an IPv6Prefix.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9694 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/lsa.cc
+++ trunk/xorp/ospf/lsa.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/lsa.cc,v 1.99 2007/02/18 11:01:58 atanu Exp $"
+#ident "$XORP: xorp/ospf/lsa.cc,v 1.100 2007/02/24 09:08:06 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -516,7 +516,7 @@ IPv6Prefix::str() const
 
     string output;
 
-    output = c_format("Options %#x", get_prefix_options());
+    output = c_format("Options %#4x", get_prefix_options());
     output += c_format(" DN-bit: %d", get_dn_bit());
     output += c_format(" P-bit: %d", get_p_bit());
     output += c_format(" MC-bit: %d", get_mc_bit());
