f8e127bb86bf3e5022d81d21e2d07283560b93a4
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-03-30 04:37:14 +0000

    If the peer is in state waiting force the DR and BDR computation.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@9863 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/peer.cc
+++ trunk/xorp/ospf/peer.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/peer.cc,v 1.276 2007/03/22 17:15:52 atanu Exp $"
+#ident "$XORP: xorp/ospf/peer.cc,v 1.277 2007/03/29 23:49:18 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -2271,7 +2271,8 @@ Peer<A>::compute_designated_router_and_backup_designated_router()
     // If nothing has changed out of here.
     if (_hello_packet.get_designated_router() == dr &&
 	_hello_packet.get_backup_designated_router() == bdr)
-	return;
+	if (Waiting != get_state())
+	    return;
 
     bool recompute = false;
     // Has this router just become the DR or BDR
