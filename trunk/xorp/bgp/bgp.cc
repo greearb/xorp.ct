3cb1d18ff20127dcafbb9516a53add86f080598f
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-07-06 01:25:41 +0000

    BGP does not initiate or accept TCP connections until the policy manager has configured the policy filters in BGP. The signal that the policy manger has finished configuring BGP is a policy push. In the regression tests the router manager is started with an empty configuration and then configuration is introduced with via the xorpsh. Once the xorpsh completes the configuration immediate attempts are made by test peers to make TCP connections to the BGP process. The tests were failing as BGP was still waiting for the policy push.
    If BGP is waiting for the policy push return a status of
    PROC_NOT_READY, the router manager will wait until the status is
    PROC_READY before signalling to the xorpsh that the configuration has
    completed. This change should only affect regression tests.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10130 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/bgp.cc
+++ trunk/xorp/bgp/bgp.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/bgp.cc,v 1.85 2007/05/08 01:15:49 pavlin Exp $"
+#ident "$XORP: xorp/bgp/bgp.cc,v 1.86 2007/05/23 04:08:21 pavlin Exp $"
 
 // #define DEBUG_MAXIMUM_DELAY
 // #define DEBUG_LOGGING
@@ -237,6 +237,9 @@ BGPMain::status(string& reason)
     } else if (! _is_ifmgr_ready) {
 	s = PROC_NOT_READY;
 	reason = "Waiting for interface manager";
+    } else if (!_first_policy_push) {
+	s = PROC_NOT_READY;
+	reason = "Waiting for policy manager";
     }
 
     return s;
