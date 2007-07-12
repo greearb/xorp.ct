75a7c84a3d7bcbe0563dda2847f4f48f06257440
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2007-07-12 22:51:50 +0000

    Fix a typo in commented-out code: rtsock -> routing_socket
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10149 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/data_plane/fibconfig/fibconfig_entry_observer_rtmv2.cc
+++ trunk/xorp/fea/data_plane/fibconfig/fibconfig_entry_observer_rtmv2.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/data_plane/fibconfig/fibconfig_entry_observer_rtmv2.cc,v 1.6 2007/06/07 01:28:37 pavlin Exp $"
+#ident "$XORP: xorp/fea/data_plane/fibconfig/fibconfig_entry_observer_rtmv2.cc,v 1.7 2007/07/11 22:18:07 pavlin Exp $"
 
 #include "fea/fea_module.h"
 
@@ -101,7 +101,7 @@ FibConfigEntryObserverRtmV2::receive_data(const vector<uint8_t>& buffer)
 }
 
 void
-FibConfigEntryObserverRtmV2::rtsock_data(const vector<uint8_t>& buffer)
+FibConfigEntryObserverRtmV2::routing_socket_data(const vector<uint8_t>& buffer)
 {
     receive_data(buffer);
 }
