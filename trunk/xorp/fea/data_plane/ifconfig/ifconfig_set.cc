6832c2f9b2faaa8b8b27082a066ba84db8653d70
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2007-09-10 17:38:41 +0000

    If a vif is a VLAN, then the interface name and the vif name don't have to match.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10305 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/data_plane/ifconfig/ifconfig_set.cc
+++ trunk/xorp/fea/data_plane/ifconfig/ifconfig_set.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/data_plane/ifconfig/ifconfig_set.cc,v 1.6 2007/07/11 22:18:15 pavlin Exp $"
+#ident "$XORP: xorp/fea/data_plane/ifconfig/ifconfig_set.cc,v 1.7 2007/09/09 04:40:42 pavlin Exp $"
 
 #include "fea/fea_module.h"
 
@@ -83,6 +83,8 @@ IfConfigSet::push_config(IfTree& iftree)
 	//
 	for (vi = i.vifs().begin(); vi != i.vifs().end(); ++vi) {
 	    IfTreeVif& v= vi->second;
+	    if (v.is_vlan())
+		continue;
 	    if (v.vifname() != i.ifname()) {
 		ifconfig().ifconfig_error_reporter().vif_error(i.ifname(),
 							       v.vifname(),
