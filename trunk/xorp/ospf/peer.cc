298348ddda8dbf392cc774a5d8acb7ca6b35d654
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-09-13 22:06:44 +0000

    If an adjacency is lost and the state of the adjacency was greater than TwoWay and the link-type does not require DR election update the links in the Router-LSA.
    Previously Point-to-Point and Point-to-MultiPoint links did not have
    their links removed.
    
    Bug found by:	Erlend Knutsen <Erlend.Knutsen AT applica.no>
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10310 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/peer.cc
+++ trunk/xorp/ospf/peer.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/peer.cc,v 1.280 2007/08/17 23:28:13 atanu Exp $"
+#ident "$XORP: xorp/ospf/peer.cc,v 1.281 2007/08/18 00:11:02 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -3947,8 +3947,12 @@ Neighbour<A>::tear_down_state(State previous_state)
     if (_peer.do_dr_or_bdr() && is_DR() && Full == previous_state)
 	_peer.adjacency_change(false);
 
-    if (_peer.do_dr_or_bdr() && TwoWay <= previous_state)
-	_peer.schedule_event("NeighbourChange");
+    if (TwoWay <= previous_state) {
+	if (_peer.do_dr_or_bdr())
+	    _peer.schedule_event("NeighbourChange");
+	else
+	    _peer.update_router_links();
+    }
 }
 
 /**
