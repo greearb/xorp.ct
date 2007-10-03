9c2f1a95e05e779afabc3254a99f00e0b6d6193e
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-10-03 22:03:03 +0000

    The link-local address prefix for IPv6 was incorrect in the debugging code. Problem was noticed because the receive code for OSPFv3 now checks that the source address is link-local and the regression test test_peering was generating a warning.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10364 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/debug_io.hh
+++ trunk/xorp/ospf/debug_io.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/ospf/debug_io.hh,v 1.23 2007/03/12 10:16:03 atanu Exp $
+// $XORP: xorp/ospf/debug_io.hh,v 1.24 2007/05/23 04:08:27 pavlin Exp $
 
 #ifndef __OSPF_DEBUG_IO_HH__
 #define __OSPF_DEBUG_IO_HH__
@@ -203,7 +203,7 @@ class DebugIO : public IO<A> {
 	// is a number, solves a problem with test_peering. Should
 	// really hash the whole interface and vid and use the name
 	// from the info structure.
-	string addr = "fc00::";
+	string addr = "fe80::";
 	addr.push_back(vif[vif.size() -1]);
 	DOUT(_info) << "address = " << addr << ")\n";
 
