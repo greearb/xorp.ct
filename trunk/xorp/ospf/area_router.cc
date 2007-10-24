7db682b2f5c84649e2d0cd9a474ac76eda31f1ee
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2007-10-24 00:56:51 +0000

    For OSPFv3 in stub/nssa areas only flood LSA's with link-local and area scope the test still captures unknown LSAs. Previously the code flooded everything that wasn't AS scope, so would have flooded LSA's with the reserved bits set.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10410 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/ospf/area_router.cc
+++ trunk/xorp/ospf/area_router.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/ospf/area_router.cc,v 1.285 2007/10/17 16:08:46 atanu Exp $"
+#ident "$XORP: xorp/ospf/area_router.cc,v 1.286 2007/10/19 02:43:06 atanu Exp $"
 
 // #define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -2363,8 +2363,15 @@ AreaRouter<A>::receive_lsas(OspfTypes::PeerID peerid,
 	    case OspfTypes::V2:
 		break;
 	    case OspfTypes::V3:
-		if (!(*i)->known() && (*i)->as_scope())
+		// Note unknown LSAs are captured in this test.
+		if (!(*i)->link_local_scope() && !(*i)->area_scope())
 		    continue;
+#if	0
+		if (!(*i)->known()) {
+		    XLOG_INFO("%s", cstring(*(*i)));
+		    (*i)->set_tracing(true);
+		}
+#endif
 		break;
 	    }
 	    break;
