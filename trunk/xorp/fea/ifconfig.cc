4137a28640279c694a0d6fd3ddd862b6e70b1a3f
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2007-12-21 03:05:15 +0000

    Compilation fix for Windows: added missing UNUSED() statement.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10508 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/fea/ifconfig.cc
+++ trunk/xorp/fea/ifconfig.cc
@@ -12,7 +12,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/fea/ifconfig.cc,v 1.75 2007/10/12 07:53:45 pavlin Exp $"
+#ident "$XORP: xorp/fea/ifconfig.cc,v 1.76 2007/12/19 04:57:32 pavlin Exp $"
 
 #include "fea_module.h"
 
@@ -858,6 +858,8 @@ uint32_t
 IfConfig::get_insert_ifindex(const string& ifname)
 {
     uint32_t ifindex = 0;
+
+    UNUSED(ifname);
 
 #ifdef HAVE_IF_NAMETOINDEX
     ifindex = if_nametoindex(ifname.c_str());
