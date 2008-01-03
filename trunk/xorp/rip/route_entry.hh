437428ccd74f2b55bc9ad2672eac5b1a5582853c
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2008-01-03 23:04:42 +0000

    Fix kdoc comments.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10540 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/rip/route_entry.hh
+++ trunk/xorp/rip/route_entry.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/rip/route_entry.hh,v 1.17 2007/05/23 12:12:49 pavlin Exp $
+// $XORP: xorp/rip/route_entry.hh,v 1.18 2007/11/21 08:29:39 pavlin Exp $
 
 #ifndef __RIP_ROUTE_ENTRY_HH__
 #define __RIP_ROUTE_ENTRY_HH__
@@ -107,7 +107,7 @@ public:
     /**
      * Set the outgoing interface name.
      *
-     * @ifname the outgoing interface name.
+     * @param ifname the outgoing interface name.
      * @return true if the stored interface name changed, false otherwise.
      */
     bool set_ifname(const string& ifname);
@@ -122,7 +122,7 @@ public:
     /**
      * Set the outgoing vif name.
      *
-     * @ifname the outgoing vif name.
+     * @param vifname the outgoing vif name.
      * @return true if the stored vif name changed, false otherwise.
      */
     bool set_vifname(const string& vifname);
