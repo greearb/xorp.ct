816b4095131332b3f5b075594121caa623e3e1ed
atanu <atanu@26435e26-4b25-483a-942d-477236992e84>
2008-04-23 03:29:44 +0000

    If a BGP peering is bounced due to the interface address changing print an info message. One of the tests was failing because the peering was being bounced.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10616 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/bgp.cc
+++ trunk/xorp/bgp/bgp.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/bgp/bgp.cc,v 1.90 2007/12/10 23:26:25 mjh Exp $"
+#ident "$XORP: xorp/bgp/bgp.cc,v 1.91 2008/01/04 03:15:17 pavlin Exp $"
 
 // #define DEBUG_MAXIMUM_DELAY
 // #define DEBUG_LOGGING
@@ -1158,8 +1158,11 @@ BGPMain::local_ip_changed(string local_address)
 	const Iptuple& iptuple = (*i)->peerdata()->iptuple();
 	debug_msg("Trying: %s ", iptuple.str().c_str());
 
-	if (iptuple.get_local_addr() == local_address)
+	if (iptuple.get_local_addr() == local_address) {
+	    XLOG_INFO("Interface address %s changed bouncing peer",
+		      local_address.c_str());
 	    bounce_peer(iptuple);
+	}
     }
 }
 
