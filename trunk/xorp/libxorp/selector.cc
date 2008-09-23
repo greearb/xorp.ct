b0e1a4a25f3014ecd43504f3cb30070146324e93
abittau <abittau@26435e26-4b25-483a-942d-477236992e84>
2008-09-23 19:55:48 +0000

    Force commit to correct CVS log message.  Message /tmp/log.r457 should read:
    get rid of superfluous select calls [+4%].  Before we'd call select 3x for each
    send / receive.  Now only once.  Furthermore, I call time() after select, only
    if select has a timeout greater than 0.  It is common to call select with a
    timeout with 0 so this often spares us a call to time().  To send / receive, we
    now do the following syscalls:
    
    1) time.  Start of event loop.
    2) select.
    3) read / write
    4) time, only if select had a timeout.  This call is not performed when there is
       data queued, and when there's a high throughput of XRLs, which is good
       because that's exactly when we need spare cycles to do work.
    
    Before it was:
    
    1) time.
    2) time.
    3) time.
    4) select.
    5) select.
    6) select.
    7) time.
    8) read / write.
    9) time.
    
    The overall syscall reduction got us 11%.  From 10961 xrls/s, to 12202 xrls/s.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10961 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxorp/selector.cc
+++ trunk/xorp/libxorp/selector.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/libxorp/selector.cc,v 1.45 2008/09/23 08:04:12 abittau Exp $"
+#ident "$XORP: xorp/libxorp/selector.cc,v 1.46 2008/09/23 08:04:25 abittau Exp $"
 
 #include "libxorp_module.h"
 
--- trunk/xorp/libxorp/selector.hh
+++ trunk/xorp/libxorp/selector.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/libxorp/selector.hh,v 1.29 2008/09/23 08:04:12 abittau Exp $
+// $XORP: xorp/libxorp/selector.hh,v 1.30 2008/09/23 08:04:25 abittau Exp $
 
 #ifndef __LIBXORP_SELECTOR_HH__
 #define __LIBXORP_SELECTOR_HH__
