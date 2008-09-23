dff1a7fd34154812059a561ef56a57cb46fbd768
abittau <abittau@26435e26-4b25-483a-942d-477236992e84>
2008-09-23 19:55:37 +0000

    Force commit to correct CVS log message.  Message /tmp/log.r454 should read:
    add a test for measuring XRL delays
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10959 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxipc/test_receiver.cc
+++ trunk/xorp/libxipc/test_receiver.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/libxipc/test_receiver.cc,v 1.3 2008/09/23 08:03:09 abittau Exp $"
+#ident "$XORP: xorp/libxipc/test_receiver.cc,v 1.4 2008/09/23 19:43:10 abittau Exp $"
 
 #include "xrl_module.h"
 #include "libxorp/xorp.h"
--- trunk/xorp/libxipc/test_xrl_sender.cc
+++ trunk/xorp/libxipc/test_xrl_sender.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/libxipc/test_xrl_sender.cc,v 1.27 2008/09/23 08:05:17 abittau Exp $"
+#ident "$XORP: xorp/libxipc/test_xrl_sender.cc,v 1.28 2008/09/23 19:43:10 abittau Exp $"
 
 //
 // Test XRLs sender.
