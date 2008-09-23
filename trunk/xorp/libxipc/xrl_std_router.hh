0fa9b64e7ed3c34e6a1b7acfd70870373b88fba9
abittau <abittau@26435e26-4b25-483a-942d-477236992e84>
2008-09-23 19:58:38 +0000

    Force commit to correct CVS log message.  Message /tmp/log.r499 should read:
    the creation of unix sockets is now a parameter - on by default
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10985 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxipc/xrl_std_router.hh
+++ trunk/xorp/libxipc/xrl_std_router.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/libxipc/xrl_std_router.hh,v 1.22 2008/09/23 19:58:26 abittau Exp $
+// $XORP: xorp/libxipc/xrl_std_router.hh,v 1.23 2008/09/23 19:58:33 abittau Exp $
 
 #ifndef __LIBXIPC_XRL_STD_ROUTER_HH__
 #define __LIBXIPC_XRL_STD_ROUTER_HH__
