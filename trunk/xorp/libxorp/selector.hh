ae7fe8e0ed946cc4af816225e2f53ad60674d17a
abittau <abittau@26435e26-4b25-483a-942d-477236992e84>
2008-09-23 19:57:34 +0000

    Force commit to correct CVS log message.  Message /tmp/log.r488 should read:
    respect the priority of selectors & tasks.  We now dispatch only 1 selector at a
    time, and then recompute priorities.  this got a 2% improvement in performance
    by the way.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10976 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxorp/selector.hh
+++ trunk/xorp/libxorp/selector.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/libxorp/selector.hh,v 1.30 2008/09/23 08:04:25 abittau Exp $
+// $XORP: xorp/libxorp/selector.hh,v 1.31 2008/09/23 19:55:48 abittau Exp $
 
 #ifndef __LIBXORP_SELECTOR_HH__
 #define __LIBXORP_SELECTOR_HH__
