f1c9a3574275d0eee09061a1bb97741f8ba9f1eb
abittau <abittau@26435e26-4b25-483a-942d-477236992e84>
2008-09-23 19:58:03 +0000

    Force commit to correct CVS log message.  Message /tmp/log.r494 should read:
    make the eventloop's "aggressiveness" a parameter.  I've added a command line
    argument to tune this in the XRL benchmark.  Try running with -a 0 ["old"
    eventloop] and -a 5 ["aggressive" eventloop].  It makes a 20% difference on my
    box: from 27K XRL/s to 32K XRL/s.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10980 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxipc/test_xrl_sender.cc
+++ trunk/xorp/libxipc/test_xrl_sender.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/libxipc/test_xrl_sender.cc,v 1.31 2008/09/23 19:57:04 abittau Exp $"
+#ident "$XORP: xorp/libxipc/test_xrl_sender.cc,v 1.32 2008/09/23 19:57:55 abittau Exp $"
 
 //
 // Test XRLs sender.
--- trunk/xorp/libxorp/eventloop.cc
+++ trunk/xorp/libxorp/eventloop.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/libxorp/eventloop.cc,v 1.38 2008/09/23 19:57:39 abittau Exp $"
+#ident "$XORP: xorp/libxorp/eventloop.cc,v 1.39 2008/09/23 19:57:46 abittau Exp $"
 
 #include "libxorp_module.h"
 
