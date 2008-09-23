d4e46fef13cacdb024bb233efd57c89a5a27326d
abittau <abittau@26435e26-4b25-483a-942d-477236992e84>
2008-09-23 19:57:39 +0000

    Force commit to correct CVS log message.  Message /tmp/log.r489 should read:
    relax eventloop a bit.  Same performance, but safer.  We need a better solution
    though.  I don't like the old event loop, but I don't like the new one either (I
    prefer it slightly though =D).  I'll think about it carefully tomorrow.
    
    
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@10977 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxorp/selector.cc
+++ trunk/xorp/libxorp/selector.cc
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-#ident "$XORP: xorp/libxorp/selector.cc,v 1.48 2008/09/23 19:57:28 abittau Exp $"
+#ident "$XORP: xorp/libxorp/selector.cc,v 1.49 2008/09/23 19:57:34 abittau Exp $"
 
 #include "libxorp_module.h"
 
