f3eb11b3f7ed705c08c44fcd5e0e5330044b93b7
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2008-10-01 22:40:15 +0000

    Fix a comment.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@11009 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxorp/profile.hh
+++ trunk/xorp/libxorp/profile.hh
@@ -13,7 +13,7 @@
 // notice is a summary of the XORP LICENSE file; the license in that file is
 // legally binding.
 
-// $XORP: xorp/libxorp/profile.hh,v 1.15 2008/09/23 19:57:04 abittau Exp $
+// $XORP: xorp/libxorp/profile.hh,v 1.16 2008/09/23 20:20:04 abittau Exp $
 
 #ifndef __LIBXORP_PROFILE_HH__
 #define __LIBXORP_PROFILE_HH__
@@ -300,4 +300,4 @@ namespace SP {
 #endif
 } // namespace SP
 
-#endif // __LIBXORP_TRACE_HH__
+#endif // __LIBXORP_PROFILE_HH__
