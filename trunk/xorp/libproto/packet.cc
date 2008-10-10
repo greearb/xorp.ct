7094822c3e7679e1cde0d180dbf321b39f60d35b
pavlin <pavlin@26435e26-4b25-483a-942d-477236992e84>
2008-10-10 01:53:54 +0000

    Bug fix: use the IP header length (method ip_header_len()) instead of method size() to compute the IP header checksum.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@11081 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libproto/packet.cc
+++ trunk/xorp/libproto/packet.cc
@@ -19,7 +19,7 @@
 // XORP, Inc, 2953 Bunker Hill Lane, Suite 204, Santa Clara, CA 95054, USA;
 // http://xorp.net
 
-#ident "$XORP: xorp/libproto/packet.cc,v 1.12 2008/10/10 01:11:03 pavlin Exp $"
+#ident "$XORP: xorp/libproto/packet.cc,v 1.13 2008/10/10 01:46:48 pavlin Exp $"
 
 
 //
@@ -233,7 +233,7 @@ void
 IpHeader4Writer::compute_checksum()
 {
     set_ip_sum(0);
-    set_ip_sum(ntohs(inet_checksum(data(), size())));
+    set_ip_sum(ntohs(inet_checksum(data(), ip_header_len())));
 }
 
 ArpHeader&
