79dd179658e6a2c5699fd53db170ffa284fd3792
jtc <jtc@26435e26-4b25-483a-942d-477236992e84>
2008-11-12 23:10:40 +0000

    Changed fprintf(_ostream, s.c_str()) to fputs(s.c_str(), _ostream), as the former can trigger g++'s *printf format directive checking warnings (I saw this on ubuntu 8.10). Reviewed by:	mjh
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@11183 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/bgp/route_table_debug.cc
+++ trunk/xorp/bgp/route_table_debug.cc
@@ -17,7 +17,7 @@
 // XORP Inc, 2953 Bunker Hill Lane, Suite 204, Santa Clara, CA 95054, USA;
 // http://xorp.net
 
-#ident "$XORP: xorp/bgp/route_table_debug.cc,v 1.18 2008/10/02 21:56:19 bms Exp $"
+#ident "$XORP: xorp/bgp/route_table_debug.cc,v 1.19 2008/11/08 06:14:38 mjh Exp $"
 
 //#define DEBUG_LOGGING
 // #define DEBUG_PRINT_FUNCTION_NAME
@@ -54,7 +54,7 @@ DebugTable<A>::print_route(const SubnetRoute<A>& route, FPAListRef palist) const
     s = "SubnetRoute:\n";
     s += "  Net: " + route.net().str() + "\n";
     s += "  PAList: " + palist->str() + "\n";
-    fprintf(_ofile, s.c_str());
+    fputs(s.c_str(), _ofile);
 }
 
 
