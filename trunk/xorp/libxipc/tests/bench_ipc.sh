d401e7bb34e4023c106d778a57cbda26fa046b04
bms_fbsd <bms_fbsd@26435e26-4b25-483a-942d-477236992e84>
2009-12-02 11:11:00 +0000

    Rephrase a few things in here, soon we won't need to embed path names in this file.
    git-svn-id: https://xorp.svn.sourceforge.net/svnroot/xorp@11642 26435e26-4b25-483a-942d-477236992e84

--- trunk/xorp/libxipc/tests/bench_ipc.sh
+++ trunk/xorp/libxipc/tests/bench_ipc.sh
@@ -11,8 +11,11 @@
 # Conditionally set ${srcdir} if it wasn't assigned (e.g., by `gmake check`)
 if [ "X${srcdir}" = "X" ] ; then srcdir=`dirname $0` ; fi
 
-BINDIR=.
+# XXX
+BINDIR=/tmp/xorp/libxipc
+TBINDIR=/home/bms/svn/xorp/xorp/obj/x86_64-unknown-freebsd7.2/libxipc/tests
 
+#${BINDIR}/xorp_finder &
 ${BINDIR}/xorp_finder &
 FINDER_PID=$!
 
@@ -29,7 +32,7 @@ test_pf()
     echo "-------------------------------------"
 
     for i in 0 1 2 3 4 5 6 7 8 9 10 12 15 18 20 25; do
-	XORP_PF=${pfenv} ${BINDIR}/test_xrl_sender ${pfcmd} -n $i
+	XORP_PF=${pfenv} ${TBINDIR}/test_xrl_sender ${pfcmd} -n $i
     done | tee ${rawfile}
     outfile=${pfname}.dat
     cat ${rawfile} | awk -f ${srcdir}/bench_ipc.awk > $outfile
@@ -42,5 +45,6 @@ test_pf()
 #test_pf "inproc" "i" "-m 0 -r"
 #test_pf "udp" "u" "-m 0 -r"
 test_pf "tcp" "t" "-m 0 -r"
+test_pf "local" "x" "-m 0 -r"
 
 kill ${FINDER_PID}
