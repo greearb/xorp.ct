520b6b63f70148836b25f76aa52aad32a05be1be
Ben Greear <greearb@candelatech.com>
2010-06-01 13:02:59 -0700

    Remove more commented 1.7 strings. Signed-off-by: Ben Greear <greearb@candelatech.com>

--- xorp/fea/xrl_fea_io.hh
+++ xorp/fea/xrl_fea_io.hh
@@ -17,7 +17,6 @@
 // XORP Inc, 2953 Bunker Hill Lane, Suite 204, Santa Clara, CA 95054, USA;
 // http://xorp.net
 
-// $XORP: xorp/fea/xrl_fea_io.hh,v 1.7 2008/10/02 21:56:50 bms Exp $
 
 
 #ifndef __FEA_XRL_FEA_IO_HH__
