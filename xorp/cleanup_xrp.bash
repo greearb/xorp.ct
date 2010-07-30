de48857c929f24fb258cd0c74328e041c5cf9aa6
Ben Greear <greearb@candelatech.com>
2010-07-30 12:49:26 -0700

    buildbot:  Tweak cleanup script for better cleanup.
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- xorp/cleanup_xrp.bash
+++ xorp/cleanup_xrp.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-#  Try to initialize the system before we start doing xorp tests.
+#  Try to initialize the system before we start doing xorp buildbot
+#  builds and tests.
 
 # Kill all existing xorp processes
 killall -9 -r xorp
 
 # Remove tmp files
 rm -fr /var/tmp/xrl.* || exit 1
+
+# Remove build directory
+rm -fr obj
 
 exit 0
