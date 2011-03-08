52655e4fd66743b342a17d7434b9fe04f7c733ae
Ben Greear <greearb@candelatech.com>
2011-03-08 14:06:59 -0800

    Fix test value related to vlans.
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- xorp/libfeaclient/tests/test_local_copy.cc
+++ xorp/libfeaclient/tests/test_local_copy.cc
@@ -157,12 +157,16 @@ test_main()
 	verbose_log("Failed to set pif index\n");
 	return 1;
     }
-    if (IfMgrVifSetIsVlan("if0", "vif0", true).execute(t) == false) {
-	verbose_log("Failed to set VLAN vif\n");
+    if (IfMgrVifSetString("if0", "eth0",  IF_STRING_PARENT_IFNAME).execute(t) == false) {
+	verbose_log("Failed to set VLAN parent ifname\n");
 	return 1;
     }
-    if (IfMgrVifSetVlanId("if0", "vif0", 1234).execute(t) == false) {
-	verbose_log("Failed to set VLAN ID\n");
+    if (IfMgrVifSetString("if0", "VLAN",  IF_STRING_IFTYPE).execute(t) == false) {
+	verbose_log("Failed to set iface-type\n");
+	return 1;
+    }
+    if (IfMgrVifSetString("if0", "1234",  IF_STRING_VID).execute(t) == false) {
+	verbose_log("Failed to set VLAN VID\n");
 	return 1;
     }
 
