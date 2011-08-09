c31e1d1c7f8bc1db1a36eba4a26c0c4c50b4fe52
Ben Greear <greearb@candelatech.com>
2011-08-08 18:58:20 -0700

    Update win32_pkg script to build as well.
    Add notes on Fedora cross-compile packages.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- xorp/win32_pkg.bash
+++ xorp/win32_pkg.bash
@@ -1,19 +1,50 @@
 #!/bin/bash
-# Script for packaging up a xorp tarball for install in
-# /usr/local/
+# Script for building and packaging up a xorp windows binary
 
+# This needs to be run as root (or via sudo) to have a good
+# chance of working...
+
+# This works on modern Fedora, with these pkgs installed:
+
+#mingw32-pdcurses-3.4-8.fc15.noarch
+#mingw32-cpp-4.5.3-1.fc15.i686
+#mingw32-binutils-2.21-1.fc15.i686
+#mingw32-w32api-3.15-2.fc15.noarch
+#mingw32-openssl-1.0.0d-1.fc15.noarch
+#mingw32-libgnurx-2.5.1-7.fc15.noarch
+#mingw32-zlib-1.2.5-3.fc15.noarch
+#mingw32-gcc-c++-4.5.3-1.fc15.i686
+#mingw32-gcc-4.5.3-1.fc15.i686
+#mingw32-runtime-3.15.2-5.fc13.noarch
+#mingw32-pthreads-2.8.0-13.fc15.noarch
+#mingw32-filesystem-69-3.fc15.noarch
+
+# In addition, you need the fix listed in this bug:
+#http://lists.fedoraproject.org/pipermail/mingw/2011-February/003442.html
+
+# mingw cross-compile arguments
+SARGS="strip=yes shared=no build=mingw32 STRIP=i686-pc-mingw32-strip \
+       CC=i686-pc-mingw32-gcc CXX=i686-pc-mingw32-g++ \
+       RANLIB=i686-pc-mingw32-ranlib  AR=i686-pc-mingw32-ar \
+       LD=i686-pc-mingw32-ld"
+
+
+# Clean up any previous installed xorp code.
 rm -fr /usr/local/xorp
 
-scons strip=yes shared=no build=mingw32 STRIP=i686-pc-mingw32-strip \
-         CC=i686-pc-mingw32-gcc CXX=i686-pc-mingw32-g++ \
-	 RANLIB=i686-pc-mingw32-ranlib \
-	 AR=i686-pc-mingw32-ar LD=i686-pc-mingw32-ld install
+# Build
+echo "Building..."
+scons -j4 $SARGS || exit 1
 
+echo "Installing..."
+scons $SARGS install || exit 2
+
+echo "Copy some files..."
 # Copy some run-time libraries to the xorp dir for packaging
-cp /usr/i686-pc-mingw32/sys-root/mingw/bin/libcrypto-10.dll /usr/local/xorp/sbin/
-cp /usr/i686-pc-mingw32/sys-root/mingw/bin/zlib1.dll /usr/local/xorp/sbin/
-cp /usr/i686-pc-mingw32/sys-root/mingw/bin/libgcc_s_sjlj-1.dll /usr/local/xorp/sbin/
-cp /usr/i686-pc-mingw32/sys-root/mingw/bin/libgnurx-0.dll /usr/local/xorp/sbin/
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/libcrypto-10.dll /usr/local/xorp/sbin/ || exit 3
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/zlib1.dll /usr/local/xorp/sbin/ || exit 4
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/libgcc_s_sjlj-1.dll /usr/local/xorp/sbin/ || exit 5
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/libgnurx-0.dll /usr/local/xorp/sbin/ || exit 6
 
 
 PWD=$(pwd)
@@ -24,7 +55,7 @@ then
     userdir = "./"
 fi
 
-cd /usr/local
+cd /usr/local || exit 7
 
 if [ ! -d ${userdir}tmp ]
 then
