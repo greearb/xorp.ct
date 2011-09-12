7be03741355c877b62944438905bfd2cdbff978e
Ben Greear <greearb@candelatech.com>
2011-09-12 16:57:31 -0700

    Update html for 1.8.4.
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- xorp/devnotes/update_changed_copyright.bash
+++ xorp/devnotes/update_changed_copyright.bash
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Called as: $0 [start-git-md5] [end-git-md5]
+# Called as: $0 [start-git-md5] [end-git-md5] [year]
 #set -x
 
 START=$1
 END=$2
+YEAR=$3
 
 for i in `git diff --name-only $START $END`
 do
   if [ -f $i ]
       then
-      ./xorp/devnotes/update_copyright.pl 2011 $i
+      ./xorp/devnotes/update_copyright.pl $YEAR $i
   fi
 done
 
