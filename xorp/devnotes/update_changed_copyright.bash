2c62e3d0fedac0e0ae9c03e29c712498b673b6c6
Ben Greear <greearb@candelatech.com>
2011-09-12 16:10:41 -0700

    Use new perl script to update copyright.
    Bash script was too hard to modify for me...
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- xorp/devnotes/update_changed_copyright.bash
+++ xorp/devnotes/update_changed_copyright.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Called as: $0 [start-git-md5] [end-git-md5]
-set -x
+#set -x
 
 START=$1
 END=$2
@@ -10,10 +10,7 @@ for i in `git diff --name-only $START $END`
 do
   if [ -f $i ]
       then
-      ./xorp/devnotes/update_copyright.sh 2009 2011 $i
-      # NOTE:  You probably need to hack on the update_copyright.sh
-      # script before using this line below.
-      #./xorp/devnotes/update_copyright.sh 2010 2011 $i
+      ./xorp/devnotes/update_copyright.pl 2011 $i
   fi
 done
 
